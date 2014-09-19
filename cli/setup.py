8589b88d5598ba9f79b8f3d7368625fdbc7d7389
Dougal Ballantyne <dougalb at gmail dot com>
2014-09-18 18:07:37 -0700

    0.0.11 release

--- cli/setup.py
+++ cli/setup.py
@@ -20,7 +20,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 console_scripts = ['cfncluster = cfncluster.cli:main']
-version = "0.0.10"
+version = "0.0.11"
 requires = ['boto', 'botocore'] 
 
 if sys.version_info[:2] == (2, 6):
