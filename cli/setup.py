1fd9cede781c1301511d8060797e14c77a020eba
Dougal Ballantyne <dougalb@amazon.com>
2015-04-15 16:56:59 -0700

    Dev builds

--- cli/setup.py
+++ cli/setup.py
@@ -20,8 +20,8 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 console_scripts = ['cfncluster = cfncluster.cli:main']
-version = "0.0.20"
-requires = ['boto>=2.36'] 
+version = "0.0.99"
+requires = ['boto>=2.38'] 
 
 if sys.version_info[:2] == (2, 6):
     # For python2.6 we have to require argparse since it
