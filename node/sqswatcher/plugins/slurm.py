f0bb582692dbc42708d0dd3fb37c93b318f7225e
mike-jt79 <mike.turner@zenotech.com>
2015-10-07 14:50:59 +0100

    updates to get slurmd started on compute nodes

--- node/sqswatcher/plugins/slurm.py
+++ node/sqswatcher/plugins/slurm.py
@@ -2,6 +2,9 @@ import subprocess as sub
 from tempfile import mkstemp
 from shutil import move
 import os
+import paramiko
+import socket
+import time
 
 
 def __runCommand(command):
@@ -10,6 +13,37 @@ def __runCommand(command):
         sub.check_call(_command, env=dict(os.environ))
     except sub.CalledProcessError:
         print ("Failed to run %s\n" % _command)
+
+
+def __restartSlurm(hostname, cluster_user):
+    # Connect and restart Slurm on compute node
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    hosts_key_file = os.path.expanduser("~" + cluster_user) + '/.ssh/known_hosts'
+    user_key_file = os.path.expanduser("~" + cluster_user) + '/.ssh/id_rsa'
+    iter=0
+    connected=False
+    while iter < 3 and not connected:
+        try:
+            print('Connecting to host: %s iter: %d' % (hostname, iter))
+            ssh.connect(hostname, username=cluster_user, key_filename=user_key_file)
+            connected = True
+        except socket.error, e:
+            print('Socket error: %s' % e)
+            time.sleep(10 + iter)
+            iter = iter + 1
+            if iter == 3:
+                print("Unable to provison host")
+                return
+    try:
+        ssh.load_host_keys(hosts_key_file)
+    except IOError:
+        ssh._host_keys_filename = None
+        pass
+    ssh.save_host_keys(hosts_key_file)
+    command = "sudo sh -c \'/etc/init.d/slurm restart &> /tmp/slurmdstart.log\'"
+    stdin, stdout, stderr = ssh.exec_command(command)
+    ssh.close()
 
 
 def __readNodeList():
@@ -58,9 +92,12 @@ def addHost(hostname, cluster_user):
     node_list.append(hostname)
     __writeNodeList(node_list)
 
-    # Restart slurmctl
+    # Restart slurmctl locally
     command = ['/etc/init.d/slurm', 'restart']
     __runCommand(command)
+
+    # Restart slurmctl on host
+    __restartSlurm(hostname, cluster_user)
 
 
 def removeHost(hostname, cluster_user):
@@ -76,4 +113,5 @@ def removeHost(hostname, cluster_user):
     # Restart slurmctl
     command = ['/etc/init.d/slurm', 'restart']
     __runCommand(command)
+
 
