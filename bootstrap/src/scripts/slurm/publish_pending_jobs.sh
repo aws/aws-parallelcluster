47e7a8ba6d9747c5213406d7d494042e8a57b2bb
mike-jt79 <mike.turner@zenotech.com>
2015-11-06 10:52:55 +0000

    report number of pending jobs rather than number of nodes requested by pending jobs

--- bootstrap/src/scripts/slurm/publish_pending_jobs.sh
+++ bootstrap/src/scripts/slurm/publish_pending_jobs.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
-# Copyright 2013-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the
-# License. A copy of the License is located at
-#
-# http://aws.amazon.com/asl/
-#
-# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES 
-# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
-# limitations under the License.
-
 . /opt/cfncluster/cfnconfig
 
 ec2_region_url="http://169.254.169.254/latest/meta-data/placement/availability-zone"
 ec2_region=$(curl --retry 3 --retry-delay 0 --silent --fail ${ec2_region_url})
 
-pending=$(/opt/slurm/bin/squeue -h -o '%t %D' | awk '$1 == "PD" { total = total + $2} END {print total}')
+pending=$(/opt/slurm/bin/squeue -h -o '%t %D' | awk '$1 == "PD" { total = total + 1} END {print total}')q
 
 if [ "${pending}x" == "x" ]; then
 pending=0
