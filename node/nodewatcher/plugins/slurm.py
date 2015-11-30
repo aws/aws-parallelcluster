9c4098b694c0d3aef30ad452cc978d76471b1016
mike-jt79 <mike.turner@zenotech.com>
2015-11-30 11:00:49 +0000

    use hostname for squeue command rather than FQDN

--- node/nodewatcher/plugins/slurm.py
+++ node/nodewatcher/plugins/slurm.py
@@ -2,8 +2,10 @@ import subprocess
 
 
 def getJobs(hostname):
+    # Slurm won't use FQDN
+    short_name = hostname.split('.')[0]
     # Checking for running jobs on the node
-    _command = ['/opt/slurm/bin/squeue', '-w', hostname, '-h']
+    _command = ['/opt/slurm/bin/squeue', '-w', short_name, '-h']
     try:
         output = subprocess.Popen(_command, stdout=subprocess.PIPE).communicate()[0]
     except subprocess.CalledProcessError:
