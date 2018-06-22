5f65844063b28222928bc818119d6c71aca68209
Enrico Usai <usai@amazon.com>
2018-06-22 11:13:52 +0200

    Update copyright year in the documentation
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CfnCluster'
-copyright = u'2014-2017, Amazon Web Services'
+copyright = u'2014-2018, Amazon Web Services'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
