ffb32a9165622fd836b07ba4311a2ee47a992c0e
Balaji Sridharan <fnubalaj@amazon.com>
2018-08-24 11:11:32 -0700

    Change the timeout for scaledown check to 5 minutes from 4

--- tests/cluster-check.sh
+++ tests/cluster-check.sh
@@ -215,7 +215,7 @@ scaledown_check_launch() {
     # bounded completion time
     if test "$CHECK_CLUSTER_SUBPROCESS" = ""; then
         export CHECK_CLUSTER_SUBPROCESS=1
-        timeout -s KILL 4m /bin/bash ./cluster-check.sh "$@"
+        timeout -s KILL 5m /bin/bash ./cluster-check.sh "$@"
         exit $?
     fi
 
