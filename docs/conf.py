d9d77730910af60b9bae3d8cc60a43450df9162b
Francesco De Martino <fdm@amazon.com>
2018-11-16 10:31:58 +0100

    Add rst extension to docs/README file
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ release = '2.0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['staging']
+exclude_patterns = ['staging', 'README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
