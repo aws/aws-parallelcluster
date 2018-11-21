ef7daa998de8bd2dd02318326cb7a18523a8bbb9
Enrico Usai <usai@amazon.com>
2018-11-21 16:14:35 +0100

    We need to be sure that docker scripts are executables
    This patch solves some permssion denied issues.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/pcluster/resources/batch/docker/scripts/entrypoint.sh
+++ cli/pcluster/resources/batch/docker/scripts/entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 echo "Starting Job ${AWS_BATCH_JOB_ID}"
 
 # mount nfs
-/usr/bin/mount_nfs.sh "${MASTER_IP}" "${SHARED_DIR}"
+/parallelcluster/bin/mount_nfs.sh "${MASTER_IP}" "${SHARED_DIR}"
 
 # create hostfile if mnp job
 if [ -n "${AWS_BATCH_JOB_NUM_NODES}" ]; then
-  /usr/bin/generate_hostfile.sh "${SHARED_DIR}" "${HOME}"
+  /parallelcluster/bin/generate_hostfile.sh "${SHARED_DIR}" "${HOME}"
 fi
 
 # run the user's script
