2af597c46ebd116d5aeeaf2a1fb11a6c065c09ff
Rex Chen <shuningc@amazon.com>
2018-12-04 10:23:55 -0800

    Patch to fix EFS Batch compatibility
    - Modified batch-substack.cfn.json to include default value "NONE" for EFSFSId
    - Modified entrypoint.sh to consider mounting EFS shared dir if EFS_FS_ID != "NONE"
    - Modified stack templates and cfnconfig to change "EFSoptions" to "EFSOptions"
    
    Signed-off-by: Rex Chen <shuningc@amazon.com>

--- util/generate-efs-substack.py
+++ util/generate-efs-substack.py
@@ -9,7 +9,7 @@ def main():
     # 4 provisioned_throughput, 5 encrypted, 6 throughput_mode, 7 exists_valid_mt]
     efs_options = t.add_parameter(
         Parameter(
-            "EFSoptions",
+            "EFSOptions",
             Type="CommaDelimitedList",
             Description="Comma separated list of efs related options, " "8 parameters in total",
         )
