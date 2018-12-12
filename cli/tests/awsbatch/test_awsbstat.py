198669cfc8630e59a0d9e1bd97f4972f3c9d3ded
Francesco De Martino <fdm@amazon.com>
2018-12-12 11:16:57 +0100

    awsbstat: show parent when passing ids
    * awsbstat —expand-children - returns all jobs but only children in case of arrays and mnps
    * awsbstat <job-array-id> (o <job-mnp-id>) - returns the table with parents and relative children
    * awsbstat -d <job-array-id> (o <job-mnp-id>) - same as above but with a detailed view
    * In case of multiple job-ids we keep the same conventions listed above
    
    Ordering:
    * In case we specify job-ids the order of the output follows the list of ids provided as input.
    * In case we don't specify job-ids the order is by (status, startedAt, jobId)
    * Order applies both to tabular and detailed view
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cli/tests/awsbatch/test_awsbstat.py
+++ cli/tests/awsbatch/test_awsbstat.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from awsbatch import awsbstat, utils
+from awsbatch import awsbstat
 from common import DEFAULT_AWSBATCHCLICONFIG_MOCK_CONFIG, MockedBoto3Request, read_text
 
 ALL_JOB_STATUS = ["SUBMITTED", "PENDING", "RUNNABLE", "STARTING", "RUNNING", "SUCCEEDED", "FAILED"]
@@ -245,14 +245,11 @@ class TestOutput(object):
 
     def test_expanded_children(self, capsys, boto3_stubber, test_datadir, shared_datadir):
         mocked_requests = []
-        jobs_with_children_ids = []
+        # Mock all list-jobs requests
         for status in ALL_JOB_STATUS:
             list_jobs_response = json.loads(
                 read_text(shared_datadir / "aws_api_responses/batch_list-jobs_{0}.json".format(status))
             )
-            for job in list_jobs_response["jobSummaryList"]:
-                if utils.get_job_type(job) != "SIMPLE":
-                    jobs_with_children_ids.append(job["jobId"])
             mocked_requests.append(
                 MockedBoto3Request(
                     method="list_jobs",
@@ -264,14 +261,50 @@ class TestOutput(object):
                     },
                 )
             )
-        describe_jobs_response = json.loads(
+        # Mock describe-jobs on parents
+        describe_parent_jobs_response = json.loads(
+            read_text(shared_datadir / "aws_api_responses/batch_describe-jobs_ALL_parents.json")
+        )
+        jobs_with_children_ids = []
+        for job in describe_parent_jobs_response["jobs"]:
+            jobs_with_children_ids.append(job["jobId"])
+        mocked_requests.append(
+            MockedBoto3Request(
+                method="describe_jobs",
+                response=describe_parent_jobs_response,
+                expected_params={"jobs": jobs_with_children_ids},
+            )
+        )
+        # Mock describe-jobs on children
+        describe_children_jobs_response = json.loads(
             read_text(shared_datadir / "aws_api_responses/batch_describe-jobs_ALL_children.json")
         )
         mocked_requests.append(
             MockedBoto3Request(
                 method="describe_jobs",
-                response=describe_jobs_response,
-                expected_params={"jobs": jobs_with_children_ids},
+                response=describe_children_jobs_response,
+                expected_params={
+                    "jobs": [
+                        "3c6ee190-9121-464e-a0ac-62e4084e6bf1#0",
+                        "3c6ee190-9121-464e-a0ac-62e4084e6bf1#1",
+                        "11aa9096-1e98-4a7c-a44b-5ac3442df177:0",
+                        "11aa9096-1e98-4a7c-a44b-5ac3442df177:1",
+                        "77712b12-71eb-4007-a865-85f05de13a71#0",
+                        "77712b12-71eb-4007-a865-85f05de13a71#1",
+                        "bbbbbcbc-2647-4d8b-a1ef-da65bffe0dd0#0",
+                        "bbbbbcbc-2647-4d8b-a1ef-da65bffe0dd0#1",
+                        "qwerfcbc-2647-4d8b-a1ef-da65bffe0dd0#0",
+                        "qwerfcbc-2647-4d8b-a1ef-da65bffe0dd0#1",
+                        "3286a19c-68a9-47c9-8000-427d23ffc7ca:0",
+                        "3286a19c-68a9-47c9-8000-427d23ffc7ca:1",
+                        "3ec00225-8b85-48ba-a321-f61d005bec46#0",
+                        "3ec00225-8b85-48ba-a321-f61d005bec46#1",
+                        "44db07a9-f8a2-48d9-8d67-dcb04ceca54c:0",
+                        "44db07a9-f8a2-48d9-8d67-dcb04ceca54c:1",
+                        "7a712b12-71eb-4007-a865-85f05de13a71#0",
+                        "7a712b12-71eb-4007-a865-85f05de13a71#1",
+                    ]
+                },
             )
         )
         boto3_stubber("batch", mocked_requests)
@@ -283,3 +316,124 @@ class TestOutput(object):
         # behavior for not stubbed calls.
         output = capsys.readouterr().out.replace("1970-01-01T00:00:00+00:00", "-                        ")
         assert output == read_text(test_datadir / "expected_output.txt")
+
+    @pytest.mark.parametrize(
+        "args, expected",
+        [
+            (
+                [
+                    "3286a19c-68a9-47c9-8000-427d23ffc7ca",
+                    "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                    "6abf3ecd-07a8-4faa-8a65-79e7404eb50f",
+                ],
+                "expected_output.txt",
+            ),
+            (
+                [
+                    "-d",
+                    "3286a19c-68a9-47c9-8000-427d23ffc7ca",
+                    "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                    "6abf3ecd-07a8-4faa-8a65-79e7404eb50f",
+                ],
+                "expected_output_detailed.txt",
+            ),
+        ],
+        ids=["tabular", "detailed"],
+    )
+    def test_default_ordering_by_id(self, args, expected, capsys, boto3_stubber, test_datadir, shared_datadir):
+        parent_jobs_response = {"jobs": []}
+        for file in [
+            "batch_describe-jobs_single_mnp_job.json",
+            "batch_describe-jobs_single_array_job.json",
+            "batch_describe-jobs_single_job.json",
+        ]:
+            parent_jobs_response["jobs"].extend(
+                json.loads(read_text(shared_datadir / "aws_api_responses/{0}".format(file)))["jobs"]
+            )
+
+        children_jobs_response = {"jobs": []}
+        for file in [
+            "batch_describe-jobs_single_mnp_job_children.json",
+            "batch_describe-jobs_single_array_job_children.json",
+        ]:
+            children_jobs_response["jobs"].extend(
+                json.loads(read_text(shared_datadir / "aws_api_responses/{0}".format(file)))["jobs"]
+            )
+
+        boto3_stubber(
+            "batch",
+            [
+                MockedBoto3Request(
+                    method="describe_jobs",
+                    response=parent_jobs_response,
+                    expected_params={
+                        "jobs": [
+                            "3286a19c-68a9-47c9-8000-427d23ffc7ca",
+                            "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                            "6abf3ecd-07a8-4faa-8a65-79e7404eb50f",
+                        ]
+                    },
+                ),
+                MockedBoto3Request(
+                    method="describe_jobs",
+                    response=children_jobs_response,
+                    expected_params={
+                        "jobs": [
+                            "6abf3ecd-07a8-4faa-8a65-79e7404eb50f#0",
+                            "6abf3ecd-07a8-4faa-8a65-79e7404eb50f#1",
+                            "3286a19c-68a9-47c9-8000-427d23ffc7ca:0",
+                            "3286a19c-68a9-47c9-8000-427d23ffc7ca:1",
+                        ]
+                    },
+                ),
+            ],
+        )
+
+        awsbstat.main(["-c", "cluster"] + args)
+
+        assert capsys.readouterr().out == read_text(test_datadir / expected)
+
+    @pytest.mark.parametrize(
+        "args, expected",
+        [
+            (
+                [
+                    "3286a19c-68a9-47c9-8000-427d23ffc7ca:0",
+                    "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                    "6abf3ecd-07a8-4faa-8a65-79e7404eb50f#1",
+                ],
+                "expected_output.txt",
+            ),
+            (
+                [
+                    "-d",
+                    "3286a19c-68a9-47c9-8000-427d23ffc7ca:0",
+                    "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                    "6abf3ecd-07a8-4faa-8a65-79e7404eb50f#1",
+                ],
+                "expected_output_detailed.txt",
+            ),
+        ],
+        ids=["tabular", "detailed"],
+    )
+    def test_children_by_ids(self, args, expected, capsys, boto3_stubber, test_datadir, shared_datadir):
+        boto3_stubber(
+            "batch",
+            MockedBoto3Request(
+                method="describe_jobs",
+                response=json.loads(
+                    read_text(shared_datadir / "aws_api_responses/batch_describe-jobs_children_jobs.json")
+                ),
+                expected_params={
+                    "jobs": [
+                        "3286a19c-68a9-47c9-8000-427d23ffc7ca:0",
+                        "ab2cd019-1d84-43c7-a016-9772dd963f3b",
+                        "6abf3ecd-07a8-4faa-8a65-79e7404eb50f#1",
+                    ]
+                },
+            ),
+        )
+
+        awsbstat.main(["-c", "cluster"] + args)
+
+        assert capsys.readouterr().out == read_text(test_datadir / expected)
