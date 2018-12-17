be69c3a3ea73f8122b7dd0f68e6e1dfba2d6cc7b
Enrico Usai <usai@amazon.com>
2018-12-17 15:17:26 +0100

    docs: fix MNP sample
    Execute compilation in the main node only and fix quoting
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- docs/tutorials/code_samples/batch_mpi/submit_mpi.sh
+++ docs/tutorials/code_samples/batch_mpi/submit_mpi.sh
@@ -2,12 +2,12 @@
 echo "ip container: $(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)"
 echo "ip host: $(curl -s "http://169.254.169.254/latest/meta-data/local-ipv4")"
 
-echo "Compiling..."
-/usr/lib64/openmpi/bin/mpicc -o mpi_hello_world /shared/mpi_hello_world.c
-echo "Running..."
 if [[ "${AWS_BATCH_JOB_NODE_INDEX}" -eq  "${AWS_BATCH_JOB_MAIN_NODE_INDEX}" ]]; then
+    echo "Compiling..."
+    /usr/lib64/openmpi/bin/mpicc -o mpi_hello_world /shared/mpi_hello_world.c
+
     echo "Hello I'm the main node! I run the mpi job!"
-    /usr/lib64/openmpi/bin/mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root --machinefile "${HOME}/hostfile mpi_hello_world"
+    /usr/lib64/openmpi/bin/mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root --machinefile "${HOME}/hostfile" mpi_hello_world
 else
     echo "Hello I'm a compute note! I let the main node orchestrate the mpi execution!"
     # Since mpi orchestration happens on the main node, we need to make sure the containers representing the compute
