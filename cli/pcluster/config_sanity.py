2a755b925766ac039e7d37fe4e5b1e27ac20c256
Enrico Usai <usai@amazon.com>
2019-02-08 16:17:35 +0100

    Define a fail function for the config sanity module
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/pcluster/config_sanity.py
+++ cli/pcluster/config_sanity.py
@@ -82,23 +82,20 @@ def check_efs_fs_id(ec2, efs, resource_value):  # noqa: C901 FIXME!!!
                     nfs_access = True
                     break
             if not nfs_access:
-                print(
-                    "Config sanity error: There is an existing Mount Target %s in the Availability Zone %s for EFS %s, "
+                _fail(
+                    "EFSFSId",
+                    "There is an existing Mount Target %s in the Availability Zone %s for EFS %s, "
                     "and it does not have a security group with inbound and outbound rules that support NFS. "
                     "Please modify the Mount Target's security group, or delete the Mount Target."
                     % (mt_id, availability_zone, resource_value[0])
                 )
-                sys.exit(1)
+
             return True
     except ClientError as e:
-        print("Config sanity error: %s" % e.response.get("Error").get("Message"))
-        sys.exit(1)
+        _fail("EFSFSId", e.response.get("Error").get("Message"))
 
 
-def check_resource(  # noqa: C901 FIXME!!!
-    region, aws_access_key_id, aws_secret_access_key, resource_type, resource_value
-):
-
+def check_resource(region, aws_access_key_id, aws_secret_access_key, resource_type, resource_value):  # noqa: C901 FIXME
     # Loop over all supported resource checks
     # EC2 KeyPair
     if resource_type == "EC2KeyPair":
@@ -111,8 +108,7 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_key_pairs(KeyNames=[resource_value])
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     if resource_type == "EC2IAMRoleName":
         try:
             iam = boto3.client(
@@ -199,8 +195,7 @@ def check_resource(  # noqa: C901 FIXME!!!
                         print("See https://aws-parallelcluster.readthedocs.io/en/latest/iam.html")
                         sys.exit(1)
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # VPC Id
     elif resource_type == "VPC":
         try:
@@ -213,23 +208,20 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_vpcs(VpcIds=[resource_value])
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
         # Check for DNS support in the VPC
         if (
             not ec2.describe_vpc_attribute(VpcId=resource_value, Attribute="enableDnsSupport")
             .get("EnableDnsSupport")
             .get("Value")
         ):
-            print("DNS Support is not enabled in %s" % resource_value)
-            sys.exit(1)
+            _fail(resource_type, "DNS Support is not enabled in %s" % resource_value)
         if (
             not ec2.describe_vpc_attribute(VpcId=resource_value, Attribute="enableDnsHostnames")
             .get("EnableDnsHostnames")
             .get("Value")
         ):
-            print("DNS Hostnames not enabled in %s" % resource_value)
-            sys.exit(1)
+            _fail(resource_type, "DNS Hostnames not enabled in %s" % resource_value)
     # VPC Subnet Id
     elif resource_type == "VPCSubnet":
         try:
@@ -241,8 +233,7 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_subnets(SubnetIds=[resource_value])
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # VPC Security Group
     elif resource_type == "VPCSecurityGroup":
         try:
@@ -254,8 +245,7 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_security_groups(GroupIds=[resource_value])
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # EC2 AMI Id
     elif resource_type == "EC2Ami":
         try:
@@ -267,8 +257,7 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_images(ImageIds=[resource_value])
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # EC2 Placement Group
     elif resource_type == "EC2PlacementGroup":
         if resource_value == "DYNAMIC":
@@ -283,10 +272,7 @@ def check_resource(  # noqa: C901 FIXME!!!
                 )
                 test = ec2.describe_placement_groups(GroupNames=[resource_value])
             except ClientError as e:
-                print(
-                    "Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message"))
-                )
-                sys.exit(1)
+                _fail(resource_type, e.response.get("Error").get("Message"))
     # URL
     elif resource_type == "URL":
         scheme = urlparse(resource_value).scheme
@@ -296,11 +282,9 @@ def check_resource(  # noqa: C901 FIXME!!!
             try:
                 urllib.request.urlopen(resource_value)
             except urllib.error.HTTPError as e:
-                print("Config sanity error:", resource_value, e.code, e.reason)
-                sys.exit(1)
+                _fail(resource_type, "%s %s %s" % (resource_value, e.code, e.reason))
             except urllib.error.URLError as e:
-                print("Config sanity error:", resource_value, e.reason)
-                sys.exit(1)
+                _fail(resource_type, "%s %s" (resource_value, e.reason))
     # EC2 EBS Snapshot Id
     elif resource_type == "EC2Snapshot":
         try:
@@ -312,11 +296,12 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_snapshots(SnapshotIds=[resource_value]).get("Snapshots")[0]
             if test.get("State") != "completed":
-                print("Snapshot %s is in state '%s' not 'completed'" % (resource_value, test.get("State")))
-                sys.exit(1)
+                _fail(
+                    resource_type,
+                    "Snapshot %s is in state '%s' not 'completed'" % (resource_value, test.get("State"))
+                )
         except ClientError as e:
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # EC2 EBS Volume Id
     elif resource_type == "EC2Volume":
         try:
@@ -328,14 +313,12 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             test = ec2.describe_volumes(VolumeIds=[resource_value]).get("Volumes")[0]
             if test.get("State") != "available":
-                print("Volume %s is in state '%s' not 'available'" % (resource_value, test.get("State")))
-                sys.exit(1)
+                _fail(resource_type, "Volume %s is in state '%s' not 'available'" % (resource_value, test.get("State")))
         except ClientError as e:
             if e.response.get("Error").get("Message").endswith("parameter volumes is invalid. Expected: 'vol-...'."):
-                print("Config sanity error: volume %s does not exist." % resource_value)
-                sys.exit(1)
-            print("Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message")))
-            sys.exit(1)
+                _fail(resource_type, "Volume %s does not exist." % resource_value)
+
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # EFS file system Id
     elif resource_type == "EFSFSId":
         try:
@@ -353,69 +336,64 @@ def check_resource(  # noqa: C901 FIXME!!!
             )
             return check_efs_fs_id(ec2, efs, resource_value)
         except ClientError as e:
-            print("Config sanity error: %s" % e.response.get("Error").get("Message"))
-            sys.exit(1)
+            _fail(resource_type, e.response.get("Error").get("Message"))
     # EFS Performance Mode check
     elif resource_type == "EFSPerfMode":
         if resource_value != "generalPurpose" and resource_value != "maxIO":
-            print(
-                "Config sanity error: Invalid value for 'performance_mode'! "
+            _fail(
+                resource_type,
+                "Invalid value for 'performance_mode'! "
                 "Acceptable values for 'performance_mode' are generalPurpose and maxIO"
             )
-            sys.exit(1)
     # EFS Throughput check
     elif resource_type == "EFSThroughput":
         throughput_mode = resource_value[0]
         provisioned_throughput = resource_value[1]
         if throughput_mode and (throughput_mode != "provisioned" and throughput_mode != "bursting"):
-            print(
-                "Config sanity error: Invalid value for 'throughput_mode'! "
+            _fail(
+                resource_type,
+                "Invalid value for 'throughput_mode'! "
                 "Acceptable values for 'throughput_mode' are bursting and provisioned"
             )
-            sys.exit(1)
         if provisioned_throughput is not None:
             if throughput_mode != "provisioned":
-                print(
-                    "Config sanity error: When specifying 'provisioned_throughput', "
-                    "the 'throughput_mode' must be set to provisioned"
+                _fail(
+                    resource_type,
+                    "When specifying 'provisioned_throughput', the 'throughput_mode' must be set to provisioned"
                 )
-                sys.exit(1)
         else:
             if throughput_mode == "provisioned":
-                print(
-                    "Config sanity error: When specifying 'throughput_mode' to provisioned, "
+                _fail(
+                    resource_type,
+                    "When specifying 'throughput_mode' to provisioned, "
                     "the 'provisioned_throughput' option must be specified"
                 )
-                sys.exit(1)
     # RAID EBS IOPS
     elif resource_type == "RAIDIOPS":
         raid_iops = float(resource_value[0])
         raid_vol_size = float(resource_value[1])
         if raid_iops > raid_vol_size * 50:
-            print(
-                "Config sanity error: IOPS to volume size ratio of %s is too high; maximum is 50."
-                % (raid_iops / raid_vol_size)
+            _fail(
+                resource_type,
+                "IOPS to volume size ratio of %s is too high; maximum is 50." % (raid_iops / raid_vol_size)
             )
-            sys.exit(1)
     # RAID Array Type
     elif resource_type == "RAIDType":
         if resource_value != "0" and resource_value != "1":
-            print("Config sanity error: invalid raid_type, only RAID 0 and RAID 1 are currently supported.")
-            sys.exit(1)
+            _fail(resource_type, "Invalid raid_type, only RAID 0 and RAID 1 are currently supported.")
     # Number of RAID Volumes Requested
     elif resource_type == "RAIDNumVol":
         if int(resource_value) > 5 or int(resource_value) < 2:
-            print(
-                "Config sanity error: invalid num_of_raid_volumes. "
+            _fail(
+                resource_type,
+                "Invalid num_of_raid_volumes. "
                 "Needs min of 2 volumes for RAID and max of 5 EBS volumes are currently supported."
             )
-            sys.exit(1)
     # Batch Parameters
     elif resource_type == "AWSBatch_Parameters":
         # Check region
         if region in ["ap-northeast-3", "eu-north-1", "cn-north-1", "cn-northwest-1", "us-gov-east-1", "us-gov-west-1"]:
-            print("ERROR: %s region is not supported with awsbatch" % region)
-            sys.exit(1)
+            _fail(resource_type, "Region %s is not supported with batch scheduler" % region)
 
         # Check compute instance types
         if "ComputeInstanceType" in resource_value:
@@ -428,44 +406,43 @@ def check_resource(  # noqa: C901 FIXME!!!
                     supported_instances = json.loads(file_contents)
                     for instance in resource_value["ComputeInstanceType"].split(","):
                         if not instance.strip() in supported_instances:
-                            print("Instance type %s not supported by batch in this region" % instance)
-                            sys.exit(1)
+                            _fail(resource_type, "Instance type %s not supported by batch in this region" % instance)
                 except ClientError as e:
-                    print(
-                        "Config sanity error on resource %s: %s"
-                        % (resource_type, e.response.get("Error").get("Message"))
-                    )
-                    sys.exit(1)
+                    _fail(resource_type, e.response.get("Error").get("Message"))
             except ClientError as e:
-                print(
-                    "Config sanity error on resource %s: %s" % (resource_type, e.response.get("Error").get("Message"))
-                )
-                sys.exit(1)
+                _fail(resource_type, e.response.get("Error").get("Message"))
 
         # Check spot bid percentage
         if "SpotPrice" in resource_value:
             if int(resource_value["SpotPrice"]) > 100 or int(resource_value["SpotPrice"]) < 0:
-                print("ERROR: Spot bid percentage needs to be between 0 and 100")
-                sys.exit(1)
+                _fail(resource_type, "Spot bid percentage needs to be between 0 and 100")
 
         # Check sanity on desired, min and max vcpus
         if "DesiredSize" in resource_value and "MinSize" in resource_value:
             if int(resource_value["DesiredSize"]) < int(resource_value["MinSize"]):
-                print("ERROR: Desired vcpus must be greater than or equal to min vcpus")
-                sys.exit(1)
+                _fail(resource_type, "Desired vcpus must be greater than or equal to min vcpus")
 
         if "DesiredSize" in resource_value and "MaxSize" in resource_value:
             if int(resource_value["DesiredSize"]) > int(resource_value["MaxSize"]):
-                print("ERROR: Desired vcpus must be fewer than or equal to max vcpus")
-                sys.exit(1)
+                _fail(resource_type, "Desired vcpus must be fewer than or equal to max vcpus")
 
         if "MaxSize" in resource_value and "MinSize" in resource_value:
             if int(resource_value["MaxSize"]) < int(resource_value["MinSize"]):
-                print("ERROR: Max vcpus must be greater than or equal to min vcpus")
-                sys.exit(1)
+                _fail(resource_type, "Max vcpus must be greater than or equal to min vcpus")
 
         # Check custom batch url
         if "CustomAWSBatchTemplateURL" in resource_value:
             check_resource(
                 region, aws_access_key_id, aws_secret_access_key, "URL", resource_value["CustomAWSBatchTemplateURL"]
             )
+
+
+def _fail(resource_type, message):
+    """
+    Print an error and exit.
+
+    :param resource_type: Resource on which the config sanity check failed
+    :param message: the message to print
+    """
+    print("Config sanity error on resource %s: %s" % (resource_type, message))
+    sys.exit(1)
