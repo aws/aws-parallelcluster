b37cf8db4255eae16db38b090d39fe0cb48c29a2
Enrico Usai <usai@amazon.com>
2019-04-03 18:12:18 +0200

    Integ Tests: extend update test by changing the compute instance type
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/update/test_update.py
+++ tests/integration-tests/tests/update/test_update.py
@@ -11,8 +11,12 @@
 # See the License for the specific language governing permissions and limitations under the License.
 import boto3
 import pytest
+
 from assertpy import assert_that
-from tests.common.scaling_common import get_max_asg_capacity
+from remote_command_executor import RemoteCommandExecutor
+from tests.common.scaling_common import get_max_asg_capacity, watch_compute_nodes
+from tests.common.schedulers_common import SlurmCommands
+from time_utils import minutes
 
 
 @pytest.mark.regions(["us-west-1"])
@@ -30,25 +34,59 @@ def test_update(region, pcluster_config_reader, clusters_factory):
 
     cluster_config = pcluster_config_reader(max_queue_size=max_queue_size, compute_instance=compute_instance)
     cluster, factory = clusters_factory(cluster_config)
-    _test_asg_size(region, cluster.cfn_name, max_queue_size)
+    # Verify initial settings
+    _test_max_queue(region, cluster.cfn_name, max_queue_size)
+    _test_compute_instance_type(cluster.cfn_name, compute_instance)
 
-    _test_update_max_queue(region, cluster, factory)
+    # Configuration parameters for the update test
+    new_max_queue_size = 10
+    new_compute_instance = "c4.xlarge"
 
-
-def _test_update_max_queue(region, cluster, factory):
-    new_queue_size = 10
-    _update_cluster_property(cluster, "max_queue_size", str(new_queue_size))
-
+    # change config settings
+    _update_cluster_property(cluster, "max_queue_size", str(new_max_queue_size))
+    _update_cluster_property(cluster, "compute_instance_type", new_compute_instance)
+    # update configuration file
+    cluster.update()
+    # update cluster
     factory.update_cluster(cluster)
-    _test_asg_size(region, cluster.cfn_name, new_queue_size)
+
+    # test update
+    _test_max_queue(region, cluster.cfn_name, new_max_queue_size)
+    _test_update_compute_instance_type(cluster, new_compute_instance)
 
 
-def _test_asg_size(region, stack_name, queue_size):
+def _test_max_queue(region, stack_name, queue_size):
     asg_max_size = get_max_asg_capacity(region, stack_name)
     assert_that(asg_max_size).is_equal_to(queue_size)
 
 
+def _test_update_compute_instance_type(cluster, new_compute_instance):
+    # submit a job to perform a scaling up action and have a new instance
+    number_of_nodes = 2
+    remote_command_executor = RemoteCommandExecutor(cluster)
+    slurm_commands = SlurmCommands(remote_command_executor)
+    result = slurm_commands.submit_command("sleep 60", nodes=number_of_nodes)
+    slurm_commands.assert_job_submitted(result.stdout)
+
+    estimated_scaleup_time = 5
+    watch_compute_nodes(
+        scheduler_commands=slurm_commands,
+        max_monitoring_time=minutes(estimated_scaleup_time),
+        number_of_nodes=number_of_nodes,
+    )
+    _test_compute_instance_type(cluster.cfn_name, new_compute_instance)
+
+
+def _test_compute_instance_type(stack_name, compute_instance_type):
+    ec2_client = boto3.resource("ec2")
+    instance_ids = []
+    instance_types = []
+    for instance in ec2_client.instances.filter(Filters=[{"Name": "tag:Application", "Values": [stack_name]}]):
+        instance_ids.append(instance.instance_id)
+        instance_types.append(instance.instance_type)
+
+    assert_that(instance_types).contains(compute_instance_type)
+
+
 def _update_cluster_property(cluster, property_name, property_value):
     cluster.config.set("cluster default", property_name, property_value)
-    # update configuration file
-    cluster.update()
