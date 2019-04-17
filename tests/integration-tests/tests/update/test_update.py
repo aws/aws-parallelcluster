be8bde1532f654b06e222b376cf8c5a4b2f7fb5c
Enrico Usai <usai@amazon.com>
2019-04-17 16:22:33 +0200

    Integ tests: test s3_read_(write_)?_resource update
    We are verifing that a customer is able to update the
    s3_read_resource and s3_read_write_resource configuration parameters
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/update/test_update.py
+++ tests/integration-tests/tests/update/test_update.py
@@ -20,7 +20,9 @@ from tests.common.scaling_common import get_max_asg_capacity, watch_compute_node
 from tests.common.schedulers_common import SlurmCommands
 from time_utils import minutes
 
-PClusterConfig = namedtuple("PClusterConfig", ["max_queue_size", "compute_instance"])
+PClusterConfig = namedtuple(
+    "PClusterConfig", ["max_queue_size", "compute_instance", "s3_read_resource", "s3_read_write_resource"]
+)
 
 
 @pytest.mark.dimensions("eu-west-1", "c5.xlarge", "alinux", "slurm")
@@ -31,27 +33,43 @@ def test_update(instance, region, pcluster_config_reader, clusters_factory):
 
     Grouped all tests in a single function so that cluster can be reused for all of them.
     """
-    init_config = PClusterConfig(max_queue_size=5, compute_instance=instance)
+    s3_arn = "arn:aws:s3:::fake_bucket/*"
+    init_config = PClusterConfig(
+        max_queue_size=5, compute_instance=instance, s3_read_resource=s3_arn, s3_read_write_resource=s3_arn
+    )
     cluster = _init_cluster(region, clusters_factory, pcluster_config_reader, init_config)
 
-    updated_config = PClusterConfig(max_queue_size=10, compute_instance="c4.xlarge")
+    s3_arn_updated = "arn:aws:s3:::fake_bucket/fake_folder/*"
+    updated_config = PClusterConfig(
+        max_queue_size=10,
+        compute_instance="c4.xlarge",
+        s3_read_resource=s3_arn_updated,
+        s3_read_write_resource=s3_arn_updated,
+    )
     _update_cluster(cluster, updated_config)
 
     # test update
     _test_max_queue(region, cluster.cfn_name, updated_config.max_queue_size)
     _test_update_compute_instance_type(region, cluster, updated_config.compute_instance)
+    _test_s3_read_resource(region, cluster, updated_config.s3_read_resource)
+    _test_s3_read_write_resource(region, cluster, updated_config.s3_read_write_resource)
 
 
 def _init_cluster(region, clusters_factory, pcluster_config_reader, config):
     # read configuration and create cluster
     cluster_config = pcluster_config_reader(
-        max_queue_size=config.max_queue_size, compute_instance=config.compute_instance
+        max_queue_size=config.max_queue_size,
+        compute_instance=config.compute_instance,
+        s3_read_resource=config.s3_read_resource,
+        s3_read_write_resource=config.s3_read_write_resource,
     )
     cluster = clusters_factory(cluster_config)
 
     # Verify initial settings
     _test_max_queue(region, cluster.cfn_name, config.max_queue_size)
     _test_compute_instance_type(region, cluster.cfn_name, config.compute_instance)
+    _test_s3_read_resource(region, cluster, config.s3_read_resource)
+    _test_s3_read_write_resource(region, cluster, config.s3_read_write_resource)
 
     return cluster
 
@@ -60,6 +78,8 @@ def _update_cluster(cluster, config):
     # change cluster.config settings
     _update_cluster_property(cluster, "max_queue_size", str(config.max_queue_size))
     _update_cluster_property(cluster, "compute_instance_type", config.compute_instance)
+    _update_cluster_property(cluster, "s3_read_resource", config.s3_read_resource)
+    _update_cluster_property(cluster, "s3_read_write_resource", config.s3_read_write_resource)
     # rewrite configuration file starting from the updated cluster.config object
     with open(cluster.config_file, "w") as configfile:
         cluster.config.write(configfile)
@@ -100,3 +120,24 @@ def _test_compute_instance_type(region, stack_name, compute_instance_type):
         instance_types.append(instance.instance_type)
 
     assert_that(instance_types).contains(compute_instance_type)
+
+
+def _test_policy_statement(region, cluster, policy_name, policy_statement):
+    iam_client = boto3.client("iam", region_name=region)
+    root_role = cluster.cfn_resources.get("RootRole")
+
+    statement = (
+        iam_client.get_role_policy(RoleName=root_role, PolicyName=policy_name)
+        .get("PolicyDocument")
+        .get("Statement")[0]
+        .get("Resource")[0]
+    )
+    assert_that(statement).is_equal_to(policy_statement)
+
+
+def _test_s3_read_resource(region, cluster, s3_arn):
+    _test_policy_statement(region, cluster, "S3Read", s3_arn)
+
+
+def _test_s3_read_write_resource(region, cluster, s3_arn):
+    _test_policy_statement(region, cluster, "S3ReadWrite", s3_arn)
