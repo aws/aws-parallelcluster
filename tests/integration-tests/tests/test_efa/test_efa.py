a0aa602f66761024ea7a2f7bdca887836b5cf94e
Francesco De Martino <fdm@amazon.com>
2019-06-07 18:06:35 +0200

    integ tests: various fixes for EFA tests
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- tests/integration-tests/tests/test_efa/test_efa.py
+++ tests/integration-tests/tests/test_efa/test_efa.py
@@ -17,67 +17,79 @@ from assertpy import assert_that
 from remote_command_executor import RemoteCommandExecutor
 from tests.common.schedulers_common import get_scheduler_commands
 
+INSTANCES_TO_SLOTS_MAP = {"c5n.18xlarge": 72, "p3dn.24xlarge": 96, "i3en.24xlarge": 96}
 
-@pytest.mark.regions(["us-east-1"])
+
+@pytest.mark.regions(["us-east-1", "eu-west-1"])
 @pytest.mark.instances(["c5n.18xlarge", "p3dn.24xlarge", "i3en.24xlarge"])
 @pytest.mark.oss(["alinux", "centos7", "ubuntu1604"])
 @pytest.mark.schedulers(["sge", "slurm"])
-@pytest.mark.usefixtures("os", "instance", "scheduler")
-def test_efa(scheduler, pcluster_config_reader, clusters_factory, test_datadir):
+@pytest.mark.usefixtures("os", "region")
+def test_efa(scheduler, instance, pcluster_config_reader, clusters_factory, test_datadir):
     """
     Test all EFA Features.
 
     Grouped all tests in a single function so that cluster can be reused for all of them.
     """
-    scaledown_idletime = 3
-    max_queue_size = 5
-    cluster_config = pcluster_config_reader(scaledown_idletime=scaledown_idletime, max_queue_size=max_queue_size)
+    max_queue_size = 2
+    slots_per_instance = INSTANCES_TO_SLOTS_MAP[instance]
+    cluster_config = pcluster_config_reader(max_queue_size=max_queue_size)
     cluster = clusters_factory(cluster_config)
     remote_command_executor = RemoteCommandExecutor(cluster)
+    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
 
-    _test_efa_installed(remote_command_executor)
-    _test_efa_mpi(remote_command_executor, scheduler, test_datadir)
+    _test_efa_installed(scheduler_commands, remote_command_executor)
+    _test_efa_mpi(remote_command_executor, scheduler_commands, test_datadir, slots_per_instance)
+    _test_osu_benchmarks(remote_command_executor, scheduler_commands, test_datadir, slots_per_instance)
 
 
-def _test_efa_installed(remote_command_executor, scheduler):
+def _test_efa_installed(scheduler_commands, remote_command_executor):
     # Output contains:
     # 00:06.0 Ethernet controller: Amazon.com, Inc. Device efa0
-    logging.info("Testing EFA Installed")
-    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
-    result = scheduler_commands.submit_interactive_command("/sbin/lspci")
+    logging.info("Testing EFA installed")
+    result = scheduler_commands.submit_command("/sbin/lspci > /shared/lspci.out")
+
+    job_id = scheduler_commands.assert_job_submitted(result.stdout)
+    scheduler_commands.wait_job_completed(job_id)
+    scheduler_commands.assert_job_succeeded(job_id)
+
+    # Check EFA interface is present on compute node
+    result = remote_command_executor.run_remote_command("cat /shared/lspci.out")
     assert_that(result.stdout).contains("00:06.0 Ethernet controller: Amazon.com, Inc. Device efa0")
 
+    # Check EFA interface not present on master
+    result = remote_command_executor.run_remote_command("/sbin/lspci")
+    assert_that(result.stdout).does_not_contain("00:06.0 Ethernet controller: Amazon.com, Inc. Device efa0")
 
-def _test_efa_mpi(remote_command_executor, scheduler, test_datadir):
-    logging.info("Testing EFA Installed")
+
+def _test_efa_mpi(remote_command_executor, scheduler_commands, test_datadir, slots_per_instance):
+    logging.info("Testing mpi job with EFA")
     # Compile mpi script
-    result = remote_command_executor.run_remote_command(
+    remote_command_executor.run_remote_command(
         "/opt/amazon/efa/bin/mpicc -o mpi_hello_world mpi_hello_world.c",
         additional_files=[str(test_datadir / "mpi_hello_world.c")],
-    ).stdout
-    logging.info(result)
+    )
 
     # submit script using additional files
-    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
-
-    result = scheduler_commands.submit_script(str(test_datadir / "{0}_submit.sh".format(scheduler)))
+    result = scheduler_commands.submit_script(str(test_datadir / "mpi_submit.sh"), slots=2 * slots_per_instance)
     job_id = scheduler_commands.assert_job_submitted(result.stdout)
     scheduler_commands.wait_job_completed(job_id)
     scheduler_commands.assert_job_succeeded(job_id)
 
+    mpi_out = remote_command_executor.run_remote_command("cat /shared/mpi.out").stdout
+    assert_that(mpi_out.splitlines()).is_length(2)
+    assert_that(mpi_out).matches(r"Hello world from processor ip-.+, rank 0 out of 2 processors")
+    assert_that(mpi_out).matches(r"Hello world from processor ip-.+, rank 1 out of 2 processors")
 
-def _test_osu_benchmarks(remote_command_executor, scheduler, test_datadir):
-    logging.info("Testing EFA Installed")
-    # Compile mpi script
-    result = remote_command_executor.run_remote_command(
-        "/bin/bash osu_benchmarks.sh", additional_files=[str(test_datadir / "osu_benchmarks.sh")]
-    ).stdout
-    logging.info(result)
 
-    # submit script using additional files
-    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
+def _test_osu_benchmarks(remote_command_executor, scheduler_commands, test_datadir, slots_per_instance):
+    logging.info("Running OSU benchmarks")
+    remote_command_executor.run_remote_script(str(test_datadir / "init_osu_benchmarks.sh"), hide=True)
 
-    result = scheduler_commands.submit_script(str(test_datadir / "{0}_submit_osu_benchmarks.sh".format(scheduler)))
+    result = scheduler_commands.submit_script(str(test_datadir / "osu_submit.sh"), slots=2 * slots_per_instance)
     job_id = scheduler_commands.assert_job_submitted(result.stdout)
     scheduler_commands.wait_job_completed(job_id)
     scheduler_commands.assert_job_succeeded(job_id)
+
+    # TODO: perform assertions on benchmarks results
+    remote_command_executor.run_remote_command("cat /shared/osu.out")
