2db1c2dd3bae6d342877009053bb5f1519f77551
Luca Carrogu <carrogu@amazon.com>
2019-07-05 09:50:35 +0200

    Blacklist c4.xlarge in HKG ap-east-1
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/conftest_markers.py
+++ tests/integration-tests/conftest_markers.py
@@ -17,6 +17,7 @@ DIMENSIONS_MARKER_ARGS = ["region", "instance", "os", "scheduler"]
 UNSUPPORTED_DIMENSIONS = [
     ("eu-north-1", "c4.xlarge", "*", "*"),
     ("eu-west-3", "c4.xlarge", "*", "*"),
+    ("ap-east-1", "c4.xlarge", "*", "*"),
     ("*", "*", "centos6", "awsbatch"),
     ("*", "*", "centos7", "awsbatch"),
     ("*", "*", "ubuntu1604", "awsbatch"),
