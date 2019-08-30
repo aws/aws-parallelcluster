82f1ed71e3c3f10cb4c9220d902a7be953f77544
Joey Dumont <joey.dumont@nrc.cnrc.gc.ca>
2019-08-30 10:43:33 -0400

    Modified ami-convert.py to support amis.yml.

--- cloudformation/ami_convert.py
+++ cloudformation/ami_convert.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python
 #
-# Convert the AMI list in amis.txt to output suitable for copying into
+# Convert the AMI list in amis.yml to output suitable for copying into
 # the json version of the CloudFormation template.
 #
-# usage: ami_convert.py <path to amis.txt>
+# usage: ami_convert.py <path to amis.yml>
 #
 
 import re
 import sys
+import yaml
+import collections
 
 if len(sys.argv) != 2:
-    sys.stderr.write("usage: %s <amis.txt file>" % (sys.argv[0]))
+    sys.stderr.write("usage: %s <amis.yml file>" % (sys.argv[0]))
     sys.exit(1)
 
 amis = sys.argv[1]
 
-regions = {}
+regions = collections.defaultdict(dict)
 current_os = ""
 
 with open(amis, "r") as f:
-    for line in f:
-        line = line.rstrip()
-        if not line:
-            continue
+    amis_yml = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-        if re.match("# ", line):
-            # new OS block
-            current_os = re.sub(r"#\s*(\w+).*", r"\1", line)
-            continue
-
-        pair = re.split(":", line)
-        if len(pair) != 2:
-            sys.stderr.write("Unexpected pair length %d for line %s" % (len(pair), line))
-            sys.exit(2)
-        region = pair[0].strip()
-        ami = pair[1].strip()
-
-        if region not in regions:
-            regions[region] = {}
-
-        regions[region][current_os] = ami
+# -- Sort by (region, OS) instead of (OS, region)
+for kos, vos in amis_yml.items():
+    for kregion, vregions in vos.items():
+        regions[kregion][kos] = amis_yml[kos][kregion]
 
 first_region = 1
 for region in sorted(regions):
