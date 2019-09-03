fdcb8f115b431d16b828a82b68de7370223f006e
Francesco De Martino <fdm@amazon.com>
2019-09-03 10:33:50 +0200

    Remove python 2.6 requirements since now it is not supported
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cli/setup.py
+++ cli/setup.py
@@ -24,11 +24,6 @@ def readme():
 VERSION = "2.4.1"
 REQUIRES = ["boto3>=1.9.54", "future>=0.16.0,<=0.17.1", "tabulate>=0.8.2,<=0.8.3", "ipaddress>=1.0.22", "enum34>=1.1.6"]
 
-if sys.version_info[:2] == (2, 6):
-    # For python2.6 we have to require argparse since it
-    # was not in stdlib until 2.7.
-    REQUIRES.append("argparse==1.4.0")
-
 if sys.version_info[0] == 2:
     REQUIRES.append("configparser>=3.5.0,<=3.5.3")
 
