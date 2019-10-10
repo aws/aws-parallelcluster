c035ec4deae5bff55a4e77c9c9b804d741b56fc5
Enrico Usai <usai@amazon.com>
2019-10-10 16:20:33 +0200

    Add ubuntu1804 to EFA supported OS
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/pcluster/config/validators.py
+++ cli/pcluster/config/validators.py
@@ -213,7 +213,7 @@ def efa_validator(param_key, param_value, pcluster_config):
             "to one of the following values : {1}".format(param_value, allowed_instances)
         )
 
-    allowed_oses = ["alinux", "centos7", "ubuntu1604"]
+    allowed_oses = ["alinux", "centos7", "ubuntu1604", "ubuntu1804"]
     if cluster_section.get_param_value("base_os") not in allowed_oses:
         errors.append(
             "When using 'enable_efa = {0}' it is required to set the 'base_os' parameter "
