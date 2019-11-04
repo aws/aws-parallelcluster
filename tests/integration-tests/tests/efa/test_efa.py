7a858708447a7cdde714d455a75c612e1958b8b5
Francesco De Martino <fdm@amazon.com>
2019-11-04 15:47:33 +0100

    Enable tests for new instance types
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- tests/integration-tests/tests/efa/test_efa.py
+++ tests/integration-tests/tests/efa/test_efa.py
@@ -22,10 +22,9 @@ from tests.common.schedulers_common import get_scheduler_commands
 from tests.common.utils import fetch_instance_slots
 
 
-@pytest.mark.regions(["us-east-1"])
-@pytest.mark.instances(["c5n.18xlarge", "p3dn.24xlarge", "i3en.24xlarge"])
+@pytest.mark.instances(["c5n.metal", "m5dn.24xlarge", "m5n.24xlarge", "r5dn.24xlarge", "r5n.24xlarge"])
 @pytest.mark.oss(["alinux", "centos7", "ubuntu1604", "ubuntu1804"])
-@pytest.mark.schedulers(["sge", "slurm"])
+@pytest.mark.schedulers(["slurm"])
 def test_efa(region, scheduler, instance, os, pcluster_config_reader, clusters_factory, test_datadir):
     """
     Test all EFA Features.
