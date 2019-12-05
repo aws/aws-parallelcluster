7035f867b94fef7911d8295e8e0b460a34a2490a
Francesco De Martino <fdm@amazon.com>
2019-12-05 11:47:24 +0100

    integ-tests: add private subnet on additional vpc cidr
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- tests/integration-tests/conftest.py
+++ tests/integration-tests/conftest.py
@@ -370,19 +370,30 @@ def vpc_stacks(cfn_stacks_factory, request):
         # defining subnets per region to allow AZs override
         public_subnet = SubnetConfig(
             name="Public",
-            cidr="10.0.124.0/22",  # 1,022 IPs
+            cidr="192.168.0.0/18",  # 16382 IPs
             map_public_ip_on_launch=True,
             has_nat_gateway=True,
             default_gateway=Gateways.INTERNET_GATEWAY,
         )
         private_subnet = SubnetConfig(
             name="Private",
-            cidr="10.0.128.0/17",  # 32766 IPs
+            cidr="192.168.64.0/18",  # 16382 IPs
             map_public_ip_on_launch=False,
             has_nat_gateway=False,
             default_gateway=Gateways.NAT_GATEWAY,
         )
-        vpc_config = VPCConfig(subnets=[public_subnet, private_subnet])
+        private_subnet_different_cidr = SubnetConfig(
+            name="PrivateAdditionalCidr",
+            cidr="192.168.128.0/17",  # 32766 IPs
+            map_public_ip_on_launch=False,
+            has_nat_gateway=False,
+            default_gateway=Gateways.NAT_GATEWAY,
+        )
+        vpc_config = VPCConfig(
+            cidr="192.168.0.0/17",
+            additional_cidr_blocks=["192.168.128.0/17"],
+            subnets=[public_subnet, private_subnet, private_subnet_different_cidr],
+        )
         template = NetworkTemplateBuilder(vpc_configuration=vpc_config, availability_zone=availability_zone).build()
         vpc_stacks[region] = _create_vpc_stack(request, template, region, cfn_stacks_factory)
 
