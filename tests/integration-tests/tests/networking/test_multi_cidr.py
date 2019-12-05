07015d69e218018875dc30406b7f78bcdea40026
Rex <shuningc@amazon.com>
2019-12-05 10:58:25 -0800

    Add test for multi CIDR VPC * Added test to test that a cluster is created when using VPC with 2 CIDR and compute subnet in the 2nd CIDR block of VPC.
    Signed-off-by: Rex <shuningc@amazon.com>

---
+++ tests/integration-tests/tests/networking/test_multi_cidr.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.mark.regions(["us-east-2"])
+@pytest.mark.instances(["c5.xlarge"])
+@pytest.mark.schedulers(["slurm", "awsbatch"])
+@pytest.mark.usefixtures("os", "instance", "scheduler", "region")
+def test_multi_cidr(pcluster_config_reader, clusters_factory):
+    """
+    Test cluster creation when there is multiple CIDR in a VPC.
+
+    Specifically test that cluster is created when compute subnet is in the second CIDR block of the VPC.
+    """
+    cluster_config = pcluster_config_reader()
+    clusters_factory(cluster_config)
