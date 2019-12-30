4d6b3c42f7292e9e7b18e71261d2a3aea97fcd9a
Rex <shuningc@amazon.com>
2019-12-30 13:13:24 -0600

    Fix efs integration tests
    * Add pre-assertion to check the AZs of master and compute subnets to make sure subnets are set up as expected
    * Add back efs settings to batch tests so that batch scripts are tested more often
    
    Signed-off-by: Rex <shuningc@amazon.com>

--- tests/integration-tests/tests/storage/test_efs.py
+++ tests/integration-tests/tests/storage/test_efs.py
@@ -11,12 +11,14 @@
 # See the License for the specific language governing permissions and limitations under the License.
 import logging
 
+import boto3
 import pytest
 
 from assertpy import assert_that
 from remote_command_executor import RemoteCommandExecutor
 from tests.common.schedulers_common import get_scheduler_commands
 from tests.storage.storage_common import verify_directory_correctly_shared
+from utils import get_vpc_snakecase_value
 
 
 # For EFS tests, only use regions defined in AVAILABILITY_ZONE_OVERRIDES in conftest
@@ -26,12 +28,13 @@ from tests.storage.storage_common import verify_directory_correctly_shared
 @pytest.mark.schedulers(["slurm", "awsbatch"])
 @pytest.mark.os(["alinux"])
 @pytest.mark.usefixtures("region", "os", "instance")
-def test_efs_compute_az(scheduler, pcluster_config_reader, clusters_factory):
+def test_efs_compute_az(region, scheduler, pcluster_config_reader, clusters_factory, vpc_stacks):
     """
     Test when compute subnet is in a different AZ from master subnet.
 
     A compute mount target should be created and the efs correctly mounted on compute.
     """
+    _assert_subnet_az_relations(region, vpc_stacks, expected_in_same_az=False)
     mount_dir = "efs_mount_dir"
     cluster_config = pcluster_config_reader(mount_dir=mount_dir)
     cluster = clusters_factory(cluster_config)
@@ -48,12 +51,13 @@ def test_efs_compute_az(scheduler, pcluster_config_reader, clusters_factory):
 @pytest.mark.schedulers(["slurm", "awsbatch"])
 @pytest.mark.os(["alinux"])
 @pytest.mark.usefixtures("region", "os", "instance")
-def test_efs_same_az(scheduler, pcluster_config_reader, clusters_factory):
+def test_efs_same_az(region, scheduler, pcluster_config_reader, clusters_factory, vpc_stacks):
     """
     Test when compute subnet is in the same AZ as master subnet.
 
     No compute mount point needed and the efs correctly mounted on compute.
     """
+    _assert_subnet_az_relations(region, vpc_stacks, expected_in_same_az=True)
     mount_dir = "efs_mount_dir"
     cluster_config = pcluster_config_reader(mount_dir=mount_dir)
     cluster = clusters_factory(cluster_config)
@@ -82,3 +86,15 @@ def _test_efs_correctly_mounted(remote_command_executor, mount_dir):
             r"timeo=30,retrans=2,noresvport,_netdev 0 0"
         ).format(mount_dir=mount_dir)
     )
+
+
+def _assert_subnet_az_relations(region, vpc_stacks, expected_in_same_az):
+    vpc = get_vpc_snakecase_value(region, vpc_stacks)
+    master_subnet_id = vpc["public_subnet_id"]
+    compute_subnet_id = vpc["private_subnet_id"] if expected_in_same_az else vpc["private_additional_cidr_subnet_id"]
+    master_subnet_az = boto3.resource("ec2", region_name=region).Subnet(master_subnet_id).availability_zone
+    compute_subnet_az = boto3.resource("ec2", region_name=region).Subnet(compute_subnet_id).availability_zone
+    if expected_in_same_az:
+        assert_that(master_subnet_az).is_equal_to(compute_subnet_az)
+    else:
+        assert_that(master_subnet_az).is_not_equal_to(compute_subnet_az)
