d40d34d9026572fa09fa265fd61107d7179ddccd
Luca Carrogu <carrogu@amazon.com>
2020-05-04 11:08:12 +0200

    Add test for createami command and cluster creation with bake at runtime
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

---
+++ tests/integration-tests/tests/createami/test_createami.py
@@ -0,0 +1,63 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+import pytest
+
+from assertpy import assert_that
+from packaging import version
+from tests.common.utils import retrieve_latest_ami
+from utils import run_command
+
+
+@pytest.fixture()
+def vpc_stack(vpc_stacks, region):
+    return vpc_stacks[region]
+
+
+@pytest.mark.dimensions("eu-west-1", "c5.xlarge", "alinux", "*")
+@pytest.mark.dimensions("us-west-1", "c5.xlarge", "alinux2", "*")
+@pytest.mark.dimensions("us-west-2", "c5.xlarge", "centos7", "*")
+@pytest.mark.dimensions("us-gov-east-1", "c5.xlarge", "ubuntu1604", "*")
+@pytest.mark.dimensions("us-gov-west-1", "c5.xlarge", "ubuntu1804", "*")
+@pytest.mark.dimensions("cn-northwest-1", "c5.xlarge", "alinux2", "*")
+def test_createami(region, os, instance, request, pcluster_config_reader, vpc_stack):
+    """Test createami for given region and os"""
+    cluster_config = pcluster_config_reader()
+
+    # Get base AMI
+    base_ami = retrieve_latest_ami(region, os, ami_type="remarkable")
+
+    # Networking
+    vpc_id = vpc_stack.cfn_outputs["VpcId"]
+    # Private Networking
+    networking_args = ["--vpc-id", vpc_id, "--subnet-id", vpc_stack.cfn_outputs["PrivateSubnetId"], "--no-public-ip"]
+    # Public Networking
+    # networking_args = ["--vpc-id", vpc_id, "--subnet-id", vpc_stack.cfn_outputs["PublicSubnetId"]]
+
+    # Custom Cookbook
+    custom_cookbook = request.config.getoption("custom_chef_cookbook")
+    custom_cookbook_args = [] if not custom_cookbook else ["-cc", custom_cookbook]
+
+    # Instance type
+    pcluster_version_result = run_command(["pcluster", "version"])
+    instance_args = (
+        [] if version.parse(pcluster_version_result.stdout.strip()) < version.parse("2.4.1") else ["-i", instance]
+    )
+
+    pcluster_createami_result = run_command(
+        ["pcluster", "createami", "-ai", base_ami, "-os", os, "-r", region, "-c", cluster_config.as_posix()]
+        + custom_cookbook_args
+        + instance_args
+        + networking_args
+    )
+
+    assert_that(pcluster_createami_result.stdout).does_not_contain("No custom AMI created")
