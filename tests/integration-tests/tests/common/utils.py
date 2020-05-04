7930ef2cbcfcad141f04be1756792371600fa10d
Luca Carrogu <carrogu@amazon.com>
2020-05-04 11:08:12 +0200

    Add test for createami command and cluster creation with bake at runtime
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/tests/common/utils.py
+++ tests/integration-tests/tests/common/utils.py
@@ -14,6 +14,48 @@ import logging
 import boto3
 from retrying import retry
 
+OS_TO_OFFICIAL_AMI_NAME_OWNER_MAP = {
+    "alinux": {"name": "amzn-ami-hvm-*.*.*.*-x86_64-gp2", "owners": ["amazon"]},
+    "alinux2": {"name": "amzn2-ami-hvm-*.*.*.*-x86_64-gp2", "owners": ["amazon"]},
+    "centos7": {"name": "CentOS Linux 7 x86_64 HVM EBS ENA *", "owners": ["410186602215"]},
+    "ubuntu1404": {
+        "name": "ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*",
+        "owners": ["099720109477", "513442679011", "837727238323"],
+    },
+    "ubuntu1604": {
+        "name": "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*",
+        "owners": ["099720109477", "513442679011", "837727238323"],
+    },
+    "ubuntu1804": {
+        "name": "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*",
+        "owners": ["099720109477", "513442679011", "837727238323"],
+    },
+}
+
+OS_TO_REMARKABLE_AMI_NAME_OWNER_MAP = {
+    "alinux": {"name": "Deep Learning Base AMI (Amazon Linux)*", "owners": ["amazon"]},
+    "alinux2": {"name": "Deep Learning Base AMI (Amazon Linux 2)*", "owners": ["amazon"]},
+    "centos7": {"name": "FPGA Developer AMI*", "owners": ["679593333241"]},
+    "ubuntu1604": {"name": "Deep Learning Base AMI (Ubuntu 16.04)*", "owners": ["amazon"]},
+    "ubuntu1804": {"name": "Deep Learning Base AMI (Ubuntu 18.04)*", "owners": ["amazon"]},
+}
+
+AMI_TYPE_DICT = {
+    "official": OS_TO_OFFICIAL_AMI_NAME_OWNER_MAP,
+    "remarkable": OS_TO_REMARKABLE_AMI_NAME_OWNER_MAP,
+}
+
+
+def retrieve_latest_ami(region, os, ami_type="official"):
+    ec2_client = boto3.client("ec2", region_name=region)
+    response = ec2_client.describe_images(
+        Filters=[{"Name": "name", "Values": [AMI_TYPE_DICT[ami_type][os]["name"]]}],
+        Owners=AMI_TYPE_DICT[ami_type][os]["owners"],
+    )
+    # Sort on Creation date Desc
+    amis = sorted(response["Images"], key=lambda x: x["CreationDate"], reverse=True)
+    return amis[0]["ImageId"]
+
 
 @retry(stop_max_attempt_number=3, wait_fixed=5000)
 def fetch_instance_slots(region, instance_type, slots="vcpus"):
---
+++ tests/integration-tests/tests/runtime_bake/test_runtime_bake.py
@@ -0,0 +1,35 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+import pytest
+
+from remote_command_executor import RemoteCommandExecutor
+from tests.common.schedulers_common import get_scheduler_commands
+from tests.common.utils import retrieve_latest_ami
+
+
+@pytest.mark.dimensions("eu-west-2", "c5.xlarge", "alinux", "slurm")
+@pytest.mark.dimensions("us-west-3", "c5.xlarge", "alinux2", "torque")
+@pytest.mark.dimensions("us-east-2", "c5.xlarge", "centos7", "sge")
+@pytest.mark.dimensions("us-gov-east-1", "c5.xlarge", "ubuntu1604", "slurm")
+@pytest.mark.dimensions("us-gov-west-1", "c5.xlarge", "ubuntu1804", "sge")
+@pytest.mark.usefixtures("instance", "scheduler")
+def test_runtime_bake(scheduler, os, region, pcluster_config_reader, clusters_factory):
+    """Test cluster creation with runtime bake."""
+    cluster_config = pcluster_config_reader(custom_ami=retrieve_latest_ami(region, os, ami_type="remarkable"))
+    cluster = clusters_factory(cluster_config)
+    remote_command_executor = RemoteCommandExecutor(cluster)
+    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
+    result = scheduler_commands.submit_command("sleep 5")
+    job_id = scheduler_commands.assert_job_submitted(result.stdout)
+    scheduler_commands.wait_job_completed(job_id)
+    scheduler_commands.assert_job_succeeded(job_id)
