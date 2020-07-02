c71f37fde0992ea4760324a02211ee5bd412d025
Tim Lane <tilne@amazon.com>
2020-07-02 16:39:43 -0700

    Remove logic for handling lack of OMPI modules
    Open MPI environment modules are configured on all of the OSs we
    support.
    
    Signed-off-by: Tim Lane <tilne@amazon.com>

--- tests/integration-tests/tests/scaling/test_mpi/test_mpi_ssh/mpi_ssh.sh
+++ tests/integration-tests/tests/scaling/test_mpi/test_mpi_ssh/mpi_ssh.sh
@@ -4,7 +4,5 @@ set -e
 _openmpi_module=$1
 _remote_host=$2
 
-if [ "${_openmpi_module}" != "no_module_available" ]; then
-  module load ${_openmpi_module}
-fi
+module load ${_openmpi_module}
 $(which mpirun) --host ${_remote_host} hostname
