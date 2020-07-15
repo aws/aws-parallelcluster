896533a361bf64db40ed7841ab67f2a619e51595
Siddharth Salot <sssalot@amazon.com>
2020-07-15 13:32:21 -0400

    Add Integration tests for FSx Lustre Backup feature

--- cli/pcluster/config/mappings.py
+++ cli/pcluster/config/mappings.py
@@ -472,18 +472,18 @@ FSX = {
             }),
             ("daily_automatic_backup_start_time", {
                 "allowed_values": r"NONE|^([01]\d|2[0-3]):([0-5]\d)$",
-                "update_policy": UpdatePolicy.SUPPORTED
+                "update_policy": UpdatePolicy.UNSUPPORTED
             }),
             ("automatic_backup_retention_days", {
                 "type": IntParam,
                 "allowed_values": "^(3[0-5]|[0-2][0-9]|[0-9])$",
-                "update_policy": UpdatePolicy.SUPPORTED
+                "update_policy": UpdatePolicy.UNSUPPORTED
             }),
             ("copy_tags_to_backups", {
                 "type": BoolParam,
-                "update_policy": UpdatePolicy.SUPPORTED
+                "update_policy": UpdatePolicy.UNSUPPORTED
             }),
-            ("backup_id", {
+            ("fsx_backup_id", {
                 "validators": [fsx_lustre_backup_validator],
                 "allowed_values": "^(backup-[0-9a-f]{8,})$",
                 "update_policy": UpdatePolicy.UNSUPPORTED
