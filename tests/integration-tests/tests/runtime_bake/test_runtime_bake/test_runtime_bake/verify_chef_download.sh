99c9d1c1f9362e5fd1d0d577defb007b869ae89e
Luca Carrogu <carrogu@amazon.com>
2020-07-20 15:12:31 +0200

    Enable "runtime bake" tests
    Test is checking if, during runtime bake, the Cinc installer is downloaded from PCluster S3 buckets
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/tests/runtime_bake/test_runtime_bake/test_runtime_bake/verify_chef_download.sh
+++ tests/integration-tests/tests/runtime_bake/test_runtime_bake/test_runtime_bake/verify_chef_download.sh
@@ -13,9 +13,24 @@ if [ $? -eq 0 ]; then
     exit 1
 fi
 
-# Verify chef client is installed from S3
-sudo grep -ir "aws-parallelcluster/archives/chef/chef-install.sh" /var/log/cloud-init-output.log
+# Verify no chef client is installed from S3
+sudo grep -iEr "aws-parallelcluster.*/archives/chef/chef-install.sh" /var/log/cloud-init-output.log
+if [ $? -eq 0 ]; then
+    echo "Chef installer downloaded from S3"
+    exit 1
+fi
+
+
+# Verify cinc.sh is not called to download cinc installer script or client
+sudo grep -ir "omnitruck.cinc.sh/install.sh" /var/log/cloud-init-output.log
+if [ $? -eq 0 ]; then
+    echo "Cinc installer downloaded from cinc.sh"
+    exit 1
+fi
+
+# Verify cinc client is installed from S3
+sudo grep -iEr "aws-parallelcluster.*/archives/cinc/cinc-install.sh" /var/log/cloud-init-output.log
 if [ $? -ne 0 ]; then
-    echo "Chef installer not downloaded from S3"
+    echo "Cinc installer NOT downloaded from S3"
     exit 1
 fi
