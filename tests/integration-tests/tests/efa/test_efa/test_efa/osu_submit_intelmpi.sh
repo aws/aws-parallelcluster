1e52032e7b5104da21158bf54585c58fa70c8077
Francesco De Martino <fdm@amazon.com>
2020-07-29 12:51:07 +0200

    Embedding osu_benchmarks in integration tests resources
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- tests/integration-tests/tests/efa/test_efa/test_efa/osu_submit_intelmpi.sh
+++ tests/integration-tests/tests/efa/test_efa/test_efa/osu_submit_intelmpi.sh
@@ -2,4 +2,4 @@
 set -e
 
 module load intelmpi
-mpirun -np 2 -rr --map-by ppr:1:node /shared/intelmpi/osu-micro-benchmarks-5.4/mpi/pt2pt/osu_latency > /shared/osu.out
+mpirun -np 2 -rr --map-by ppr:1:node /shared/intelmpi/osu-micro-benchmarks-5.6.3/mpi/pt2pt/osu_latency > /shared/osu.out
--- tests/integration-tests/tests/efa/test_efa/test_efa/osu_submit_openmpi.sh
+++ tests/integration-tests/tests/efa/test_efa/test_efa/osu_submit_openmpi.sh
@@ -2,4 +2,4 @@
 set -e
 
 module load openmpi
-mpirun --map-by ppr:1:node /shared/openmpi/osu-micro-benchmarks-5.4/mpi/pt2pt/osu_latency > /shared/osu.out
+mpirun --map-by ppr:1:node /shared/openmpi/osu-micro-benchmarks-5.6.3/mpi/pt2pt/osu_latency > /shared/osu.out
