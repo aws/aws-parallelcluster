25f6844b9d9ad0868f59fe685f88f7464ea6dac4
Alexandre Gobeaux <gobeaa@amazon.com>
2020-08-03 15:47:05 +0200

    Create automatic Master CloudWatch dashboard with unconditional metrics
    Previously, no CloudWatch dashboard was created when launching a cluster. Now, a dashboard is created in the CloudFormation stack. This dashboard contains graphs about the Master's metrics. Those metrics concern the EC2, EBS, RAID, EFS and FSx products. The provided graphs are only about the metrics that are always present when using one of those solutions (for example, the EBS BurstBalance metric depends on the volume type and is thus not yet included).
    
    Signed-off-by: Alexandre Gobeaux <gobeaa@amazon.com>

--- util/uploadTemplate.sh
+++ util/uploadTemplate.sh
@@ -102,10 +102,11 @@ main() {
     sed -i "s#.*aws-parallelcluster.*/templates/fsx-substack-\${version}.cfn.json.*#\"https://${_s3_folder_url}/fsx-substack.cfn.json\",#" ${_temp_dir}/aws-parallelcluster.cfn.json
     sed -i "s#.*aws-parallelcluster.*/templates/batch-substack-\${version}.cfn.json.*#\"https://${_s3_folder_url}/batch-substack.cfn.json\",#" ${_temp_dir}/aws-parallelcluster.cfn.json
     sed -i "s#.*aws-parallelcluster.*/templates/cw-logs-substack-\${version}.cfn.json.*#\"https://${_s3_folder_url}/cw-logs-substack.cfn.json\",#" ${_temp_dir}/aws-parallelcluster.cfn.json
+    sed -i "s#.*aws-parallelcluster.*/templates/cw-dashboard-substack-\${version}.cfn.yaml.*#\"https://${_s3_folder_url}/cw-dashboard-substack.cfn.yaml\",#" ${_temp_dir}/aws-parallelcluster.cfn.json
 
     # upload templates
     aws ${_profile} --region "${_region}" s3 cp --acl public-read ${_temp_dir}/aws-parallelcluster.cfn.json s3://${_bucket}/${_templates_folder}/ || _error_exit 'Failed to push cloudformation template to S3'
-    aws ${_profile} --region "${_region}" s3 cp --acl public-read --recursive --exclude "*" --include "*substack.cfn.json" ${_srcdir}/cloudformation/ s3://${_bucket}/${_templates_folder}/ || _error_exit 'Failed to push substack cfn templates to S3'
+    aws ${_profile} --region "${_region}" s3 cp --acl public-read --recursive --exclude "*" --include "*substack.cfn.json" --include "*substack.cfn.yaml" ${_srcdir}/cloudformation/ s3://${_bucket}/${_templates_folder}/ || _error_exit 'Failed to push substack cfn templates to S3'
 
     echo ""
     echo "Done. Add the following variables to the pcluster config file, under the [cluster ...] section"
