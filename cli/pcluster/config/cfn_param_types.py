aws/aws-parallelcluster

commit f93201f0d4db4bb0ff518dc720bf4a13ee0184ec

Introduce support for dynamic configuration models
This commit splits the "cluster" section mappings in two different definitions, each one dedicated to its corresponding cluster model.

A "ClusterModel" class has been introduced to infer and map the "model" of a cluster configuration.
Supported cluster models are:
- SIT: Single Instance Type
- HIT: Heterogeneous Instance Types

When a pcluster_config instance is loaded from a config file or from cfn params, its target cluster model is first inferred; then the configuration is loaded and validated accordingly to this model.

An important change introduced by this patch is that the number of vcpus per instance type is now retrieved directly from AWS APIs rather instead of using instances.json.

Signed-off-by: ddeidda <ddeidda@amazon.com>

cli/pcluster/config/cfn_param_types.py
@@ -641,7 +641,7 @@ class DisableHyperThreadingCfnParam(BoolCfnParam):
             cfn_converter = self.definition.get("cfn_param_mapping", None)
             if cfn_converter and cfn_params:
                 cores = get_cfn_param(cfn_params, cfn_converter)
-                if cores and cores != "NONE":
+                if cores and cores != "NONE,NONE":
                     cores = int(cores.split(",")[0])
                     self.value = cores > 0
         except (ValueError, IndexError):
@@ -655,15 +655,20 @@ class DisableHyperThreadingCfnParam(BoolCfnParam):
 
         :return: string (cores_master,cores_compute)
         """
-        cfn_params = {self.definition.get("cfn_param_mapping"): "-1,-1"}
+        cfn_params = {self.definition.get("cfn_param_mapping"): "NONE,NONE"}
 
         cluster_config = self.pcluster_config.get_section(self.section_key)
-        if cluster_config.get_param_value("disable_hyperthreading"):
+        if self.value:
             master_instance_type = cluster_config.get_param_value("master_instance_type")
-            compute_instance_type = cluster_config.get_param_value("compute_instance_type")
-
             master_cores = get_instance_vcpus(self.pcluster_config.region, master_instance_type) // 2
-            compute_cores = get_instance_vcpus(self.pcluster_config.region, compute_instance_type) // 2
+
+            if self.pcluster_config.cluster_model.name == "SIT":
+                # compute_instance_type parameter is valid only in SIT clusters
+                compute_instance_type = cluster_config.get_param_value("compute_instance_type")
+                compute_cores = get_instance_vcpus(self.pcluster_config.region, compute_instance_type) // 2
+            else:
+                compute_instance_type = None
+                compute_cores = 0
 
             if master_cores < 0 or compute_cores < 0:
                 self.pcluster_config.error(

cli/pcluster/config/json_param_types.py
@@ -95,52 +95,6 @@ class FloatJsonParam(JsonParam):
         return config_parser.getfloat(section_name, self.key)
 
 
-class ComputeInstanceTypeJsonParam(JsonParam):
-    """JsonParam to manage compute instance type in compute_resource sections."""
-
-    def refresh(self):
-        """
-        Populate additional settings needed for the linked compute resource like vcpus, gpus etc.
-
-        These parameters are set according to queue settings and instance type capabilities.
-        """
-        if self.value:
-            instance_type = utils.get_instance_type(self.value)
-
-            # Set vcpus according to queue's disable_hyperthreading and instance features
-            queue_section = self.owner_section.parent_section
-            ht_disabled = queue_section.get_param_value("disable_hyperthreading") if queue_section else None
-            vcpus_info = instance_type.get("VCpuInfo")
-            default_cores = vcpus_info.get("DefaultCores")
-            vcpus = default_cores if ht_disabled and default_cores else vcpus_info.get("DefaultVCpus")
-            self.owner_section.get_param("vcpus").value = vcpus
-
-            # Set gpus according to instance features
-            gpu_info = instance_type.get("GpuInfo", None)
-            if gpu_info:
-                # Currently adding up all gpus. To be reviewed if the case of heterogeneous GPUs arises.
-                self.owner_section.get_param("gpus").value = sum([gpus.get("Count") for gpus in gpu_info.get("Gpus")])
-
-            # Set enable_efa according to queues' enable_efa and instance features
-            enable_efa = queue_section.get_param_value("enable_efa") if queue_section else None
-            self.owner_section.get_param("enable_efa").value = enable_efa and instance_type.get("NetworkInfo").get(
-                "EfaSupported"
-            )
-
-            # print warnings if any configuration fallback has occurred
-            if ht_disabled and not default_cores:
-                self.pcluster_config.warn(
-                    "Hyperthreading was disabled on queue '{0}', but disabling hyperthreading on instance type '{1}' "
-                    "is not currently supported by ParallelCluster.".format(queue_section.label, self.value)
-                )
-
-            if enable_efa and not self.owner_section.get_param("enable_efa").value:
-                self.pcluster_config.warn(
-                    "EFA was enabled on queue '{0}', but instance type '{1}' "
-                    "does not support EFA.".format(queue_section.label, self.value)
-                )
-
-
 class ScaleDownIdleTimeJsonParam(JsonParam):
     """JsonParam to manage scaledown_idletime for Json configuration."""
 
@@ -250,6 +204,83 @@ class JsonSection(Section):
     def get_default_param_type(self):
         """Get the default Param type managed by the Section type."""
         return JsonParam
+
+    def refresh(self):
+        """Refresh the Json section."""
+        self.refresh_section()
+        super(JsonSection, self).refresh()
+
+    def refresh_section(self):
+        """Perform custom refresh operations."""
+        pass
+
+
+class QueueJsonSection(JsonSection):
+    """JSon Section for queues."""
+
+    def refresh_section(self):
+        """Take values of disable_hyperthreading and enable_efa from cluster section if not specified."""
+        if self.get_param_value("disable_hyperthreading") is None:
+            self.get_param("disable_hyperthreading").value = self.pcluster_config.get_section(
+                "cluster"
+            ).get_param_value("disable_hyperthreading")
+
+        if self.get_param_value("enable_efa") is None:
+            cluster_enable_efa = self.pcluster_config.get_section("cluster").get_param_value("enable_efa")
+            # enable_efa is of string type in cluster section and of bool type in queue section
+            if cluster_enable_efa is not None:
+                self.get_param("enable_efa").value = cluster_enable_efa == "compute"
+
+        compute_resource_labels = self.get_param_value("compute_resource_settings")
+        if compute_resource_labels:
+            for compute_resource_label in compute_resource_labels.split(","):
+                compute_resource_section = self.pcluster_config.get_section("compute_resource", compute_resource_label)
+                self.refresh_compute_resource(compute_resource_section)
+
+    def refresh_compute_resource(self, compute_resource_section):
+        """
+        Populate additional settings needed for the linked compute resource like vcpus, gpus etc.
+
+        These parameters are set according to queue settings and instance type capabilities.
+        """
+        instance_type_param = compute_resource_section.get_param("instance_type")
+
+        if instance_type_param.value:
+            instance_type = utils.get_instance_type(instance_type_param.value)
+
+            # Set vcpus according to queue's disable_hyperthreading and instance features
+            ht_disabled = self.get_param_value("disable_hyperthreading")
+            vcpus_info = instance_type.get("VCpuInfo")
+            default_cores = vcpus_info.get("DefaultCores")
+            vcpus = default_cores if ht_disabled and default_cores else vcpus_info.get("DefaultVCpus")
+            compute_resource_section.get_param("vcpus").value = vcpus
+
+            # Set gpus according to instance features
+            gpu_info = instance_type.get("GpuInfo", None)
+            if gpu_info:
+                # Currently adding up all gpus. To be reviewed if the case of heterogeneous GPUs arises.
+                compute_resource_section.get_param("gpus").value = sum(
+                    [gpus.get("Count") for gpus in gpu_info.get("Gpus")]
+                )
+
+            # Set enable_efa according to queues' enable_efa and instance features
+            enable_efa = self.get_param_value("enable_efa")
+            compute_resource_section.get_param("enable_efa").value = enable_efa and instance_type.get(
+                "NetworkInfo"
+            ).get("EfaSupported")
+
+            # Printing warnings here to spare additional boto3 calls from validation.
+            if ht_disabled and not default_cores:
+                self.pcluster_config.warn(
+                    "Hyperthreading was disabled on queue '{0}', but disabling hyperthreading on instance type '{1}' "
+                    "is not currently supported by ParallelCluster.".format(self.label, instance_type_param.value)
+                )
+
+            if enable_efa and not instance_type_param.owner_section.get_param("enable_efa").value:
+                self.pcluster_config.warn(
+                    "EFA was enabled on queue '{0}', but instance type '{1}' "
+                    "does not support EFA.".format(self.label, instance_type_param.value)
+                )
 
 
 # ---------------------- Common functions ---------------------- #

cli/pcluster/config/validators.py
@@ -404,11 +404,15 @@ def efa_validator(param_key, param_value, pcluster_config):
     cluster_section = pcluster_config.get_section("cluster")
 
     allowed_instances = _get_efa_enabled_instance_types(errors)
-    if cluster_section.get_param_value("compute_instance_type") not in allowed_instances:
-        errors.append(
-            "When using 'enable_efa = {0}' it is required to set the 'compute_instance_type' parameter "
-            "to one of the following values : {1}".format(param_value, allowed_instances)
-        )
+    if pcluster_config.cluster_model.name == "SIT":
+        # Specific validations for SIT clusters
+        if cluster_section.get_param_value("compute_instance_type") not in allowed_instances:
+            errors.append(
+                "When using 'enable_efa = {0}' it is required to set the 'compute_instance_type' parameter "
+                "to one of the following values : {1}".format(param_value, allowed_instances)
+            )
+        if cluster_section.get_param_value("placement_group") is None:
+            warnings.append("You may see better performance using a cluster placement group.")
 
     allowed_oses = ["alinux", "alinux2", "centos7", "ubuntu1604", "ubuntu1804"]
     if cluster_section.get_param_value("base_os") not in allowed_oses:
@@ -423,9 +427,6 @@ def efa_validator(param_key, param_value, pcluster_config):
             "When using 'enable_efa = {0}' it is required to set the 'scheduler' parameter "
             "to one of the following values : {1}".format(param_value, allowed_schedulers)
         )
-
-    if cluster_section.get_param_value("placement_group") is None:
-        warnings.append("You may see better performance using a cluster placement group.")
 
     _validate_efa_sg(pcluster_config, errors)
 
@@ -969,7 +970,7 @@ def compute_instance_type_validator(param_key, param_value, pcluster_config):
         if "," not in param_value and "." in param_value:
             # if the type is not a list, and contains dot (nor optimal, nor a family)
             # validate instance type against max_vcpus limit
-            vcpus = get_instance_vcpus(pcluster_config.region, param_value)
+            vcpus = get_instance_vcpus(param_value)
             if vcpus <= 0:
                 warnings.append(
                     "Unable to get the number of vcpus for the compute_instance_type '{0}'. "
@@ -1077,8 +1078,22 @@ def queue_settings_validator(param_key, param_value, pcluster_config):
 
 def queue_validator(section_key, section_label, pcluster_config):
     errors = []
-    section = pcluster_config.get_section(section_key, section_label)
-    compute_resource_labels = str(section.get_param_value("compute_resource_settings") or "").split(",")
+    warnings = []
+    queue_section = pcluster_config.get_section(section_key, section_label)
+    compute_resource_labels = str(queue_section.get_param_value("compute_resource_settings") or "").split(",")
+
+    def check_queue_xor_cluster(param_key):
+        cluster_section_value = pcluster_config.get_section("cluster").get_param_value(param_key)
+        param_value = queue_section.get_param_value(param_key)
+        if param_value is not None:
+            if cluster_section_value is not None:
+                errors.append("Parameter '{0}' can be used only in 'cluster' or in 'queue' section".format(param_key))
+        else:
+            if cluster_section_value is None:
+                errors.append("Parameter '{0}' must be specified in 'cluster' or in 'queue' section".format(param_key))
+
+    check_queue_xor_cluster("enable_efa")
+    check_queue_xor_cluster("disable_hyperthreading")
 
     instance_types = []
     for compute_resource_label in compute_resource_labels:
@@ -1094,7 +1109,14 @@ def queue_validator(section_key, section_label, pcluster_config):
                 )
             else:
                 instance_types.append(instance_type)
-    return errors, []
+
+            enable_efa = compute_resource.get_param_value("enable_efa")
+            if enable_efa and not compute_resource.get_param("enable_efa").value:
+                warnings.append(
+                    "EFA was enabled on queue '{0}', but instance type '{1}' "
+                    "does not support EFA.".format(queue_section.label, instance_type)
+                )
+    return errors, warnings
 
 
 def settings_validator(param_key, param_value, pcluster_config):

cli/pcluster/utils.py
@@ -259,19 +259,19 @@ def get_supported_features(region, feature):
     return supported_features
 
 
-def get_instance_vcpus(region, instance_type):
+def get_instance_vcpus(instance_type):
     """
     Get number of vcpus for the given instance type.
 
     :param region: AWS Region
     :param instance_type: the instance type to search for.
     :return: the number of vcpus or -1 if the instance type cannot be found
-    or the pricing file cannot be retrieved/parsed
     """
     try:
-        instances = _get_json_from_s3(region, "instances/instances.json")
-        vcpus = int(instances[instance_type]["vcpus"])
-    except (KeyError, ValueError, ClientError):
+        instance_type_info = get_instance_type(instance_type)
+        vcpus_info = instance_type_info.get("VCpuInfo")
+        vcpus = vcpus_info.get("DefaultVCpus")
+    except (ClientError):
         vcpus = -1
 
     return vcpus

cli/tests/pcluster/config/test_json_param_types/test_config_to_json/pcluster.config.ini
@@ -3,6 +3,8 @@ cluster_template = default
 
 [cluster default]
 queue_settings = {{queue_settings}}
+enable_efa = compute
+disable_hyperthreading = false
 
 [queue queue1]
 compute_resource_settings = q1-i1,q1-i2,q1-i3
@@ -37,3 +39,16 @@ spot_price = 0.5
 [compute_resource q2-i3]
 instance_type = i3en.24xlarge
 spot_price = 0.6
+
+[queue queue3]
+compute_resource_settings = q3-i1
+compute_type = spot
+# enable_efa should be inherited from cluster section
+# enable_efa = false
+# disable_hyperthreading should be inherited from cluster section
+# disable_hyperthreading = false
+placement_group = DYNAMIC
+
+[compute_resource q3-i1]
+instance_type = i3en.24xlarge
+spot_price = 0.4

commit b6ee98d19ba2c6aaeca87ffd90d2cee979123e22

Restore default values for disable_hyperthreading and enable_efa
This commit fixes a regression introduced in https://github.com/aws/aws-parallelcluster/pull/1928, which made these parameters mandatory either in the cluster or in queue sections.

The new behavior allows configuration files to omit these parameter, with the meaning of setting them to false.
The validation checks that the parameters are specified only in cluster or in queue section, or that at least their valuea are the same in both sections.

Signed-off-by: ddeidda <ddeidda@amazon.com>

cli/pcluster/config/cfn_param_types.py
@@ -15,15 +15,7 @@ from collections import OrderedDict
 import yaml
 
 from pcluster.config.iam_policy_rules import AWSBatchFullAccessInclusionRule, CloudWatchAgentServerPolicyInclusionRule
-from pcluster.config.param_types import (
-    LOGGER,
-    Param,
-    Section,
-    SettingsParam,
-    StorageData,
-    _ensure_section_existence,
-    get_file_section_name,
-)
+from pcluster.config.param_types import LOGGER, Param, Section, SettingsParam, StorageData, _ensure_section_existence
 from pcluster.config.resource_map import ResourceMap
 from pcluster.constants import PCLUSTER_ISSUES_LINK
 from pcluster.utils import (
@@ -31,6 +23,7 @@ from pcluster.utils import (
     get_avail_zone,
     get_cfn_param,
     get_efs_mount_target_id,
+    get_file_section_name,
     get_instance_vcpus,
     get_supported_architectures_for_instance_type,
 )
@@ -660,12 +653,12 @@ class DisableHyperThreadingCfnParam(BoolCfnParam):
         cluster_config = self.pcluster_config.get_section(self.section_key)
         if self.value:
             master_instance_type = cluster_config.get_param_value("master_instance_type")
-            master_cores = get_instance_vcpus(self.pcluster_config.region, master_instance_type) // 2
+            master_cores = get_instance_vcpus(master_instance_type) // 2
 
             if self.pcluster_config.cluster_model.name == "SIT":
                 # compute_instance_type parameter is valid only in SIT clusters
                 compute_instance_type = cluster_config.get_param_value("compute_instance_type")
-                compute_cores = get_instance_vcpus(self.pcluster_config.region, compute_instance_type) // 2
+                compute_cores = get_instance_vcpus(compute_instance_type) // 2
             else:
                 compute_instance_type = None
                 compute_cores = 0

cli/pcluster/config/config_patch.py
@@ -16,8 +16,8 @@ from collections import namedtuple
 
 # Represents a single parameter change in a ConfigPatch instance
 from pcluster import utils
-from pcluster.config.param_types import get_file_section_name
 from pcluster.config.update_policy import UpdatePolicy
+from pcluster.utils import get_file_section_name
 
 Change = namedtuple("Change", ["section_key", "section_label", "param_key", "old_value", "new_value", "update_policy"])
 

cli/pcluster/config/json_param_types.py
@@ -11,10 +11,11 @@
 from collections import OrderedDict
 
 from pcluster import utils
-from pcluster.config.param_types import Param, Section, SettingsParam, get_file_section_name
-
+from pcluster.config.param_types import Param, Section, SettingsParam
 
 # ---------------------- Params ---------------------- #
+
+
 class JsonParam(Param):
     """Base class to manage configuration parameters stored in Json format."""
 
@@ -24,7 +25,7 @@ class JsonParam(Param):
 
     def from_file(self, config_parser):
         """Load the param value from configuration file."""
-        section_name = get_file_section_name(self.section_key, self.section_label)
+        section_name = utils.get_file_section_name(self.section_key, self.section_label)
         if config_parser.has_option(section_name, self.key):
             try:
                 self.value = self._parse_value(config_parser, section_name)
@@ -221,15 +222,18 @@ class QueueJsonSection(JsonSection):
     def refresh_section(self):
         """Take values of disable_hyperthreading and enable_efa from cluster section if not specified."""
         if self.get_param_value("disable_hyperthreading") is None:
-            self.get_param("disable_hyperthreading").value = self.pcluster_config.get_section(
-                "cluster"
-            ).get_param_value("disable_hyperthreading")
+            cluster_disable_hyperthreading = self.pcluster_config.get_section("cluster").get_param_value(
+                "disable_hyperthreading"
+            )
+            # None value at cluster level is converted to False at queue level
+            self.get_param("disable_hyperthreading").value = cluster_disable_hyperthreading is True
 
         if self.get_param_value("enable_efa") is None:
             cluster_enable_efa = self.pcluster_config.get_section("cluster").get_param_value("enable_efa")
-            # enable_efa is of string type in cluster section and of bool type in queue section
-            if cluster_enable_efa is not None:
-                self.get_param("enable_efa").value = cluster_enable_efa == "compute"
+
+            # enable_efa is of string type in cluster section and of bool type in queue section.
+            # None value at cluster level is converted to False at queue level
+            self.get_param("enable_efa").value = cluster_enable_efa == "compute"
 
         compute_resource_labels = self.get_param_value("compute_resource_settings")
         if compute_resource_labels:

cli/pcluster/config/param_types.py
@@ -19,6 +19,7 @@ from configparser import NoSectionError
 
 from pcluster.config.update_policy import UpdatePolicy
 from pcluster.config.validators import settings_validator
+from pcluster.utils import get_file_section_name
 
 LOGGER = logging.getLogger(__name__)
 
@@ -589,10 +590,6 @@ class Section(ABC):
 
 
 # ---------------------- Common functions ---------------------- #
-def get_file_section_name(section_key, section_label=None):
-    return section_key + (" {0}".format(section_label) if section_label else "")
-
-
 def _ensure_section_existence(config_parser, section_name):
     """Add a section to the config_parser if not present."""
     if not config_parser.has_section(section_name):

cli/pcluster/config/validators.py
@@ -23,6 +23,7 @@ from pcluster.utils import (
     ellipsize,
     get_base_additional_iam_policies,
     get_efs_mount_target_id,
+    get_file_section_name,
     get_instance_vcpus,
     get_partition,
     get_region,
@@ -1083,14 +1084,19 @@ def queue_validator(section_key, section_label, pcluster_config):
     compute_resource_labels = str(queue_section.get_param_value("compute_resource_settings") or "").split(",")
 
     def check_queue_xor_cluster(param_key):
-        cluster_section_value = pcluster_config.get_section("cluster").get_param_value(param_key)
-        param_value = queue_section.get_param_value(param_key)
-        if param_value is not None:
-            if cluster_section_value is not None:
+        """Check that the param is not used in both queue and cluster section."""
+        # FIXME: Improve the design of the validation mechanism to allow validators to be linked to a specific
+        # validation phase (before, after refresh operations)
+        config_parser = pcluster_config.config_parser
+        if config_parser:
+            # This check is performed only if the configuration is loaded from file.
+            queue_param_in_config_file = config_parser.has_option(
+                get_file_section_name("queue", section_label), param_key
+            )
+            cluster_param_in_config_file = pcluster_config.get_section("cluster").get_param_value(param_key) is not None
+
+            if cluster_param_in_config_file and queue_param_in_config_file:
                 errors.append("Parameter '{0}' can be used only in 'cluster' or in 'queue' section".format(param_key))
-        else:
-            if cluster_section_value is None:
-                errors.append("Parameter '{0}' must be specified in 'cluster' or in 'queue' section".format(param_key))
 
     check_queue_xor_cluster("enable_efa")
     check_queue_xor_cluster("disable_hyperthreading")

cli/pcluster/utils.py
@@ -263,7 +263,6 @@ def get_instance_vcpus(instance_type):
     """
     Get number of vcpus for the given instance type.
 
-    :param region: AWS Region
     :param instance_type: the instance type to search for.
     :return: the number of vcpus or -1 if the instance type cannot be found
     """
@@ -868,3 +867,8 @@ def get_bucket_url(region):
     return ("https://{region}-aws-parallelcluster.s3.{region}.amazonaws.com{suffix}").format(
         region=region, suffix=s3_suffix
     )
+
+
+def get_file_section_name(section_key, section_label=None):
+    """Build a section name as in the config file, given section key and label."""
+    return section_key + (" {0}".format(section_label) if section_label else "")

cli/tests/pcluster/config/test_json_param_types/test_config_to_json/pcluster.config.ini
@@ -4,7 +4,8 @@ cluster_template = default
 [cluster default]
 queue_settings = {{queue_settings}}
 enable_efa = compute
-disable_hyperthreading = false
+# disable_hyperthreading not defined, fallback to False expected
+# disable_hyperthreading = false
 
 [queue queue1]
 compute_resource_settings = q1-i1,q1-i2,q1-i3
@@ -49,7 +50,7 @@ spot_price = 0.6
 compute_resource_settings = q3-i1
 compute_type = spot
 # enable_efa should be inherited from cluster section
-# enable_efa = false
+# enable_efa = true
 # disable_hyperthreading should be inherited from cluster section
 # disable_hyperthreading = false
 placement_group = DYNAMIC

cli/tests/pcluster/config/test_validators.py
@@ -1525,19 +1525,7 @@ def test_queue_settings_validator(mocker, cluster_section_dict, expected_message
             {"compute_resource_settings": "cr2,cr4", "enable_efa": True, "disable_hyperthreading": True},
             None,
         ),
-        (
-            {"queue_settings": "default"},
-            {"compute_resource_settings": "cr1"},
-            [
-                "Parameter 'enable_efa' must be specified in 'cluster' or in 'queue' section",
-                "Parameter 'disable_hyperthreading' must be specified in 'cluster' or in 'queue' section",
-            ],
-        ),
-        (
-            {"queue_settings": "default", "enable_efa": "compute", "disable_hyperthreading": True},
-            {"compute_resource_settings": "cr1"},
-            None,
-        ),
+        ({"queue_settings": "default"}, {"compute_resource_settings": "cr1"}, None,),
         (
             {"queue_settings": "default", "enable_efa": "compute", "disable_hyperthreading": True},
             {"compute_resource_settings": "cr1", "enable_efa": True, "disable_hyperthreading": True},
@@ -1546,9 +1534,17 @@ def test_queue_settings_validator(mocker, cluster_section_dict, expected_message
                 "Parameter 'disable_hyperthreading' can be used only in 'cluster' or in 'queue' section",
             ],
         ),
+        (
+            {"queue_settings": "default", "enable_efa": "compute", "disable_hyperthreading": True},
+            {"compute_resource_settings": "cr1", "enable_efa": False, "disable_hyperthreading": False},
+            [
+                "Parameter 'enable_efa' can be used only in 'cluster' or in 'queue' section",
+                "Parameter 'disable_hyperthreading' can be used only in 'cluster' or in 'queue' section",
+            ],
+        ),
     ],
 )
-def test_queue_validator(mocker, cluster_dict, queue_dict, expected_messages):
+def test_queue_validator(cluster_dict, queue_dict, expected_messages):
     config_parser_dict = {
         "cluster default": cluster_dict,
         "queue default": queue_dict,