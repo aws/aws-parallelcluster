872e91db6858b85b93dee4b2be1a70c362d48b83
Francesco De Martino <fdm@amazon.com>
2020-08-18 11:26:32 +0200

    Refactor start/stop commands to allow multiple implementations
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cli/pcluster/cli_commands/start.py
+++ cli/pcluster/cli_commands/start.py
@@ -8,8 +8,10 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
+import abc
 import logging
 import sys
+from abc import abstractmethod
 
 import boto3
 from botocore.exceptions import ClientError
@@ -17,27 +19,71 @@ from botocore.exceptions import ClientError
 from pcluster import utils
 from pcluster.config.pcluster_config import PclusterConfig
 
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta("ABC", (), {})
+
 LOGGER = logging.getLogger(__name__)
 
 
 def start(args):
-    """Restore ASG limits or awsbatch CE to min/max/desired."""
-    stack_name = utils.get_stack_name(args.cluster_name)
+    """Start cluster compute fleet."""
     pcluster_config = PclusterConfig(config_file=args.config_file, cluster_name=args.cluster_name, auto_refresh=False)
     cluster_section = pcluster_config.get_section("cluster")
+    scheduler = cluster_section.get_param_value("scheduler")
 
-    if cluster_section.get_param_value("scheduler") == "awsbatch":
+    SCHEDULER_TO_START_COMMAND_MAP[scheduler]().start(args, pcluster_config)
+
+
+class StartCommand(ABC):
+    """Interface to implement start command."""
+
+    @abstractmethod
+    def start(self, args, pcluster_config):
+        """Start the compute fleet."""
+        pass
+
+
+class AWSBatchStartCommand(StartCommand):
+    """Start command for the AWSBatch cluster."""
+
+    def start(self, args, pcluster_config):
+        """Start the compute fleet."""
         LOGGER.info("Enabling AWS Batch compute environment : %s", args.cluster_name)
+        stack_name = utils.get_stack_name(args.cluster_name)
+        cluster_section = pcluster_config.get_section("cluster")
         max_vcpus = cluster_section.get_param_value("max_vcpus")
         desired_vcpus = cluster_section.get_param_value("desired_vcpus")
         min_vcpus = cluster_section.get_param_value("min_vcpus")
         ce_name = utils.get_batch_ce(stack_name)
-        _start_batch_ce(ce_name=ce_name, min_vcpus=min_vcpus, desired_vcpus=desired_vcpus, max_vcpus=max_vcpus)
-    elif cluster_section.get_param_value("scheduler") == "slurm":
-        # TODO: to be implemented
-        raise NotImplementedError()
-    else:
+        self._start_batch_ce(ce_name=ce_name, min_vcpus=min_vcpus, desired_vcpus=desired_vcpus, max_vcpus=max_vcpus)
+
+    @staticmethod
+    def _start_batch_ce(ce_name, min_vcpus, desired_vcpus, max_vcpus):
+        try:
+            boto3.client("batch").update_compute_environment(
+                computeEnvironment=ce_name,
+                state="ENABLED",
+                computeResources={
+                    "minvCpus": int(min_vcpus),
+                    "maxvCpus": int(max_vcpus),
+                    "desiredvCpus": int(desired_vcpus),
+                },
+            )
+        except ClientError as e:
+            LOGGER.critical(e.response.get("Error").get("Message"))
+            sys.exit(1)
+
+
+class SITStartCommand(StartCommand):
+    """Start command for the ASG based clusters."""
+
+    def start(self, args, pcluster_config):
+        """Start the compute fleet."""
         LOGGER.info("Starting compute fleet: %s", args.cluster_name)
+        cluster_section = pcluster_config.get_section("cluster")
+        stack_name = utils.get_stack_name(args.cluster_name)
         max_queue_size = cluster_section.get_param_value("max_queue_size")
         min_desired_size = (
             cluster_section.get_param_value("initial_queue_size")
@@ -48,17 +94,17 @@ def start(args):
         utils.set_asg_limits(asg_name=asg_name, min=min_desired_size, max=max_queue_size, desired=min_desired_size)
 
 
-def _start_batch_ce(ce_name, min_vcpus, desired_vcpus, max_vcpus):
-    try:
-        boto3.client("batch").update_compute_environment(
-            computeEnvironment=ce_name,
-            state="ENABLED",
-            computeResources={
-                "minvCpus": int(min_vcpus),
-                "maxvCpus": int(max_vcpus),
-                "desiredvCpus": int(desired_vcpus),
-            },
-        )
-    except ClientError as e:
-        LOGGER.critical(e.response.get("Error").get("Message"))
-        sys.exit(1)
+class HITStartCommand(StartCommand):
+    """Start command for the HIT based clusters."""
+
+    def start(self, args, pcluster_config):
+        """Start the compute fleet."""
+        raise NotImplementedError
+
+
+SCHEDULER_TO_START_COMMAND_MAP = {
+    "awsbatch": AWSBatchStartCommand,
+    "sge": SITStartCommand,
+    "torque": SITStartCommand,
+    "slurm": HITStartCommand,
+}
--- cli/pcluster/cli_commands/stop.py
+++ cli/pcluster/cli_commands/stop.py
@@ -8,34 +8,78 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
+import abc
 import logging
+import sys
 
 import boto3
 
 from pcluster import utils
 from pcluster.config.pcluster_config import PclusterConfig
 
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta("ABC", (), {})
+
 LOGGER = logging.getLogger(__name__)
 
 
 def stop(args):
-    """Set ASG limits or awsbatch ce to min/max/desired = 0/0/0."""
-    stack_name = utils.get_stack_name(args.cluster_name)
+    """Stop cluster compute fleet."""
     pcluster_config = PclusterConfig(config_file=args.config_file, cluster_name=args.cluster_name, auto_refresh=False)
     cluster_section = pcluster_config.get_section("cluster")
+    scheduler = cluster_section.get_param_value("scheduler")
 
-    if cluster_section.get_param_value("scheduler") == "awsbatch":
+    SCHEDULER_TO_STOP_COMMAND_MAP[scheduler]().stop(args, pcluster_config)
+
+
+class StopCommand(ABC):
+    """Interface to implement stop command."""
+
+    @abc.abstractmethod
+    def stop(self, args, pcluster_config):
+        """Stop the compute fleet."""
+        pass
+
+
+class AWSBatchStopCommand(StopCommand):
+    """Stop command for the AWSBatch cluster."""
+
+    def stop(self, args, pcluster_config):
+        """Stop the compute fleet."""
         LOGGER.info("Disabling AWS Batch compute environment : %s", args.cluster_name)
+        stack_name = utils.get_stack_name(args.cluster_name)
         ce_name = utils.get_batch_ce(stack_name)
-        _stop_batch_ce(ce_name=ce_name)
-    elif cluster_section.get_param_value("scheduler") == "slurm":
-        # TODO: to be implemented
-        raise NotImplementedError()
-    else:
+        self._stop_batch_ce(ce_name=ce_name)
+
+    @staticmethod
+    def _stop_batch_ce(ce_name):
+        boto3.client("batch").update_compute_environment(computeEnvironment=ce_name, state="DISABLED")
+
+
+class SITStopCommand(StopCommand):
+    """Stop command for the ASG based clusters."""
+
+    def stop(self, args, pcluster_config):
+        """Stop the compute fleet."""
         LOGGER.info("Stopping compute fleet: %s", args.cluster_name)
+        stack_name = utils.get_stack_name(args.cluster_name)
         asg_name = utils.get_asg_name(stack_name)
         utils.set_asg_limits(asg_name=asg_name, min=0, max=0, desired=0)
 
 
-def _stop_batch_ce(ce_name):
-    boto3.client("batch").update_compute_environment(computeEnvironment=ce_name, state="DISABLED")
+class HITStopCommand(StopCommand):
+    """Stop command for the HIT based clusters."""
+
+    def stop(self, args, pcluster_config):
+        """Stop the compute fleet."""
+        raise NotImplementedError
+
+
+SCHEDULER_TO_STOP_COMMAND_MAP = {
+    "awsbatch": AWSBatchStopCommand,
+    "sge": SITStopCommand,
+    "torque": SITStopCommand,
+    "slurm": HITStopCommand,
+}
