15b421b0610c85c1029627ec8c53686123e6e360
Luca Carrogu <carrogu@amazon.com>
2020-09-10 10:31:43 +0200

    Reduce the number of tests and re-balance them across regions (part 2)
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/tests/schedulers/test_awsbatch.py
+++ tests/integration-tests/tests/schedulers/test_awsbatch.py
@@ -20,9 +20,11 @@ from tests.common.schedulers_common import AWSBatchCommands
 
 @pytest.mark.batch_dockerfile_deps
 @pytest.mark.skip_regions(["ap-northeast-3", "us-gov-east-1", "us-gov-west-1"])
-@pytest.mark.instances(["c5.xlarge", "t2.large", "m6g.xlarge"])
-@pytest.mark.dimensions("*", "*", "alinux", "awsbatch")
-@pytest.mark.dimensions("*", "*", "alinux2", "awsbatch")
+@pytest.mark.instances(["c5.xlarge", "c4.xlarge", "m6g.xlarge"])
+@pytest.mark.dimensions("*", "c5.xlarge", "alinux2", "awsbatch")
+@pytest.mark.dimensions("cn-north-1", "c4.xlarge", "alinux2", "awsbatch")
+@pytest.mark.dimensions("ap-southeast-1", "c5.xlarge", "alinux", "awsbatch")
+@pytest.mark.dimensions("ap-northeast-1", "m6g.xlarge", "alinux2", "awsbatch")
 @pytest.mark.usefixtures("region", "os", "instance", "scheduler")
 def test_awsbatch(pcluster_config_reader, clusters_factory, test_datadir):
     """
