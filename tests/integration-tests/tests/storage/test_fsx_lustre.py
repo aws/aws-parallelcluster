aws/aws-parallelcluster

commit 4747a0b03c0931f8d761e70dd944666deff1b925

Add support to HDD storage type in FSx section
Signed-off-by: chenwany <chenwany@amazon.com>

cli/pcluster/config/validators.py
@@ -17,7 +17,7 @@ from urllib.parse import urlparse
 import boto3
 from botocore.exceptions import ClientError
 
-from pcluster.constants import CIDR_ALL_IPS
+from pcluster.constants import CIDR_ALL_IPS, FSX_HDD_THROUGHPUT, FSX_SSD_THROUGHPUT
 from pcluster.dcv.utils import get_supported_dcv_os
 from pcluster.utils import (
     ellipsize,
@@ -187,6 +187,8 @@ def fsx_validator(section_key, section_label, pcluster_config):
     fsx_daily_automatic_backup_start_time = fsx_section.get_param_value("daily_automatic_backup_start_time")
     fsx_automatic_backup_retention_days = fsx_section.get_param_value("automatic_backup_retention_days")
     fsx_copy_tags_to_backups = fsx_section.get_param_value("copy_tags_to_backups")
+    fsx_storage_type = fsx_section.get_param_value("storage_type")
+    fsx_drive_cache_type = fsx_section.get_param_value("drive_cache_type")
 
     validate_s3_options(errors, fsx_import_path, fsx_imported_file_chunk_size, fsx_export_path, fsx_auto_import_policy)
     validate_persistent_options(errors, fsx_deployment_type, fsx_kms_key_id, fsx_per_unit_storage_throughput)
@@ -200,7 +202,11 @@ def fsx_validator(section_key, section_label, pcluster_config):
         fsx_import_path,
         fsx_export_path,
         fsx_auto_import_policy,
+    ),
+    validate_storage_type_options(
+        errors, fsx_storage_type, fsx_deployment_type, fsx_per_unit_storage_throughput, fsx_drive_cache_type
     )
+
     return errors, warnings
 
 
@@ -287,11 +293,10 @@ def fsx_storage_capacity_validator(section_key, section_label, pcluster_config):
     fsx_section = pcluster_config.get_section(section_key, section_label)
     storage_capacity = fsx_section.get_param_value("storage_capacity")
     deployment_type = fsx_section.get_param_value("deployment_type")
-
-    if fsx_section.get_param_value("fsx_fs_id"):
+    storage_type = fsx_section.get_param_value("storage_type")
+    per_unit_storage_throughput = fsx_section.get_param_value("per_unit_storage_throughput")
+    if fsx_section.get_param_value("fsx_fs_id") or fsx_section.get_param_value("fsx_backup_id"):
         # if fsx_fs_id is provided, don't validate storage_capacity
-        return errors, warnings
-    elif fsx_section.get_param_value("fsx_backup_id"):
         # if fsx_backup_id is provided, validation for storage_capacity will be done in fsx_lustre_backup_validator.
         return errors, warnings
     elif not storage_capacity:
@@ -300,6 +305,11 @@ def fsx_storage_capacity_validator(section_key, section_label, pcluster_config):
     elif deployment_type == "SCRATCH_1":
         if not (storage_capacity == 1200 or storage_capacity == 2400 or storage_capacity % 3600 == 0):
             warnings.append("Capacity for FSx SCRATCH_1 filesystem is 1,200 GB, 2,400 GB or increments of 3,600 GB")
+    elif deployment_type == "PERSISTENT_1" and storage_type == "HDD":
+        if per_unit_storage_throughput == 12 and not (storage_capacity % 6000 == 0):
+            warnings.append("Capacity for FSx PERSISTENT HDD 12 MB/s/TiB file systems is increments of 6,000 GiB")
+        elif per_unit_storage_throughput == 40 and not (storage_capacity % 1800 == 0):
+            warnings.append("Capacity for FSx PERSISTENT HDD 40 MB/s/TiB file systems is increments of 1,800 GiB")
     elif deployment_type in ["SCRATCH_2", "PERSISTENT_1"]:
         if not (storage_capacity == 1200 or storage_capacity % 2400 == 0):
             warnings.append(
@@ -1304,11 +1314,13 @@ def fsx_ignored_parameters_validator(section_key, section_label, pcluster_config
 
     # If fsx_fs_id is specified, all parameters besides shared_dir are ignored.
     relevant_when_using_existing_fsx = ["fsx_fs_id", "shared_dir"]
+    PARAM_WITH_DEFAULT = {"drive_cache_type": "NONE"}
     if fsx_section.get_param_value("fsx_fs_id") is not None:
         for fsx_param in fsx_section.params:
-            if fsx_param not in relevant_when_using_existing_fsx and fsx_section.get_param_value(fsx_param) is not None:
+            if fsx_param not in relevant_when_using_existing_fsx and PARAM_WITH_DEFAULT.get(
+                fsx_param, None
+            ) != fsx_section.get_param_value(fsx_param):
                 errors.append(FSX_MESSAGES["errors"]["ignored_param_with_fsx_fs_id"].format(fsx_param=fsx_param))
-
     return errors, warnings
 
 
@@ -1384,11 +1396,14 @@ def validate_s3_options(errors, fsx_import_path, fsx_imported_file_chunk_size, f
         errors.append("When specifying 'export_path', the 'import_path' option must be specified")
 
     if fsx_auto_import_policy and not fsx_import_path:
-        errors.append("When specifying 'auto_import_policy', the 'import_path' option msut be specified")
+        errors.append("When specifying 'auto_import_policy', the 'import_path' option must be specified")
 
 
 def validate_persistent_options(errors, fsx_deployment_type, fsx_kms_key_id, fsx_per_unit_storage_throughput):
-    if fsx_deployment_type != "PERSISTENT_1":
+    if fsx_deployment_type == "PERSISTENT_1":
+        if not fsx_per_unit_storage_throughput:
+            errors.append("'per_unit_storage_throughput' must be specified when 'deployment_type = PERSISTENT_1'")
+    else:
         if fsx_kms_key_id:
             errors.append("'fsx_kms_key_id' can only be used when 'deployment_type = PERSISTENT_1'")
         if fsx_per_unit_storage_throughput:
@@ -1484,3 +1499,26 @@ def ebs_volume_size_snapshot_validator(section_key, section_label, pcluster_conf
                     )
                 )
     return errors, warnings
+
+
+def validate_storage_type_options(
+    errors, fsx_storage_type, fsx_deployment_type, fsx_per_unit_storage_throughput, fsx_drive_cache_type
+):
+    if fsx_storage_type == "HDD":
+        if fsx_deployment_type != "PERSISTENT_1":
+            errors.append("For HDD filesystems, 'deployment_type' must be 'PERSISTENT_1'")
+        if fsx_per_unit_storage_throughput not in FSX_HDD_THROUGHPUT:
+            errors.append(
+                "For HDD filesystems, 'per_unit_storage_throughput' can only have the following values: {0}".format(
+                    FSX_HDD_THROUGHPUT
+                )
+            )
+    else:  # SSD or None
+        if fsx_drive_cache_type is not "NONE":
+            errors.append("'drive_cache_type' features can be used only with HDD filesystems")
+        if fsx_per_unit_storage_throughput and fsx_per_unit_storage_throughput not in FSX_SSD_THROUGHPUT:
+            errors.append(
+                "For SSD filesystems, 'per_unit_storage_throughput' can only have the following values: {0}".format(
+                    FSX_SSD_THROUGHPUT
+                )
+            )

tests/integration-tests/tests/storage/test_fsx_lustre.py
@@ -33,8 +33,15 @@ MAX_MINUTES_TO_WAIT_FOR_BACKUP_COMPLETION = 7
 
 
 @pytest.mark.parametrize(
-    "deployment_type, per_unit_storage_throughput, auto_import_policy",
-    [("PERSISTENT_1", 200, "NEW_CHANGED"), ("SCRATCH_1", None, "NEW"), ("SCRATCH_2", None, "NONE")],
+    "deployment_type, per_unit_storage_throughput, auto_import_policy, storage_type, drive_cache_type, storage_capacity",
+    [
+        ("PERSISTENT_1", 200, "NEW_CHANGED", None, None, 1200),
+        ("SCRATCH_1", None, "NEW", None, None, 1200),
+        ("SCRATCH_2", None, None, None, None, 1200),
+        ("PERSISTENT_1", 200, None, "SSD", None, 1200),
+        ("PERSISTENT_1", 40, None, "HDD", None, 1800),
+        ("PERSISTENT_1", 12, None, "HDD", "READ", 6000),
+    ],
 )
 @pytest.mark.regions(["eu-west-1"])
 @pytest.mark.instances(["c5.xlarge", "m6g.xlarge"])
@@ -57,6 +64,9 @@ def test_fsx_lustre(
     test_datadir,
     os,
     scheduler,
+    storage_type,
+    drive_cache_type,
+    storage_capacity,
 ):
     """
     Test all FSx Lustre related features.
@@ -73,6 +83,9 @@ def test_fsx_lustre(
         deployment_type=deployment_type,
         per_unit_storage_throughput=per_unit_storage_throughput,
         auto_import_policy=auto_import_policy,
+        storage_type=storage_type,
+        drive_cache_type=drive_cache_type,
+        storage_capacity=storage_capacity,
     )
     cluster = clusters_factory(cluster_config)
     remote_command_executor = RemoteCommandExecutor(cluster)
@@ -82,6 +95,7 @@ def test_fsx_lustre(
     _test_fsx_lustre_correctly_mounted(remote_command_executor, mount_dir, os, region, fsx_fs_id)
     _test_import_path(remote_command_executor, mount_dir)
     _test_fsx_lustre_correctly_shared(scheduler_commands, remote_command_executor, mount_dir)
+    _test_storage_type(storage_type, fsx_fs_id, region)
     _test_export_path(remote_command_executor, mount_dir, bucket_name)
     _test_auto_import(auto_import_policy, remote_command_executor, mount_dir, bucket_name, region)
     _test_data_repository_task(remote_command_executor, mount_dir, bucket_name, fsx_fs_id, region)
@@ -170,8 +184,9 @@ def _test_fsx_lustre_correctly_mounted(remote_command_executor, mount_dir, os, r
     result = remote_command_executor.run_remote_command("df -h -t lustre | tail -n +2 | awk '{print $1, $2, $6}'")
     mount_name = get_mount_name(fsx_fs_id, region)
     assert_that(result.stdout).matches(
-        r"[0-9\.]+@tcp:/{mount_name}\s+1\.[12]T\s+{mount_dir}".format(mount_name=mount_name, mount_dir=mount_dir)
+        r"[0-9\.]+@tcp:/{mount_name}\s+[15]\.[1278]T\s+{mount_dir}".format(mount_name=mount_name, mount_dir=mount_dir)
     )
+    # example output: "192.168.46.168@tcp:/cg7k7bmv 1.7T /fsx_mount_dir"
 
     result = remote_command_executor.run_remote_command("cat /etc/fstab")
     mount_options = {
@@ -201,6 +216,19 @@ def get_mount_name(fsx_fs_id, region):
 def get_fsx_fs_id(cluster, region):
     fsx_stack = utils.get_substacks(cluster.cfn_name, region=region, sub_stack_name="FSXSubstack")[0]
     return utils.retrieve_cfn_outputs(fsx_stack, region).get("FileSystemId")
+
+
+def get_storage_type(fsx_fs_id, region):
+    logging.info("Getting StorageType from DescribeFilesystem API.")
+    fsx = boto3.client("fsx", region_name=region)
+    return fsx.describe_file_systems(FileSystemIds=[fsx_fs_id]).get("FileSystems")[0].get("StorageType")
+
+
+def _test_storage_type(storage_type, fsx_fs_id, region):
+    if storage_type == "HDD":
+        assert_that(get_storage_type(fsx_fs_id, region)).is_equal_to("HDD")
+    else:
+        assert_that(get_storage_type(fsx_fs_id, region)).is_equal_to("SSD")
 
 
 def _test_import_path(remote_command_executor, mount_dir):

cli/pcluster/config/mappings.py
@@ -95,7 +95,7 @@ from pcluster.config.validators import (
     tags_validator,
     url_validator,
 )
-from pcluster.constants import CIDR_ALL_IPS, SUPPORTED_ARCHITECTURES
+from pcluster.constants import CIDR_ALL_IPS, FSX_HDD_THROUGHPUT, FSX_SSD_THROUGHPUT, SUPPORTED_ARCHITECTURES
 
 # This file contains a definition of all the sections and the parameters configurable by the user
 # in the configuration file.
@@ -142,10 +142,13 @@ ALLOWED_VALUES = {
     "volume_id": r"^vol-[0-9a-z]{8}$|^vol-[0-9a-z]{17}$",
     "volume_types": ["standard", "io1", "gp2", "st1", "sc1"],
     "vpc_id": r"^vpc-[0-9a-z]{8}$|^vpc-[0-9a-z]{17}$",
-    "deployment_type": ["SCRATCH_1", "SCRATCH_2", "PERSISTENT_1"],
-    "per_unit_storage_throughput": [50, 100, 200],
+    "fsx_deployment_type": ["SCRATCH_1", "SCRATCH_2", "PERSISTENT_1"],
+    "fsx_ssd_throughput": FSX_SSD_THROUGHPUT,
+    "fsx_hdd_throughput": FSX_HDD_THROUGHPUT,
     "architectures": SUPPORTED_ARCHITECTURES,
-    "auto_import_policy": ["NONE", "NEW", "NEW_CHANGED"]
+    "fsx_auto_import_policy": ["NEW", "NEW_CHANGED"],
+    "fsx_storage_type": ["SSD", "HDD"],
+    "fsx_drive_cache_type": ["READ"]
 }
 
 AWS = {
@@ -497,12 +500,12 @@ FSX = {
                 "update_policy": UpdatePolicy.SUPPORTED
             }),
             ("deployment_type", {
-                "allowed_values": ALLOWED_VALUES["deployment_type"],
+                "allowed_values": ALLOWED_VALUES["fsx_deployment_type"],
                 "update_policy": UpdatePolicy.UNSUPPORTED
             }),
             ("per_unit_storage_throughput", {
                 "type": IntCfnParam,
-                "allowed_values": ALLOWED_VALUES["per_unit_storage_throughput"],
+                "allowed_values": ALLOWED_VALUES["fsx_ssd_throughput"] + ALLOWED_VALUES["fsx_hdd_throughput"],
                 "update_policy": UpdatePolicy.UNSUPPORTED
             }),
             ("daily_automatic_backup_start_time", {
@@ -525,7 +528,16 @@ FSX = {
             }),
             ("auto_import_policy", {
                 "validators": [fsx_lustre_auto_import_validator],
-                "allowed_values": ALLOWED_VALUES["auto_import_policy"],
+                "allowed_values": ALLOWED_VALUES["fsx_auto_import_policy"],
+                "update_policy": UpdatePolicy.UNSUPPORTED
+            }),
+            ("storage_type", {
+                "allowed_values": ALLOWED_VALUES["fsx_storage_type"],
+                "update_policy": UpdatePolicy.UNSUPPORTED
+            }),
+            ("drive_cache_type", {
+                "default": "NONE",
+                "allowed_values": ALLOWED_VALUES["fsx_drive_cache_type"],
                 "update_policy": UpdatePolicy.UNSUPPORTED
             })
         ]

cli/pcluster/constants.py
@@ -16,3 +16,5 @@ PCLUSTER_ISSUES_LINK = "https://github.com/aws/aws-parallelcluster/issues"
 CIDR_ALL_IPS = "0.0.0.0/0"
 DEFAULT_ARCHITECTURE = "x86_64"
 SUPPORTED_ARCHITECTURES = ["x86_64", "arm64"]
+FSX_SSD_THROUGHPUT = [50, 100, 200]
+FSX_HDD_THROUGHPUT = [12, 40]

cli/tests/pcluster/config/defaults.py
@@ -83,6 +83,8 @@ DEFAULT_FSX_DICT = {
     "copy_tags_to_backups": None,
     "fsx_backup_id": None,
     "auto_import_policy": None,
+    "storage_type": None,
+    "drive_cache_type": "NONE",
 }
 
 DEFAULT_DCV_DICT = {"enable": None, "port": 8443, "access_from": "0.0.0.0/0"}
@@ -247,7 +249,9 @@ DEFAULT_EFS_CFN_PARAMS = {"EFSOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE
 
 DEFAULT_RAID_CFN_PARAMS = {"RAIDOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"}
 
-DEFAULT_FSX_CFN_PARAMS = {"FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"}
+DEFAULT_FSX_CFN_PARAMS = {
+    "FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"
+}
 
 DEFAULT_DCV_CFN_PARAMS = {"DCVOptions": "NONE,NONE,NONE"}
 DEFAULT_CW_LOG_CFN_PARAMS = {"CWLogOptions": "true,14"}
@@ -314,7 +318,7 @@ DEFAULT_CLUSTER_SIT_CFN_PARAMS = {
     # raid
     "RAIDOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
     # fsx
-    "FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+    "FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
     # dcv
     "DCVOptions": "NONE,NONE,NONE",
     # cw_log_settings
@@ -379,7 +383,7 @@ DEFAULT_CLUSTER_HIT_CFN_PARAMS = {
     # raid
     "RAIDOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
     # fsx
-    "FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+    "FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
     # dcv
     "DCVOptions": "NONE,NONE,NONE",
     # cw_log_settings

cli/tests/pcluster/config/test_section_cluster.py
@@ -1120,7 +1120,8 @@ def test_cluster_section_to_cfn(
                     # raid
                     "RAIDOptions": "raid,NONE,2,gp2,20,100,false,NONE",
                     # fsx
-                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,"
+                    "NONE,NONE",
                     # dcv
                     "DCVOptions": "master,8555,10.0.0.0/0",
                     "Scheduler": "sge",
@@ -1185,7 +1186,8 @@ def test_cluster_section_to_cfn(
                     # raid
                     "RAIDOptions": "raid,NONE,2,gp2,20,100,false,NONE",
                     # fsx
-                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,"
+                    "NONE,NONE",
                     # dcv
                     "DCVOptions": "master,8555,10.0.0.0/0",
                 },

cli/tests/pcluster/config/test_section_fsx.py
@@ -21,21 +21,21 @@ from tests.pcluster.config.defaults import DefaultCfnParams, DefaultDict
         (DefaultCfnParams["fsx"].value, DefaultDict["fsx"].value),
         ({}, DefaultDict["fsx"].value),
         (
-            {"FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
+            {"FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
             DefaultDict["fsx"].value,
         ),
         (
-            {"FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
+            {"FSXOptions": "NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
             DefaultDict["fsx"].value,
         ),
         (
-            {"FSXOptions": "test,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
+            {"FSXOptions": "test,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE"},
             utils.merge_dicts(DefaultDict["fsx"].value, {"shared_dir": "test"}),
         ),
         (
             {
                 "FSXOptions": "test,test1,10,test2,20,test3,test4,test5,SCRATCH_1,"
-                "50,01:00,5,false,backup-0a1b2c3d4e5f6a7b8,NEW_CHANGED"
+                "50,01:00,5,false,backup-0a1b2c3d4e5f6a7b8,NEW_CHANGED,HDD,READ"
             },
             {
                 "shared_dir": "test",
@@ -53,6 +53,80 @@ from tests.pcluster.config.defaults import DefaultCfnParams, DefaultDict
                 "copy_tags_to_backups": False,
                 "fsx_backup_id": "backup-0a1b2c3d4e5f6a7b8",
                 "auto_import_policy": "NEW_CHANGED",
+                "storage_type": "HDD",
+                "drive_cache_type": "READ",
+            },
+        ),
+        (
+            {
+                "FSXOptions": "test,test1,10,test2,20,test3,test4,test5,SCRATCH_1,"
+                "50,01:00,5,false,backup-0a1b2c3d4e5f6a7b8,NEW_CHANGED,HDD,NONE"
+            },
+            {
+                "shared_dir": "test",
+                "fsx_fs_id": "test1",
+                "storage_capacity": 10,
+                "fsx_kms_key_id": "test2",
+                "imported_file_chunk_size": 20,
+                "export_path": "test3",
+                "import_path": "test4",
+                "weekly_maintenance_start_time": "test5",
+                "deployment_type": "SCRATCH_1",
+                "per_unit_storage_throughput": 50,
+                "daily_automatic_backup_start_time": "01:00",
+                "automatic_backup_retention_days": 5,
+                "copy_tags_to_backups": False,
+                "fsx_backup_id": "backup-0a1b2c3d4e5f6a7b8",
+                "auto_import_policy": "NEW_CHANGED",
+                "storage_type": "HDD",
+                "drive_cache_type": "NONE",
+            },
+        ),
+        (
+            {
+                "FSXOptions": "test,test1,10,test2,20,test3,test4,test5,SCRATCH_1,"
+                "50,01:00,5,false,backup-0a1b2c3d4e5f6a7b8,NEW_CHANGED,SSD,NONE"
+            },
+            {
+                "shared_dir": "test",
+                "fsx_fs_id": "test1",
+                "storage_capacity": 10,
+                "fsx_kms_key_id": "test2",
+                "imported_file_chunk_size": 20,
+                "export_path": "test3",
+                "import_path": "test4",
+                "weekly_maintenance_start_time": "test5",
+                "deployment_type": "SCRATCH_1",
+                "per_unit_storage_throughput": 50,
+                "daily_automatic_backup_start_time": "01:00",
+                "automatic_backup_retention_days": 5,
+                "copy_tags_to_backups": False,
+                "fsx_backup_id": "backup-0a1b2c3d4e5f6a7b8",
+                "auto_import_policy": "NEW_CHANGED",
+                "storage_type": "SSD",
+            },
+        ),
+        (
+            {
+                "FSXOptions": "test,test1,10,test2,20,test3,test4,test5,SCRATCH_1,"
+                "50,01:00,5,false,backup-0a1b2c3d4e5f6a7b8,NONE,NONE,NONE"
+            },
+            {
+                "shared_dir": "test",
+                "fsx_fs_id": "test1",
+                "storage_capacity": 10,
+                "fsx_kms_key_id": "test2",
+                "imported_file_chunk_size": 20,
+                "export_path": "test3",
+                "import_path": "test4",
+                "weekly_maintenance_start_time": "test5",
+                "deployment_type": "SCRATCH_1",
+                "per_unit_storage_throughput": 50,
+                "daily_automatic_backup_start_time": "01:00",
+                "automatic_backup_retention_days": 5,
+                "copy_tags_to_backups": False,
+                "fsx_backup_id": "backup-0a1b2c3d4e5f6a7b8",
+                "auto_import_policy": None,
             },
         ),
     ],
@@ -177,6 +251,8 @@ def test_fsx_section_to_cfn(mocker, section_dict, expected_cfn_params):
             "INVALID_VALUE",
             " 'deployment_type' has an invalid value 'INVALID_VALUE'",
         ),
+        ("per_unit_storage_throughput", "12", 12, None),
+        ("per_unit_storage_throughput", "40", 40, None),
         ("per_unit_storage_throughput", "50", 50, None),
         ("per_unit_storage_throughput", "100", 100, None),
         ("per_unit_storage_throughput", "200", 200, None),
@@ -223,9 +299,25 @@ def test_fsx_section_to_cfn(mocker, section_dict, expected_cfn_params):
         ),
         ("fsx_backup_id", "backup-0a1b2c3d4e5f6a7b8", "backup-0a1b2c3d4e5f6a7b8", None),
         ("auto_import_policy", None, None, None),
-        ("auto_import_policy", "NONE", "NONE", None),
         ("auto_import_policy", "NEW", "NEW", None),
         ("auto_import_policy", "NEW_CHANGED", "NEW_CHANGED", None),
+        ("storage_type", None, None, None),
+        ("storage_type", "SSD", "SSD", None),
+        ("storage_type", "HDD", "HDD", None),
+        (
+            "storage_type",
+            "INVALID_VALUE",
+            "INVALID_VALUE",
+            " 'storage_type' has an invalid value 'INVALID_VALUE'",
+        ),
+        ("drive_cache_type", None, "NONE", None),
+        ("drive_cache_type", "READ", "READ", None),
+        (
+            "drive_cache_type",
+            "INVALID_VALUE",
+            "INVALID_VALUE",
+            " 'drive_cache_type' has an invalid value 'INVALID_VALUE'",
+        ),
     ],
 )
 def test_fsx_param_from_file(mocker, param_key, param_value, expected_value, expected_message):
@@ -250,7 +342,8 @@ def test_fsx_param_from_file(mocker, param_key, param_value, expected_value, exp
                 {
                     "MasterSubnetId": "subnet-12345678",
                     "AvailabilityZone": "mocked_avail_zone",
-                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+                    "FSXOptions": "fsx,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,"
+                    "NONE,NONE",
                 },
             ),
         ),
@@ -262,7 +355,19 @@ def test_fsx_param_from_file(mocker, param_key, param_value, expected_value, exp
                     "MasterSubnetId": "subnet-12345678",
                     "AvailabilityZone": "mocked_avail_zone",
                     "FSXOptions": "fsx,fs-12345678901234567,10,key1,1020,s3://test-export,"
-                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NEW_CHANGED",
+                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NEW_CHANGED,HDD,READ",
+                },
+            ),
+        ),
+        (
+            "test3",
+            utils.merge_dicts(
+                DefaultCfnParams["cluster_sit"].value,
+                {
+                    "MasterSubnetId": "subnet-12345678",
+                    "AvailabilityZone": "mocked_avail_zone",
+                    "FSXOptions": "fsx,fs-12345678901234567,10,key1,1020,s3://test-export,"
+                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NEW_CHANGED,HDD,READ",
                 },
             ),
         ),
@@ -276,7 +381,45 @@ def test_fsx_param_from_file(mocker, param_key, param_value, expected_value, exp
                 {
                     "MasterSubnetId": "subnet-12345678",
                     "AvailabilityZone": "mocked_avail_zone",
-                    "FSXOptions": "/fsx,NONE,3600,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE",
+                    "FSXOptions": "/fsx,NONE,3600,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,NONE,"
+                    "NONE,"
+                    "NONE",
+                },
+            ),
+        ),
+        (
+            "test7",
+            utils.merge_dicts(
+                DefaultCfnParams["cluster_sit"].value,
+                {
+                    "MasterSubnetId": "subnet-12345678",
+                    "AvailabilityZone": "mocked_avail_zone",
+                    "FSXOptions": "fsx,fs-12345678901234567,10,key1,1020,s3://test-export,"
+                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NONE,HDD,NONE",
+                },
+            ),
+        ),
+        (
+            "test8",
+            utils.merge_dicts(
+                DefaultCfnParams["cluster_sit"].value,
+                {
+                    "MasterSubnetId": "subnet-12345678",
+                    "AvailabilityZone": "mocked_avail_zone",
+                    "FSXOptions": "fsx,fs-12345678901234567,10,key1,1020,s3://test-export,"
+                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NONE,HDD,READ",
+                },
+            ),
+        ),
+        (
+            "test9",
+            utils.merge_dicts(
+                DefaultCfnParams["cluster_sit"].value,
+                {
+                    "MasterSubnetId": "subnet-12345678",
+                    "AvailabilityZone": "mocked_avail_zone",
+                    "FSXOptions": "fsx,fs-12345678901234567,10,key1,1020,s3://test-export,"
+                    "s3://test-import,1:10:17,SCRATCH_1,50,01:00,5,false,NONE,NONE,SSD,NONE",
                 },
             ),
         ),

commit 135a45e22441092ae5b3ed81ee36f43759e6ff58

Fix code formatting
* Fix code formatting issues introduced by 4747a0b03c0931f8d761e70dd944666deff1b925

Signed-off-by: Rex <shuningc@amazon.com>

cli/pcluster/config/validators.py
@@ -66,6 +66,8 @@ FSX_SUPPORTED_ARCHITECTURES_OSES = {
     "arm64": ["ubuntu1804", "alinux2"],
 }
 
+FSX_PARAM_WITH_DEFAULT = {"drive_cache_type": "NONE"}
+
 EBS_VOLUME_TYPE_TO_VOLUME_SIZE_BOUNDS = {
     "standard": (1, 1024),
     "io1": (4, 16 * 1024),
@@ -1314,10 +1316,9 @@ def fsx_ignored_parameters_validator(section_key, section_label, pcluster_config
 
     # If fsx_fs_id is specified, all parameters besides shared_dir are ignored.
     relevant_when_using_existing_fsx = ["fsx_fs_id", "shared_dir"]
-    PARAM_WITH_DEFAULT = {"drive_cache_type": "NONE"}
     if fsx_section.get_param_value("fsx_fs_id") is not None:
         for fsx_param in fsx_section.params:
-            if fsx_param not in relevant_when_using_existing_fsx and PARAM_WITH_DEFAULT.get(
+            if fsx_param not in relevant_when_using_existing_fsx and FSX_PARAM_WITH_DEFAULT.get(
                 fsx_param, None
             ) != fsx_section.get_param_value(fsx_param):
                 errors.append(FSX_MESSAGES["errors"]["ignored_param_with_fsx_fs_id"].format(fsx_param=fsx_param))
@@ -1514,7 +1515,7 @@ def validate_storage_type_options(
                 )
             )
     else:  # SSD or None
-        if fsx_drive_cache_type is not "NONE":
+        if fsx_drive_cache_type != "NONE":
             errors.append("'drive_cache_type' features can be used only with HDD filesystems")
         if fsx_per_unit_storage_throughput and fsx_per_unit_storage_throughput not in FSX_SSD_THROUGHPUT:
             errors.append(

tests/integration-tests/tests/storage/test_fsx_lustre.py
@@ -33,7 +33,14 @@ MAX_MINUTES_TO_WAIT_FOR_BACKUP_COMPLETION = 7
 
 
 @pytest.mark.parametrize(
-    "deployment_type, per_unit_storage_throughput, auto_import_policy, storage_type, drive_cache_type, storage_capacity",
+    (
+        "deployment_type",
+        "per_unit_storage_throughput",
+        "auto_import_policy",
+        "storage_type",
+        "drive_cache_type",
+        "storage_capacity",
+    ),
     [
         ("PERSISTENT_1", 200, "NEW_CHANGED", None, None, 1200),
         ("SCRATCH_1", None, "NEW", None, None, 1200),