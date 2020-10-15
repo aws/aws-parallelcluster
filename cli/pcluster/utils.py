aws/aws-parallelcluster

commit 65cd4db1b519952675f418fc2ae683fde9f3fea5

Include user tags in HIT substack launch templates
CFN does not currently support tag propagation to launch templates. See
https://github.com/aws-cloudformation/aws-cloudformation-coverage-roadmap/issues/184
for details. Work around this by including the tags when rendering the
template.

Also tag EBS volumes in the head node substack.

Signed-off-by: Tim Lane <tilne@amazon.com>

cli/pcluster/utils.py
@@ -1101,7 +1101,7 @@ def read_remote_file(url):
         raise e
 
 
-def render_template(template_str, params_dict, config_version):
+def render_template(template_str, params_dict, config_version, tags):
     """
     Render a Jinjia template and return the rendered output.
 
@@ -1112,7 +1112,7 @@ def render_template(template_str, params_dict, config_version):
         environment = Environment(loader=BaseLoader)
         environment.filters["sha1"] = lambda value: hashlib.sha1(value.strip().encode()).hexdigest()
         template = environment.from_string(template_str)
-        output_from_parsed_template = template.render(config=params_dict, config_version=config_version)
+        output_from_parsed_template = template.render(config=params_dict, config_version=config_version, tags=tags)
         return output_from_parsed_template
     except Exception as e:
         LOGGER.error("Error when rendering template: %s", e)

cli/pcluster/commands.py
@@ -38,7 +38,7 @@ from pcluster.constants import PCLUSTER_NAME_MAX_LENGTH, PCLUSTER_NAME_REGEX, PC
 LOGGER = logging.getLogger(__name__)
 
 
-def _create_bucket_with_resources(pcluster_config, json_params):
+def _create_bucket_with_resources(pcluster_config, json_params, tags):
     """Create a bucket associated to the given stack and upload specified resources."""
     scheduler = pcluster_config.get_section("cluster").get_param_value("scheduler")
     if scheduler not in ["awsbatch", "slurm"]:
@@ -61,7 +61,7 @@ def _create_bucket_with_resources(pcluster_config, json_params):
             resources = pkg_resources.resource_filename(__name__, resources_dir)
             utils.upload_resources_artifacts(s3_bucket_name, root=resources)
         if utils.is_hit_enabled_scheduler(scheduler):
-            _upload_hit_resources(s3_bucket_name, pcluster_config, json_params)
+            _upload_hit_resources(s3_bucket_name, pcluster_config, json_params, tags)
     except Exception:
         LOGGER.error("Unable to upload cluster resources to the S3 bucket %s.", s3_bucket_name)
         utils.delete_s3_bucket(s3_bucket_name)
@@ -70,7 +70,7 @@ def _create_bucket_with_resources(pcluster_config, json_params):
     return s3_bucket_name
 
 
-def _upload_hit_resources(bucket_name, pcluster_config, json_params):
+def _upload_hit_resources(bucket_name, pcluster_config, json_params, tags):
     hit_template_url = pcluster_config.get_section("cluster").get_param_value(
         "hit_template_url"
     ) or "{bucket_url}/templates/compute-fleet-hit-substack-{version}.cfn.yaml".format(
@@ -83,7 +83,7 @@ def _upload_hit_resources(bucket_name, pcluster_config, json_params):
             Bucket=bucket_name, Body=json.dumps(json_params), Key="configs/cluster-config.json"
         )
         file_contents = utils.read_remote_file(hit_template_url)
-        rendered_template = utils.render_template(file_contents, json_params, result.get("VersionId"))
+        rendered_template = utils.render_template(file_contents, json_params, result.get("VersionId"), tags)
     except ClientError as client_error:
         LOGGER.error("Error when uploading cluster configuration file to bucket %s: %s", bucket_name, client_error)
         raise
@@ -148,7 +148,10 @@ def create(args):  # noqa: C901 FIXME!!!
         cfn_client = boto3.client("cloudformation")
         stack_name = utils.get_stack_name(args.cluster_name)
 
-        bucket_name = _create_bucket_with_resources(pcluster_config, storage_data.json_params)
+        # merge tags from configuration, command-line and internal ones
+        tags = _evaluate_tags(pcluster_config, preferred_tags=args.tags)
+
+        bucket_name = _create_bucket_with_resources(pcluster_config, storage_data.json_params, tags)
         if bucket_name:
             cfn_params["ResourcesS3Bucket"] = bucket_name
 
@@ -156,9 +159,6 @@ def create(args):  # noqa: C901 FIXME!!!
 
         # determine the CloudFormation Template URL to use
         template_url = _evaluate_pcluster_template_url(pcluster_config, preferred_template_url=args.template_url)
-
-        # merge tags from configuration, command-line and internal ones
-        tags = _evaluate_tags(pcluster_config, preferred_tags=args.tags)
 
         # append extra parameters from command-line
         if args.extra_parameters:

cli/tests/pcluster/test_commands.py
@@ -56,14 +56,14 @@ def test_create_bucket_with_resources_success(
 
     storage_data = pcluster_config_mock.to_storage()
 
-    bucket_name = _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params)
+    bucket_name = _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params, {})
 
     delete_s3_bucket_mock.assert_not_called()
     upload_resources_artifacts_mock.assert_has_calls(
         [mocker.call(bucket_name, root=pkg_resources.resource_filename(utils.__name__, dir)) for dir in expected_dirs]
     )
     if expect_upload_hit_resources:
-        upload_hit_resources_mock.assert_called_with(bucket_name, pcluster_config_mock, storage_data.json_params)
+        upload_hit_resources_mock.assert_called_with(bucket_name, pcluster_config_mock, storage_data.json_params, {})
     assert_that(bucket_name).is_equal_to(expected_bucket_name)
 
 
@@ -83,7 +83,7 @@ def test_create_bucket_with_resources_creation_failure(mocker, caplog):
     storage_data = pcluster_config_mock.to_storage()
 
     with pytest.raises(ClientError, match=error):
-        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params)
+        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params, {})
     delete_s3_bucket_mock.assert_not_called()
     assert_that(caplog.text).contains("Unable to create S3 bucket")
 
@@ -104,7 +104,7 @@ def test_create_bucket_with_resources_upload_failure(mocker, caplog):
     storage_data = pcluster_config_mock.to_storage()
 
     with pytest.raises(ClientError, match=error):
-        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params)
+        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params, {})
     # if resource upload fails we delete the bucket
     delete_s3_bucket_mock.assert_called_with(bucket_name)
     assert_that(caplog.text).contains("Unable to upload cluster resources to the S3 bucket")
@@ -128,7 +128,7 @@ def test_create_bucket_with_resources_deletion_failure(mocker, caplog):
 
     # force upload failure to trigger a bucket deletion and then check the behaviour when the deletion fails
     with pytest.raises(ClientError, match=error):
-        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params)
+        _create_bucket_with_resources(pcluster_config_mock, storage_data.json_params, {})
     delete_s3_bucket_mock.assert_called_with(bucket_name)
     assert_that(caplog.text).contains("Unable to upload cluster resources to the S3 bucket")
 

cloudformation/compute-fleet-hit-substack.cfn.yaml
@@ -157,6 +157,28 @@ Resources:
                 Value: !Ref 'FilesystemTagValue'
               - Key: aws-parallelcluster-node-type
                 Value: Compute
+    {%- for tag in tags %}
+              - Key: {{ tag.Key }}
+                Value: {{ tag.Value }}
+    {%- endfor %}
+          - ResourceType: volume
+            Tags:
+              - Key: ClusterName
+                Value: !Select
+                  - '1'
+                  - !Split
+                    - parallelcluster-
+                    - !Ref 'MainStackName'
+              - Key: QueueName
+                Value: {{ queue }}
+              - Key: Application
+                Value: !Ref 'MainStackName'
+              - Key: aws-parallelcluster-node-type
+                Value: Compute
+    {%- for tag in tags %}
+              - Key: {{ tag.Key }}
+                Value: {{ tag.Value }}
+    {%- endfor %}
         BlockDeviceMappings:
           - DeviceName: /dev/xvdba
             VirtualName: ephemeral0

cloudformation/compute-fleet-substack.cfn.yaml
@@ -298,6 +298,19 @@ Resources:
             Ebs:
               VolumeSize: !Ref 'RootVolumeSize'
               VolumeType: gp2
+        TagSpecifications:
+          - ResourceType: volume
+            Tags:
+              - Key: ClusterName
+                Value: !Select
+                  - '1'
+                  - !Split
+                    - parallelcluster-
+                    - !Ref 'MainStackName'
+              - Key: Application
+                Value: !Ref 'MainStackName'
+              - Key: aws-parallelcluster-node-type
+                Value: Compute
         UserData: !Base64
           Fn::Sub:
             - |

cloudformation/master-server-substack.cfn.yaml
@@ -326,6 +326,18 @@ Resources:
                 Value: !Ref 'NetworkingTagValue'
               - Key: aws-parallelcluster-filesystem
                 Value: !Ref 'FilesystemTagValue'
+          - ResourceType: volume
+            Tags:
+              - Key: ClusterName
+                Value: !Select
+                  - '1'
+                  - !Split
+                    - parallelcluster-
+                    - !Ref 'MainStackName'
+              - Key: Application
+                Value: !Ref 'MainStackName'
+              - Key: aws-parallelcluster-node-type
+                Value: Master
         NetworkInterfaces:
           - NetworkInterfaceId: !Ref 'MasterENI'
             DeviceIndex: 0

cloudformation/tests/test_cfn_template_rendering.py
@@ -145,7 +145,9 @@ def test_hit_substack_rendering(tmp_path, test_config):
     env = Environment(loader=FileSystemLoader(".."))
     env.filters["sha1"] = lambda value: hashlib.sha1(value.strip().encode()).hexdigest()
     template = env.get_template("compute-fleet-hit-substack.cfn.yaml")
-    output_from_parsed_template = template.render(config=test_config, config_version="version")
+    output_from_parsed_template = template.render(
+        config=test_config, config_version="version", tags=[{"Key": "TagKey", "Value": "TagValue"}]
+    )
     rendered_file = tmp_path / "compute-fleet-hit-substack.cfn.yaml"
     rendered_file.write_text(output_from_parsed_template)
 

commit b842fd6320ffaadc87f1c15e31b4034a12195cce

Enable minimal pylint checks and fix errors
This commit adds a pylint check to the checks run for the code-linters
tox environment. Currently the check only looks for function calls which
are missing required arguments. The motivation for enabling this
particular check is the bug that was introduced by 65cd4db1, where a
required arg was added to the function _upload_hit_resources, but the
usage in update.py was not modified accordingly.

Signed-off-by: Tim Lane <tilne@amazon.com>

cli/pcluster/utils.py
@@ -857,7 +857,7 @@ def get_master_server_state(stack_name):
     :param stack_name: The name of the cloudformation stack
     :return master server state name
     """
-    instances = describe_cluster_instances(stack_name, filter_by_name="Master")
+    instances = describe_cluster_instances(stack_name, "Master")
     if not instances:
         error("MasterServer not running.")
     return instances[0].get("State").get("Name")

cli/tox.ini
@@ -152,6 +152,20 @@ commands =
         ../util/ \
         {posargs}
 
+[testenv:pylint-permissive]
+deps =
+    {[testenv:pylint]deps}
+commands =
+    pylint \
+        --disable=all \
+        --enable=no-value-for-parameter \
+        setup.py \
+        awsbatch/ \
+        pcluster/ \
+        pcluster/resources/custom_resources/custom_resources_code/ \
+        ../util/ \
+        {posargs}
+
 # Vulture finds unused code in python: https://github.com/jendrikseipp/vulture
 [testenv:vulture]
 basepython = python3
@@ -187,6 +201,7 @@ deps =
     {[testenv:black-check]deps}
     {[testenv:isort-check]deps}
     {[testenv:flake8]deps}
+    {[testenv:pylint-permissive]deps}
     # {[testenv:pylint]deps}
     # {[testenv:bandit]deps}
     # {[testenv:readme]deps}
@@ -194,6 +209,7 @@ commands =
     {[testenv:black-check]commands}
     {[testenv:isort-check]commands}
     {[testenv:flake8]commands}
+    {[testenv:pylint-permissive]commands}
     # {[testenv:pylint]commands}
     # {[testenv:bandit]commands}
     # {[testenv:readme]commands}