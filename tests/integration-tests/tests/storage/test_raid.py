79a932aa76303534c9856b695141d1bdfbef9666
Enrico Usai <usai@amazon.com>
2020-10-30 15:33:31 +0100

    Extend RAID test to work with different mdadm versions
    The output of `mdadm` utility in is a bit different, this could depend by the version
    of the tool (4.1-13 on Centos8 vs 4.1-4 on Centos7) or by the fact we're using a
    different superblock version (1.2 on Centos8 vs 0.90 on Centos7 and other OSes).
    
    With the same `volume_size` we have:
    * `Array Size : 41942912 (40.00 GiB 42.95 GB)` on Centos7 with `mdadm-4.1-4.el7`
    * `Array Size : 41908224 (39.97 GiB 42.91 GB)` on Centos8 with `mdadm-4.1-13.el8`
    
    # References
    * https://github.com/aws/aws-parallelcluster-cookbook/pull/789
    * https://raid.wiki.kernel.org/index.php/RAID_superblock_formats
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/storage/test_raid.py
+++ tests/integration-tests/tests/storage/test_raid.py
@@ -10,11 +10,12 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 import logging
+import re
 
 import pytest
 from assertpy import assert_that
-from remote_command_executor import RemoteCommandExecutor
 
+from remote_command_executor import RemoteCommandExecutor
 from tests.common.schedulers_common import get_scheduler_commands
 from tests.storage.storage_common import verify_directory_correctly_shared
 
@@ -73,10 +74,16 @@ def _test_raid_correctly_shared(remote_command_executor, mount_dir, scheduler_co
 def _test_raid_correctly_configured(remote_command_executor, raid_type, volume_size, raid_devices):
     result = remote_command_executor.run_remote_command("sudo mdadm --detail /dev/md0")
     assert_that(result.stdout).contains("Raid Level : raid{0}".format(raid_type))
-    assert_that(result.stdout).matches(r"Array Size : .*{0}\.00 GiB".format(volume_size))
     assert_that(result.stdout).contains("Raid Devices : {0}".format(raid_devices))
     assert_that(result.stdout).contains("Active Devices : {0}".format(raid_devices))
     assert_that(result.stdout).contains("Failed Devices : 0")
+
+    # Compare rounded size to match output from different mdadm version
+    # Array Size : 41942912 (40.00 GiB 42.95 GB) --> on Centos7 with mdadm-4.1-4.el7
+    # Array Size : 41908224 (39.97 GiB 42.91 GB) --> on Centos8 with mdadm-4.1-13.el8
+    array_size = re.search(r"Array Size : .*\((.*) GiB", result.stdout).group(1)
+    expected_size = volume_size - 0.1
+    assert_that(float(array_size)).is_greater_than_or_equal_to(expected_size)
 
     # ensure that the RAID array is reassembled automatically on boot
     expected_entry = remote_command_executor.run_remote_command("sudo mdadm --detail --scan").stdout
