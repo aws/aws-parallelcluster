0041a8b42dfe7533b4fb27961667a59b76323a48
chenwany <chenwany@amazon.com>
2020-12-06 14:34:53 -0800

    Revert "awsbatch: download AmazonLinux image from ECR rather than Docker Hub"

--- cli/src/pcluster/resources/batch/docker/upload-docker-images.sh
+++ cli/src/pcluster/resources/batch/docker/upload-docker-images.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
-set -euxo pipefail
-
-DOMAIN_SUFFIX=""
-if [[ ${AWS_REGION} == cn-* ]]; then
-    DOMAIN_SUFFIX=".cn"
-fi
+set -eu
 
 push_docker_image() {
     local image=$1
     echo "Uploading image ${image}"
-    docker tag "${IMAGE_REPO_NAME}:${image}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com${DOMAIN_SUFFIX}/${IMAGE_REPO_NAME}:${image}"
-    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com${DOMAIN_SUFFIX}/${IMAGE_REPO_NAME}:${image}"
+    S3_SUFFIX=""
+    if [[ ${AWS_REGION} == cn-* ]]; then
+        S3_SUFFIX=".cn"
+    fi
+    docker tag "${IMAGE_REPO_NAME}:${image}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com${S3_SUFFIX}/${IMAGE_REPO_NAME}:${image}"
+    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com${S3_SUFFIX}/${IMAGE_REPO_NAME}:${image}"
 }
-
-aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com${DOMAIN_SUFFIX}"
 
 if [ -z "${IMAGE}" ]; then
     for file in $(find `pwd` -type f -name Dockerfile); do
