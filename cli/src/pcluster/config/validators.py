27721ec4c346f1e8fd1bfca38642b9b42ad7f169
Enrico Usai <usai@amazon.com>
2021-02-02 14:42:04 +0100

    Remove os validator
    It was used to alert the users when using alinux
    but it will be deprecated within the next version.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/src/pcluster/config/validators.py
+++ cli/src/pcluster/config/validators.py
@@ -902,19 +902,6 @@ def intel_hpc_architecture_validator(param_key, param_value, pcluster_config):
     return errors, warnings
 
 
-def base_os_validator(param_key, param_value, pcluster_config):
-    warnings = []
-
-    eol_2020 = ["alinux"]
-    if param_value in eol_2020:
-        warnings.append(
-            "The operating system you are using ({0}) will reach end-of-life in late 2020. It will be deprecated in "
-            "future releases of ParallelCluster".format(param_value)
-        )
-
-    return [], warnings
-
-
 def tags_validator(param_key, param_value, pcluster_config):
     errors = []
 
