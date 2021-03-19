f7ad2cc865968be8585fe664fd74a8e84c50c5ca
Yulei Wang <yuleiwan@amazon.com>
2021-03-19 16:43:59 -0700

    integ-tests: Enable runtime baking test * Enable runtime baking test except slurm scheduler, there exists error in slurm scheduler for this test
    Signed-off-by: Yulei Wang <yuleiwan@amazon.com>

--- tests/integration-tests/tests/runtime_bake/test_runtime_bake/test_runtime_bake/verify_cinc_download.sh
+++ tests/integration-tests/tests/runtime_bake/test_runtime_bake/test_runtime_bake/verify_cinc_download.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 
+# get cloudformation log if it doesn't exist
+if [ ! -f "/var/log/cloud-init-output.log" ]; then
+  sudo grep cloud-init /var/log/messages | sudo tee /var/log/cloud-init-output.log > /dev/null
+fi
+
 # Verify chef.io is not called to download chef installer script or client
 sudo grep -ir "chef.io/chef/install.sh" /var/log/cloud-init-output.log
 if [ $? -eq 0 ]; then
@@ -13,9 +18,9 @@ if [ $? -eq 0 ]; then
     exit 1
 fi
 
-# Verify chef client is installed from S3
-sudo grep -ir "aws-parallelcluster/archives/chef/chef-install.sh" /var/log/cloud-init-output.log
+# Verify cinc client is installed from S3
+sudo grep -ir "archives/cinc/cinc-install.sh" /var/log/cloud-init-output.log
 if [ $? -ne 0 ]; then
-    echo "Chef installer not downloaded from S3"
+    echo "Cinc installer not downloaded from S3"
     exit 1
 fi
