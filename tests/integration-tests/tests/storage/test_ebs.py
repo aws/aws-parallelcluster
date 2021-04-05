b0570514182148a2fb96139aaced46914888db58
Hanwen <hanwenli@amazon.com>
2021-04-05 07:20:36 -0700

    Integ-tests: fix EBS tests
    1. Remove test for empty ebs section
    The test was to verify that the default shared directory is not `/NONE` if the ebs section is empty. However, in pcluster3, MountDir is required in SharedStorage, and there is a validator to verify it is not `/NONE`. In conclusion, this test can be removed.
    2. Fix test_ebs_multiple to get the correct information from the new configuration file
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- tests/integration-tests/tests/storage/test_ebs.py
+++ tests/integration-tests/tests/storage/test_ebs.py
@@ -110,27 +110,20 @@ def test_ebs_multiple(scheduler, pcluster_config_reader, clusters_factory, regio
     volume_ids = get_ebs_volume_ids(cluster, region)
     for i in range(len(volume_ids)):
         # test different volume types
-        volume_type = cluster.config.get("ebs ebs{0}".format(i + 1), "volume_type")
+        volume_type = _get_ebs_settings_by_name(cluster.config, f"ebs{i+1}")["VolumeType"]
         volume = describe_volume(volume_ids[i], region)
         assert_that(volume[0]).is_equal_to(volume_type)
         # test different iops
         # only the iops of io1 and io2 can be configured by us
         if volume_type in ["io1", "io2", "gp3"]:
-            volume_iops = cluster.config.get("ebs ebs{0}".format(i + 1), "volume_iops")
+            volume_iops = _get_ebs_settings_by_name(cluster.config, f"ebs{i+1}")["Iops"]
             assert_that(volume[1]).is_equal_to(int(volume_iops))
 
 
-@pytest.mark.dimensions("us-gov-east-1", "c5.xlarge", "ubuntu1804", "torque")
-@pytest.mark.usefixtures("region", "os", "instance")
-def test_ebs_single_empty(scheduler, pcluster_config_reader, clusters_factory):
-    cluster_config = pcluster_config_reader()
-    cluster = clusters_factory(cluster_config)
-    remote_command_executor = RemoteCommandExecutor(cluster)
-
-    mount_dir = "/shared"
-    scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
-    _test_ebs_correctly_mounted(remote_command_executor, mount_dir, volume_size=20)
-    _test_ebs_correctly_shared(remote_command_executor, mount_dir, scheduler_commands)
+def _get_ebs_settings_by_name(config, name):
+    for shared_storage in config["SharedStorage"]:
+        if shared_storage["Name"] == name:
+            return shared_storage["EbsSettings"]
 
 
 @pytest.mark.dimensions("ap-northeast-2", "c5.xlarge", "centos7", "sge")
