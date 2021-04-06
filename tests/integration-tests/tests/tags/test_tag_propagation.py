b7195cc7cbeeb61b40654836d47644e15cd6d0f1
Hanwen <hanwenli@amazon.com>
2021-04-06 13:21:58 -0700

    integ-tests: fix test_tag and remove substack properties in clusters_factory
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- tests/integration-tests/tests/tags/test_tag_propagation.py
+++ tests/integration-tests/tests/tags/test_tag_propagation.py
@@ -130,7 +130,7 @@ def get_main_stack_tags(cluster):
 
 def get_head_node_instance_id(cluster):
     """Return the given cluster's head node's instance ID."""
-    return cluster.head_node_substack_cfn_resources.get("MasterServer")
+    return cluster.cfn_resources.get("MasterServer")
 
 
 def get_ec2_instance_tags(instance_id, region):
@@ -212,7 +212,7 @@ def get_ebs_volume_tags(volume_id, region):
 
 def get_shared_volume_tags(cluster):
     """Return the given cluster's EBS volume's tags."""
-    shared_volume = cluster.ebs_substack_cfn_resources.get("Volume1")
+    shared_volume = cluster.cfn_resources.get("EBS0")
     return get_ebs_volume_tags(shared_volume, cluster.region)
 
 
