0483e17f650a16da1816cd5efd5309fe715b0d9f
Enrico Usai <usai@amazon.com>
2021-04-07 11:45:08 +0200

    Remove deprecate parameters from update command in test framework
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/clusters_factory.py
+++ tests/integration-tests/clusters_factory.py
@@ -41,22 +41,16 @@ class Cluster:
         attrs = ", ".join(["{key}={value}".format(key=key, value=repr(value)) for key, value in self.__dict__.items()])
         return "{class_name}({attrs})".format(class_name=self.__class__.__name__, attrs=attrs)
 
-    def update(self, reset_desired=False, extra_params=None, force=True):
+    def update(self, force=True):
         """
         Update a cluster with an already updated config.
-        :param reset_desired: reset the current ASG desired capacity to initial config values
-        :param extra_params: extra parameters to pass to stack update
         :param force: if to use --force flag when update
         """
         # update the cluster
         logging.info("Updating cluster {0} with config {1}".format(self.name, self.config_file))
-        command = ["pcluster", "update", "--config", self.config_file, "--yes"]
+        command = ["pcluster", "update", "--config", self.config_file]
         if force:
             command.append("--force")
-        if reset_desired:
-            command.append("--reset-desired")
-        if extra_params:
-            command.extend(["--extra-parameters", extra_params])
         command.append(self.name)
         result = run_command(command)
         if "Status: {0} - UPDATE_COMPLETE".format(self.cfn_name) not in result.stdout:
