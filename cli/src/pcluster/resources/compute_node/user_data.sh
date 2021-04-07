35dda46b75555554c08f5648c273ad8ccb0e2a70
Enrico Usai <usai@amazon.com>
2021-04-07 17:55:44 +0200

    Change "cfncluster" to "cluster" in dna.json and chef attributes
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/src/pcluster/resources/compute_node/user_data.sh
+++ cli/src/pcluster/resources/compute_node/user_data.sh
@@ -47,7 +47,7 @@ write_files:
     owner: root:root
     content: |
       {
-        "cfncluster": {
+        "cluster": {
           "stack_name": "${AWS::StackName}",
           "enable_efa": "${EnableEfa}",
           "cfn_raid_parameters": "${RAIDOptions}",
@@ -182,7 +182,7 @@ cd /tmp
 
 mkdir -p /etc/chef/ohai/hints
 touch /etc/chef/ohai/hints/ec2.json
-jq --argfile f1 /tmp/dna.json --argfile f2 /tmp/extra.json -n '$f1 + $f2 | .cfncluster = $f1.cfncluster + $f2.cfncluster' > /etc/chef/dna.json || ( echo "jq not installed or invalid extra_json"; cp /tmp/dna.json /etc/chef/dna.json)
+jq --argfile f1 /tmp/dna.json --argfile f2 /tmp/extra.json -n '$f1 + $f2 | .cluster = $f1.cluster + $f2.cluster' > /etc/chef/dna.json || ( echo "jq not installed or invalid extra_json"; cp /tmp/dna.json /etc/chef/dna.json)
 {
   pushd /etc/chef &&
   chef-client --local-mode --config /etc/chef/client.rb --log_level auto --force-formatter --no-color --chef-zero-port 8889 --json-attributes /etc/chef/dna.json --override-runlist aws-parallelcluster::prep_env &&
