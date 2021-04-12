8243a146cdea320e3f030cda49c6f0dd7f645b34
Francesco De Martino <fdm@amazon.com>
2021-04-12 15:09:46 +0200

    Add API Readme file
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- api/spec/run-swagger-ui.sh
+++ api/spec/run-swagger-ui.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-docker run -p 8080:8080 -e URL=docs/ParallelCluster.openapi.yaml -v "$(pwd)/openapi":/usr/share/nginx/html/docs/ swaggerapi/swagger-ui
+docker run -p 8080:8080 -e URL=docs/ParallelCluster.openapi.yaml --name pcluster-swagger-ui -v "$(pwd)/openapi":/usr/share/nginx/html/docs/ swaggerapi/swagger-ui
