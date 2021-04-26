9dc0ab25dc831df42f8b1dcdace4f19957ef6c51
Luca Carrogu <carrogu@amazon.com>
2021-04-26 16:17:42 +0200

    Disable pcluster tag component if pcluster main component is disabled
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- cli/src/pcluster/templates/imagebuilder_stack.py
+++ cli/src/pcluster/templates/imagebuilder_stack.py
@@ -432,34 +432,35 @@ class ImageBuilderCdkStack(Stack):
         if self.config.build.components:
             self._add_custom_components(components, lambda_cleanup_policy_statements, components_resources)
 
-        tag_component_resource = imagebuilder.CfnComponent(
-            self,
-            id="TagComponent",
-            name=self._build_resource_name(RESOURCE_NAME_PREFIX + "-Tag"),
-            version=utils.get_installed_version(),
-            tags=build_tags,
-            description="Tag ParallelCluster AMI",
-            platform="Linux",
-            data=_load_yaml(imagebuilder_resources_dir, "parallelcluster_tag.yaml"),
-        )
-        components.append(
-            imagebuilder.CfnImageRecipe.ComponentConfigurationProperty(component_arn=Fn.ref("TagComponent"))
-        )
-        components_resources.append(tag_component_resource)
-        if not self.custom_cleanup_lambda_role:
-            self._add_resource_delete_policy(
-                lambda_cleanup_policy_statements,
-                ["imagebuilder:DeleteComponent"],
-                [
-                    self.format_arn(
-                        service="imagebuilder",
-                        resource="component",
-                        resource_name="{0}/*".format(
-                            self._build_resource_name(RESOURCE_NAME_PREFIX + "-Tag", to_lower=True)
-                        ),
-                    )
-                ],
+        if not disable_pcluster_component:
+            tag_component_resource = imagebuilder.CfnComponent(
+                self,
+                id="TagComponent",
+                name=self._build_resource_name(RESOURCE_NAME_PREFIX + "-Tag"),
+                version=utils.get_installed_version(),
+                tags=build_tags,
+                description="Tag ParallelCluster AMI",
+                platform="Linux",
+                data=_load_yaml(imagebuilder_resources_dir, "parallelcluster_tag.yaml"),
             )
+            components.append(
+                imagebuilder.CfnImageRecipe.ComponentConfigurationProperty(component_arn=Fn.ref("TagComponent"))
+            )
+            components_resources.append(tag_component_resource)
+            if not self.custom_cleanup_lambda_role:
+                self._add_resource_delete_policy(
+                    lambda_cleanup_policy_statements,
+                    ["imagebuilder:DeleteComponent"],
+                    [
+                        self.format_arn(
+                            service="imagebuilder",
+                            resource="component",
+                            resource_name="{0}/*".format(
+                                self._build_resource_name(RESOURCE_NAME_PREFIX + "-Tag", to_lower=True)
+                            ),
+                        )
+                    ],
+                )
 
         return components, components_resources
 
