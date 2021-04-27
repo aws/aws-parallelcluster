f7579b2c353579cebbc079962436e42885638cc0
Francesco De Martino <fdm@amazon.com>
2021-04-27 10:29:18 +0200

    Improve logging and error handling in flask app
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cli/src/pcluster/api/flask_app.py
+++ cli/src/pcluster/api/flask_app.py
@@ -5,17 +5,19 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
-
 import json
+import logging
 
 import connexion
 from connexion import ProblemException
 from connexion.decorators.validation import ParameterValidator
-from flask import Response
+from flask import Response, request
 from werkzeug.exceptions import HTTPException
 
 from pcluster.api import encoder
 from pcluster.api.errors import ParallelClusterApiException
+
+LOGGER = logging.getLogger(__name__)
 
 
 class CustomParameterValidator(ParameterValidator):
@@ -46,9 +48,10 @@ class ParallelClusterFlaskApp:
     def __init__(self, swagger_ui: bool = False, validate_responses=False):
         options = {"swagger_ui": swagger_ui}
 
-        app = connexion.FlaskApp(__name__, specification_dir="openapi/", skip_error_handlers=True)
-        app.app.json_encoder = encoder.JSONEncoder
-        app.add_api(
+        self.app = connexion.FlaskApp(__name__, specification_dir="openapi/", skip_error_handlers=True)
+        self.flask_app = self.app.app
+        self.flask_app.json_encoder = encoder.JSONEncoder
+        self.app.add_api(
             "openapi.yaml",
             arguments={"title": "ParallelCluster"},
             pythonic_params=True,
@@ -56,35 +59,78 @@ class ParallelClusterFlaskApp:
             validate_responses=validate_responses,
             validator_map={"parameter": CustomParameterValidator},
         )
-        app.add_error_handler(HTTPException, self._render_http_exception)
-        app.add_error_handler(ProblemException, self._render_problem_exception)
-        app.add_error_handler(ParallelClusterApiException, self._render_parallel_cluster_api_exception)
-        self.app = app
+        self.app.add_error_handler(HTTPException, self._handle_http_exception)
+        self.app.add_error_handler(ProblemException, self._handle_problem_exception)
+        self.app.add_error_handler(ParallelClusterApiException, self._handle_parallel_cluster_api_exception)
+        self.app.add_error_handler(Exception, self._handle_unexpected_exception)
+
+        @self.flask_app.before_request
+        def _log_request():  # pylint: disable=unused-variable
+            LOGGER.info("Handling request: %s %s", request.method, request.full_path)
+
+        @self.flask_app.after_request
+        def _log_response(response: Response):  # pylint: disable=unused-variable
+            LOGGER.info(
+                "Responding with status code %s to request %s %s",
+                response.status_code,
+                request.method,
+                request.full_path,
+            )
+            return response
 
     @staticmethod
-    def _render_http_exception(exception: HTTPException):
+    def _handle_http_exception(exception: HTTPException):
         """Render a HTTPException according to ParallelCluster API specs."""
+        ParallelClusterFlaskApp._log_exception(exception.code, exception.description)
         return Response(
             response=json.dumps({"message": exception.description}), status=exception.code, mimetype="application/json"
         )
 
     @staticmethod
-    def _render_problem_exception(exception: ProblemException):
+    def _handle_problem_exception(exception: ProblemException):
         """Render a ProblemException according to ParallelCluster API specs."""
         # Connexion does not return a clear error message on missing request body
         if "None is not of type 'object'" in exception.detail:
             exception.detail = "request body is required"
+        message = f"{exception.title}: {exception.detail}"
+        ParallelClusterFlaskApp._log_exception(exception.status, message)
         return Response(
-            response=json.dumps({"message": f"{exception.title}: {exception.detail}"}),
+            response=json.dumps({"message": message}),
             status=exception.status,
             mimetype="application/json",
         )
 
     @staticmethod
-    def _render_parallel_cluster_api_exception(exception: ParallelClusterApiException):
+    def _handle_parallel_cluster_api_exception(exception: ParallelClusterApiException):
         """Render a ParallelClusterApiException according to ParallelCluster API specs."""
+        ParallelClusterFlaskApp._log_exception(exception.code, exception.content)
         return Response(
             response=json.dumps(exception.content.to_dict()), status=exception.code, mimetype="application/json"
+        )
+
+    @staticmethod
+    def _handle_unexpected_exception(exception: Exception):
+        """Handle an unexpected exception."""
+        LOGGER.critical("Unexpected exception: %s", exception, exc_info=True)
+        return Response(
+            response=json.dumps(
+                {
+                    "message": "Unexpected fatal exception. "
+                    "Please look at the application logs for details on the encountered failure."
+                }
+            ),
+            status=500,
+            mimetype="application/json",
+        )
+
+    @staticmethod
+    def _log_exception(code, message):
+        LOGGER.log(
+            logging.ERROR if code >= 500 else logging.INFO,
+            "Handling exception (status code %s): %s",
+            code,
+            message,
+            exc_info=code >= 500,
         )
 
     def start_local_server(self, port: int = 8080, debug: bool = False):
@@ -93,4 +139,5 @@ class ParallelClusterFlaskApp:
 
 
 if __name__ == "__main__":
-    ParallelClusterFlaskApp(swagger_ui=True, validate_responses=False).start_local_server(debug=False)
+    logging.basicConfig(level=logging.INFO)
+    ParallelClusterFlaskApp(swagger_ui=True, validate_responses=True).start_local_server(debug=True)
