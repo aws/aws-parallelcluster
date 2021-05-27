e0ecfdf8be92da809b997dd367413df01fc88ae7
Luca Carrogu <carrogu@amazon.com>
2021-05-27 17:58:29 +0200

    Increase reserved root volume size
    This is the value added to the root volume size of the parent image when volume size isn't specified into the config
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- cli/tests/pcluster/templates/test_imagebuilder_stack.py
+++ cli/tests/pcluster/templates/test_imagebuilder_stack.py
@@ -2636,7 +2636,7 @@ def test_imagebuilder_distribution_configuraton(mocker, resource, response, expe
                     }
                 ],
             },
-            {"Encrypted": False, "VolumeSize": 23, "VolumeType": "gp2"},
+            {"Encrypted": False, "VolumeSize": 35, "VolumeType": "gp2"},
         ),
         (
             {
@@ -2718,7 +2718,7 @@ def test_imagebuilder_distribution_configuraton(mocker, resource, response, expe
             },
             {
                 "Encrypted": True,
-                "VolumeSize": 65,
+                "VolumeSize": 77,
                 "VolumeType": "gp2",
                 "KmsKeyId": "arn:aws:kms:us-east-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab",
             },
