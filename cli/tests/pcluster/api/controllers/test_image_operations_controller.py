49e6867bdc47e2ed84030b9f9b486ffb427db9a7
Ermanno Moser <ermann@amazon.com>
2021-06-01 02:39:34 +0200

    Implement DeleteImage API
    This patch implements the DeleteImage API. We create an ImageBuilder
    class with the given image id and retrieve the associated image or
    stack, then we delete the image and, in case we only had the stack, we
    retrieve it with another ImageBuilder object (the stack is initialized
    only once, and if we tried to retrieve it after calling the delete
    operation we might be unable to describe it as it might have already
    transitioned to the DELETE_COMPLETE state).
    
    One thing to note here is that the ImageBuilder.delete() call might fail
    raising an ImageBuilderActionError, which is surfaced to the caller of
    the DeleteImage API as a 500 with the message: "Unexpected fatal
    exception. Please look at the application logs for details on the
    encountered failure."

--- cli/tests/pcluster/api/controllers/test_image_operations_controller.py
+++ cli/tests/pcluster/api/controllers/test_image_operations_controller.py
@@ -7,10 +7,15 @@
 #  limitations under the License.
 import json
 
-from assertpy import assert_that
+import pytest
+from assertpy import assert_that, soft_assertions
 
+from pcluster.api.models import CloudFormationStatus
 from pcluster.api.models.image_build_status import ImageBuildStatus
 from pcluster.api.models.validation_level import ValidationLevel
+from pcluster.aws.aws_resources import ImageInfo
+from pcluster.aws.common import ImageNotFoundError, StackNotFoundError
+from pcluster.models.imagebuilder import ImageBuilderActionError
 
 
 class TestImageOperationsController:
@@ -40,20 +45,6 @@ class TestImageOperationsController:
             headers=headers,
             data=json.dumps(build_image_request_content),
             content_type="application/json",
-            query_string=query_string,
-        )
-        assert_that(response.status_code).is_equal_to(200)
-
-    def test_delete_image(self, client):
-        """Test case for delete_image."""
-        query_string = [("region", "eu-west-1"), ("clientToken", "client_token_example"), ("force", True)]
-        headers = {
-            "Accept": "application/json",
-        }
-        response = client.open(
-            "/v3/images/custom/{image_id}".format(image_id="imageid"),
-            method="DELETE",
-            headers=headers,
             query_string=query_string,
         )
         assert_that(response.status_code).is_equal_to(200)
@@ -99,3 +90,190 @@ class TestImageOperationsController:
         }
         response = client.open("/v3/images/custom", method="GET", headers=headers, query_string=query_string)
         assert_that(response.status_code).is_equal_to(200)
+
+
+class TestDeleteImage:
+    url = "/v3/images/custom/{image_name}"
+    method = "DELETE"
+
+    def _send_test_request(self, client, image_name, region="us-east-1", force=True):
+        query_string = [
+            ("force", force),
+            ("region", region),
+        ]
+        headers = {
+            "Accept": "application/json",
+        }
+        return client.open(
+            self.url.format(image_name=image_name), method=self.method, headers=headers, query_string=query_string
+        )
+
+    def _create_image_info(self, image_id):
+        return ImageInfo(
+            {
+                "Tags": [
+                    {"Key": "parallelcluster:image_id", "Value": image_id},
+                    {"Key": "parallelcluster:version", "Value": "3.0.0"},
+                ],
+            }
+        )
+
+    def _create_stack(self, image_id, status):
+        return {
+            "StackId": "arn:{}".format(image_id),
+            "StackStatus": status,
+            "Tags": [
+                {"Key": "parallelcluster:image_id", "Value": image_id},
+                {"Key": "parallelcluster:version", "Value": "3.0.0"},
+            ],
+        }
+
+    def _assert_successful(
+        self, mocker, client, image_id, force, region, image, stack, expected_response, stack_after_deletion=None
+    ):
+        if image:
+            mocker.patch("pcluster.aws.ec2.Ec2Client.describe_image_by_id_tag", return_value=image)
+        else:
+            mocker.patch(
+                "pcluster.aws.ec2.Ec2Client.describe_image_by_id_tag",
+                side_effect=ImageNotFoundError("describe_image_by_id_tag"),
+            )
+
+        if stack:
+            if stack_after_deletion:
+                mocker.patch("pcluster.aws.cfn.CfnClient.describe_stack", side_effect=[stack, stack_after_deletion])
+            else:
+                mocker.patch(
+                    "pcluster.aws.cfn.CfnClient.describe_stack",
+                    side_effect=[stack, StackNotFoundError("describe_stack", "stack_name")],
+                )
+        else:
+            mocker.patch(
+                "pcluster.aws.cfn.CfnClient.describe_stack",
+                side_effect=StackNotFoundError("describe_stack", "stack_name"),
+            )
+
+        mocker.patch("pcluster.models.imagebuilder.ImageBuilder.delete", return_value=None)
+
+        response = self._send_test_request(client, image_id, region, force)
+
+        with soft_assertions():
+            assert_that(response.status_code).is_equal_to(200)
+            assert_that(response.get_json()).is_equal_to(expected_response)
+
+    def test_delete_available_ec2_image_with_stack_yet_to_be_removed_succeeds(self, mocker, client):
+        image = self._create_image_info("image1")
+        stack = self._create_stack("image1", CloudFormationStatus.DELETE_IN_PROGRESS)
+        expected_response = {
+            "image": {
+                "imageId": "image1",
+                "imageBuildStatus": ImageBuildStatus.DELETE_COMPLETE,
+                "region": "us-east-1",
+                "version": "3.0.0",
+            }
+        }
+        self._assert_successful(mocker, client, "image1", True, "us-east-1", image, stack, expected_response)
+
+    def test_delete_available_ec2_image_with_stack_already_removed_succeeds(self, mocker, client):
+        image = self._create_image_info("image1")
+        expected_response = {
+            "image": {
+                "imageId": "image1",
+                "imageBuildStatus": ImageBuildStatus.DELETE_COMPLETE,
+                "region": "us-east-1",
+                "version": "3.0.0",
+            }
+        }
+        self._assert_successful(mocker, client, "image1", True, "us-east-1", image, None, expected_response)
+
+    def test_delete_image_with_only_stack_and_no_available_image_succeeds(self, mocker, client):
+        stack = self._create_stack("image1", CloudFormationStatus.DELETE_FAILED)
+        stack_after_deletion = self._create_stack("image1", CloudFormationStatus.DELETE_IN_PROGRESS)
+        expected_response = {
+            "image": {
+                "imageId": "image1",
+                "imageBuildStatus": ImageBuildStatus.DELETE_IN_PROGRESS,
+                "region": "us-east-1",
+                "version": "3.0.0",
+                "cloudformationStackStatus": CloudFormationStatus.DELETE_IN_PROGRESS,
+                "cloudformationStackArn": "arn:image1",
+            }
+        }
+        self._assert_successful(
+            mocker, client, "image1", True, "us-east-1", None, stack, expected_response, stack_after_deletion
+        )
+
+    def test_delete_image_with_only_stack_when_stack_is_deleted_immediately(self, mocker, client):
+        stack = self._create_stack("image1", CloudFormationStatus.DELETE_FAILED)
+        expected_response = {
+            "image": {
+                "imageId": "image1",
+                "imageBuildStatus": ImageBuildStatus.DELETE_COMPLETE,
+                "region": "us-east-1",
+                "version": "3.0.0",
+                "cloudformationStackStatus": CloudFormationStatus.DELETE_COMPLETE,
+                "cloudformationStackArn": "arn:image1",
+            }
+        }
+        self._assert_successful(
+            mocker, client, "image1", True, "us-east-1", None, stack, expected_response, stack_after_deletion=None
+        )
+
+    def test_delete_image_with_no_available_image_or_stack_throws_not_found_exception(self, mocker, client):
+        mocker.patch(
+            "pcluster.aws.ec2.Ec2Client.describe_image_by_id_tag",
+            side_effect=ImageNotFoundError("describe_image_by_id_tag"),
+        )
+        mocker.patch(
+            "pcluster.aws.cfn.CfnClient.describe_stack", side_effect=StackNotFoundError("describe_stack", "stack_name")
+        )
+        response = self._send_test_request(client, "nonExistentImage")
+
+        with soft_assertions():
+            assert_that(response.status_code).is_equal_to(404)
+            assert_that(response.get_json()).is_equal_to(
+                {"message": "Unable to find an image of stack with id: nonExistentImage"}
+            )
+
+    @pytest.mark.parametrize(
+        "region, image_id, force, expected_response",
+        [
+            pytest.param(
+                "us-east-",
+                "imageId",
+                True,
+                {"message": "Bad Request: invalid or unsupported region 'us-east-'"},
+                id="bad_region",
+            ),
+            pytest.param(None, "imageId", True, {"message": "Bad Request: region needs to be set"}, id="unset_region"),
+            pytest.param(
+                "us-east-1",
+                "_malformedImageId",
+                True,
+                {"message": "Bad Request: '_malformedImageId' does not match '^[a-zA-Z][a-zA-Z0-9-]+$'"},
+                id="invalid_image_id",
+            ),
+        ],
+    )
+    def test_malformed_request(self, client, region, image_id, force, expected_response):
+        response = self._send_test_request(client, image_id, region, force)
+
+        with soft_assertions():
+            assert_that(response.status_code).is_equal_to(400)
+            assert_that(response.get_json()).is_equal_to(expected_response)
+
+    def test_that_a_server_internal_error_is_thrown_if_the_delete_fails(self, client, mocker):
+        image = self._create_image_info("image1")
+        mocker.patch("pcluster.aws.ec2.Ec2Client.describe_image_by_id_tag", return_value=image)
+        mocker.patch(
+            "pcluster.models.imagebuilder.ImageBuilder.delete", side_effect=ImageBuilderActionError("test error")
+        )
+        expected_error = {
+            "message": "Unexpected fatal exception. Please look "
+            "at the application logs for details on the encountered failure."
+        }
+        response = self._send_test_request(client, "image1")
+
+        with soft_assertions():
+            assert_that(response.status_code).is_equal_to(500)
+            assert_that(response.get_json()).is_equal_to(expected_error)
