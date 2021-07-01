603bc49529e312689011543467140f4a854ce4a1
Giacomo Marciani <mgiacomo@amazon.com>
2021-07-01 19:19:47 +0200

    SQUASHME Remove explicit cluster deletion at the end of the test

--- tests/integration-tests/tests/multi-user/test_multi_user.py
+++ tests/integration-tests/tests/multi-user/test_multi_user.py
@@ -202,9 +202,6 @@ def test_create_multi_user_with_opsworks(
     # TODO add assertions about ssh access and job submission
     # assert_ssh_login(cluster=cluster, username=ssh_user_name, ssh_key=private_key_path)
 
-    # Need to delete the cluster before deleting OpsWorks stack
-    # cluster.delete()
-
 
 def _get_head_node_instance(cluster):
     return (
