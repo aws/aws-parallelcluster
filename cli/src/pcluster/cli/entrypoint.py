f0f123fa8e4808bb95a5ade6c552fa5c44a5ef55
Charlie Gruenwald <cgruenwa@amazon.com>
2021-07-09 10:40:46 -0600

    [CLI] pylint: disable import outside of top level warning.

--- cli/src/pcluster/cli/entrypoint.py
+++ cli/src/pcluster/cli/entrypoint.py
@@ -11,6 +11,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
 # implied. See the License for the specific language governing permissions and
 # limitations under the License.
+# pylint: disable=import-outside-toplevel
 
 import argparse
 from botocore.exceptions import NoCredentialsError  # TODO: remove
