8a88d6fa5a3dc772dcf72a273242045c5bad6d76
Charlie Gruenwald <cgruenwa@amazon.com>
2021-07-20 22:09:51 -0600

    [CLI/test] remove unused import.

--- cli/tests/pcluster/cli/test_model.py
+++ cli/tests/pcluster/cli/test_model.py
@@ -6,8 +6,6 @@
 #  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import base64
-
 import pytest
 from assertpy import assert_that
 
