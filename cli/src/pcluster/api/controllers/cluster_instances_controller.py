449839984ff36363a4abee52162fb6f38e51e8d6
Charlie Gruenwald <cgruenwa@amazon.com>
2021-07-22 13:52:56 -0600

    [API] Align timestamp formats.

--- cli/src/pcluster/api/controllers/cluster_instances_controller.py
+++ cli/src/pcluster/api/controllers/cluster_instances_controller.py
@@ -18,6 +18,7 @@ from pcluster.api.models import ClusterInstance, DescribeClusterInstancesRespons
 from pcluster.api.models import NodeType as ApiNodeType
 from pcluster.aws.common import StackNotFoundError
 from pcluster.models.cluster import Cluster, NodeType
+from pcluster.utils import to_iso_time
 
 # pylint: disable=W0613
 
@@ -84,7 +85,7 @@ def describe_cluster_instances(cluster_name, region=None, next_token=None, node_
         ec2_instances.append(
             ClusterInstance(
                 instance_id=instance.id,
-                launch_time=instance.launch_time,
+                launch_time=to_iso_time(instance.launch_time),
                 public_ip_address=instance.public_ip,
                 instance_type=instance.instance_type,
                 state=instance.state,
