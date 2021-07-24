fe8885fed386f3d505dc13673dc3879fa911a835
Hanwen <hanwenli@amazon.com>
2021-07-24 14:17:21 -0700

    integ-tests: test CLI commands
    * Test `create-cluster` (`--dryrun`, `--suppress-validators`, `--validation-failure-level`, error response, successful response).
    
    * Add wait (default is True) parameter on tests. Therefore we can test the response with `wait==False`. (Note that when `wait==True`, the response is always from describe-cluster, so we need to set it to `False` if we want to test the response for create/update/delete
    
    * Split `_test_pcluster_instances_and_status` to `_test_describe_instances` and `_test_pcluster_compute_fleet` to make testing function names to be consistent with the commands under testing
    
    * Other minor renaming
    
    * Fix retrieval of `cfn_stack_arn` in `class Cluster`: Previous code could not retrieve the arn if stack creation failed. The `cfn_stack_arn` is used to clean up resources according to stack-id tag
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- tests/integration-tests/tests/cli_commands/test_cli_commands.py
+++ tests/integration-tests/tests/cli_commands/test_cli_commands.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and limitations under the License.
 import json
 import logging
+import re
 import tarfile
 import tempfile
 
@@ -19,43 +20,263 @@ import botocore
 import pytest
 from assertpy import assert_that
 from remote_command_executor import RemoteCommandExecutor
-from utils import check_status, get_cluster_nodes_instance_ids
+from utils import check_status, get_cluster_nodes_instance_ids, run_command
 
 from tests.common.assertions import assert_no_errors_in_logs, wait_for_num_instances_in_cluster
+from tests.common.utils import get_installed_parallelcluster_version, retrieve_latest_ami
 
 
 @pytest.mark.regions(["us-east-2"])
 @pytest.mark.instances(["c5.xlarge"])
 @pytest.mark.schedulers(["slurm"])
 @pytest.mark.oss(["ubuntu1804"])
-@pytest.mark.usefixtures("region", "os", "instance")
-def test_slurm_cli_commands(scheduler, region, pcluster_config_reader, clusters_factory, s3_bucket_factory):
+@pytest.mark.usefixtures("region", "instance")
+def test_slurm_cli_commands(
+    request, scheduler, region, os, pcluster_config_reader, clusters_factory, s3_bucket_factory
+):
     """Test pcluster cli commands are working."""
     # Use long scale down idle time so we know nodes are terminated by pcluster stop
     cluster_config = pcluster_config_reader(scaledown_idletime=60)
-    cluster = clusters_factory(cluster_config)
+    # Generate configuration with warnings and errors
+    custom_ami = retrieve_latest_ami(
+        region, os, ami_type="official", architecture="x86_64"
+    )  # Using custom AMI not tagged by pcluser will generate a warning
+    cluster_config_with_warning = pcluster_config_reader(
+        config_file="pcluster.config.with.warnings.yaml", custom_ami=custom_ami
+    )
+    cluster = _test_create_cluster(clusters_factory, cluster_config, cluster_config_with_warning, request)
     remote_command_executor = RemoteCommandExecutor(cluster)
+    _test_list_cluster(cluster.name, "CREATE_COMPLETE")
+    _test_create_or_update_with_warnings(str(cluster_config_with_warning), cluster=cluster)
 
-    instance_ids = _test_pcluster_instances_and_status(cluster, region, compute_fleet_status="RUNNING")
-    _test_pcluster_stop_and_start(cluster, region, expected_num_nodes=2)
+    instance_ids = _test_describe_instances(cluster, region)
+    check_status(cluster, "CREATE_COMPLETE", "running", "RUNNING")
+    _test_describe_instances(cluster, region, node_type="HeadNode")
+    _test_describe_instances(cluster, region, node_type="Compute")
+    _test_describe_instances(cluster, region, queue_name="ondemand1")
+    _test_pcluster_compute_fleet(cluster, region, expected_num_nodes=2)
     _test_pcluster_export_cluster_logs(s3_bucket_factory, cluster, region, instance_ids)
     cfn_init_log_stream = _test_pcluster_list_cluster_logs(cluster, instance_ids)
     _test_pcluster_get_cluster_log_events(cluster, cfn_init_log_stream)
     assert_no_errors_in_logs(remote_command_executor, scheduler)
 
 
-def _test_pcluster_instances_and_status(cluster, region, compute_fleet_status=None):
-    """Test pcluster status and pcluster instances functionalities."""
-    logging.info("Testing that pcluster status and pcluster instances output are expected")
-    cluster_instances_from_ec2 = get_cluster_nodes_instance_ids(cluster.cfn_name, region)
-    cluster_instances_from_cli = cluster.instances()
+def _test_create_cluster(clusters_factory, cluster_config, cluster_config_with_warning, request):
+    if not request.config.getoption("cluster"):
+        # Test below is not compatible with `--cluster` flag. Therefore, skip it if the flag is provided.
+        _test_create_or_update_with_warnings(cluster_config_with_warning, clusters_factory)
+
+    cluster = clusters_factory(cluster_config, wait=False)
+    if not request.config.getoption("cluster"):
+        expected_creation_response = {
+            "cluster": {
+                "clusterName": cluster.name,
+                "cloudformationStackStatus": "CREATE_IN_PROGRESS",
+                "cloudformationStackArn": cluster.cfn_stack_arn,
+                "region": cluster.region,
+                "version": get_installed_parallelcluster_version(),
+                "clusterStatus": "CREATE_IN_PROGRESS",
+            }
+        }
+        assert_that(cluster.creation_response).is_equal_to(expected_creation_response)
+        _test_list_cluster(cluster.name, "CREATE_IN_PROGRESS")
+        logging.info("Waiting for CloudFormation stack creation completion")
+        cloud_formation = boto3.client("cloudformation")
+        waiter = cloud_formation.get_waiter("stack_create_complete")
+        waiter.wait(StackName=cluster.name)
+    return cluster
+
+
+def _test_create_or_update_with_warnings(cluster_config_with_warning, clusters_factory=None, cluster=None):
+    """
+    Test create-cluster or update-cluster with a erroneous configuration file.
+
+    If clusters_factory is not None, this function tests create-cluster.
+    Otherwise cluster has to be provided and this function tests update-cluster.
+    """
+    # clusters_factory and cluster are mutually exclusive but one of them has to be provided
+    assert_that(clusters_factory or cluster).is_not_none()
+    logging.info("Testing cluster creation/update on a configuration file with 1 error and 2 warnings.")
+    custom_ami_tag_warning = {
+        "level": "WARNING",
+        "type": "CustomAmiTagValidator",
+        "message": "The custom AMI may not.*been created by pcluster",
+    }
+    name_error = {
+        "level": "ERROR",
+        "type": "NameValidator",
+        "message": "Name must begin with a letter and only contain lowercase letters, digits and hyphens.",
+    }
+    key_pair_warning = {
+        "level": "WARNING",
+        "type": "KeyPairValidator",
+        "message": ".*you do not specify a key pair.*",
+    }
+
+    test_cases = []
+
+    def construct_validation_error_expected_response(configuration_validation_errors):
+        return {
+            "configurationValidationErrors": configuration_validation_errors,
+            "message": "Invalid cluster configuration",
+        }
+
+    expected_response = construct_validation_error_expected_response(
+        [custom_ami_tag_warning, key_pair_warning, name_error]
+    )
+    test_cases.extend(
+        [
+            (expected_response, ["--validation-failure-level", "WARNING"]),
+            (expected_response, ["--validation-failure-level", "WARNING", "--dryrun", "true"]),
+            # Test default --validation-failure-level shows errors and warnings
+            (expected_response, None),
+        ]
+    )
+
+    # Test suppressing a error and a warning
+    expected_response = construct_validation_error_expected_response([key_pair_warning])
+    test_cases.append(
+        (
+            expected_response,
+            [
+                "--validation-failure-level",
+                "WARNING",
+                "--dryrun",
+                "true",
+                "--suppress-validators",
+                "type:CustomAmiTagValidator",
+                "type:NameValidator",
+            ],
+        )
+    )
+
+    # Test dry run with successful validations
+    # Test default --validation-failure-level does not fail on warnings
+    expected_response = {"message": "Request would have succeeded, but DryRun flag is set."}
+    test_cases.extend(
+        [
+            (expected_response, ["--dryrun", "true", "--suppress-validators", "type:NameValidator"]),
+            (
+                expected_response,
+                [
+                    "--validation-failure-level",
+                    "WARNING",
+                    "--dryrun",
+                    "true",
+                    "--suppress-validators",
+                    "type:CustomAmiTagValidator",
+                    "type:NameValidator",
+                    "type:KeyPairValidator",
+                ],
+            ),
+            # Test suppressing all validators
+            (
+                expected_response,
+                ["--validation-failure-level", "WARNING", "--dryrun", "true", "--suppress-validators", "ALL"],
+            ),
+        ]
+    )
+    for test_case in test_cases:
+        _check_response(
+            cluster_config_with_warning,
+            test_case[0],
+            extra_args=test_case[1],
+            clusters_factory=clusters_factory,
+            cluster=cluster,
+        )
+
+
+def _create_cluster_with_warnings(clusters_factory, cluster_config, extra_args=None):
+    """
+    Create cluster with warnings expected.
+
+    Set log_error=False and raise_on_error=False if error is expected.
+    Therefore, the test log won't be flooded with errors.
+    """
+    return clusters_factory(
+        cluster_config, extra_args=extra_args, raise_on_error=False, wait=False, log_error=False
+    ).creation_response
+
+
+def _check_response(cluster_config, expected_response, clusters_factory=None, cluster=None, extra_args=None):
+    if clusters_factory:
+        actual_response = _create_cluster_with_warnings(
+            clusters_factory,
+            cluster_config,
+            extra_args=extra_args,
+        )
+    else:
+        actual_response = cluster.update(
+            cluster_config, extra_args=extra_args, wait=False, log_error=False, raise_on_error=False
+        )
+    expected_validation_errors = expected_response.get("configurationValidationErrors")
+    if expected_validation_errors:
+        # If validation error is expected. The code below checks for the validation error without enforcing the order
+        # of errors and warnings and use Regex to match the error message
+        actual_validation_errors = actual_response["configurationValidationErrors"]
+        assert_that(actual_validation_errors).is_length(len(expected_validation_errors))
+        for actual_error in actual_validation_errors:
+            for expected_error in expected_validation_errors:
+                if actual_error["type"] == expected_error["type"]:
+                    # Either check the regex of expected_message match actual failure or check the
+                    # whole strings are equal. This is to deal with strings having regex symbols (e.g. "[") inside
+                    assert_that(
+                        re.search(expected_error["message"], actual_error["message"])
+                        or expected_error["message"] == actual_error["message"]
+                    )
+                    assert_that(actual_error["level"]).is_equal_to(expected_error["level"])
+    else:
+        # Otherwise, assert the actual response is exactly as expected
+        assert_that(actual_response).is_equal_to(expected_response)
+
+
+def _test_list_cluster(cluster_name, expected_status):
+    # Test the command response contains the cluster_name and expected_status.
+    # ToDo: design test for this command to check stacks inside a region.
+    #  It is hard because the test will be dependent on other tests
+    logging.info("Testing list clusters")
+    cmd_args = ["pcluster", "list-clusters"]
+    found_cluster = _find_cluster_with_pagination(cmd_args, cluster_name)
+    assert_that(found_cluster).is_not_none()
+    assert_that(found_cluster["cloudformationStackStatus"]).is_equal_to(expected_status)
+
+    logging.info("Testing list clusters with status filter")
+    cmd_args.extend(["--cluster-status", expected_status])
+    found_cluster = _find_cluster_with_pagination(cmd_args, cluster_name)
+    assert_that(found_cluster).is_not_none()
+    assert_that(found_cluster["cloudformationStackStatus"]).is_equal_to(expected_status)
+
+
+def _find_cluster_with_pagination(cmd_args, cluster_name):
+    result = run_command(cmd_args)
+    response = json.loads(result.stdout)
+    found_cluster = _find_cluster_in_list(cluster_name, response["items"])
+    while response.get("nextToken") and found_cluster is None:
+        cmd_args_with_next_token = cmd_args + ["--next-token", response["nextToken"]]
+        result = run_command(cmd_args_with_next_token)
+        response = json.loads(result.stdout)
+        found_cluster = _find_cluster_in_list(cluster_name, response["items"])
+    return found_cluster
+
+
+def _find_cluster_in_list(cluster_name, cluster_list):
+    for cluster in cluster_list:
+        if cluster_name == cluster["clusterName"]:
+            return cluster
+
+
+def _test_describe_instances(cluster, region, node_type=None, queue_name=None):
+    logging.info("Testing the result from describe-cluster-instances is the same as calling boto3 directly.")
+    cluster_instances_from_ec2 = get_cluster_nodes_instance_ids(
+        cluster.cfn_name, region, node_type=node_type, queue_name=queue_name
+    )
+    cluster_instances_from_cli = cluster.get_cluster_instance_ids(node_type=node_type, queue_name=queue_name)
     assert_that(set(cluster_instances_from_cli)).is_equal_to(set(cluster_instances_from_ec2))
-    check_status(cluster, "CREATE_COMPLETE", "running", compute_fleet_status)
     return cluster_instances_from_cli
 
 
-def _test_pcluster_stop_and_start(cluster, region, expected_num_nodes):
-    """Test pcluster start and stop functionality."""
+def _test_pcluster_compute_fleet(cluster, region, expected_num_nodes):
+    """Test pcluster compute fleet commands."""
     logging.info("Testing pcluster stop functionalities")
     cluster.stop()
     # Sample pcluster stop output:
@@ -64,7 +285,10 @@ def _test_pcluster_stop_and_start(cluster, region, expected_num_nodes):
     # Please run 'pcluster status' if you need to check compute fleet status
 
     wait_for_num_instances_in_cluster(cluster.cfn_name, region, desired=0)
-    _test_pcluster_instances_and_status(cluster, region, compute_fleet_status="STOPPED")
+    _test_describe_instances(cluster, region)
+    compute_fleet = cluster.describe_compute_fleet()
+    assert_that(compute_fleet["status"]).is_equal_to("STOPPED")
+    last_stop_time = compute_fleet["lastStatusUpdatedTime"]
 
     logging.info("Testing pcluster start functionalities")
     # Do a complicated sequence of start and stop and see if commands will still work
@@ -72,8 +296,13 @@ def _test_pcluster_stop_and_start(cluster, region, expected_num_nodes):
     cluster.stop()
     cluster.stop()
     cluster.start()
+    compute_fleet = cluster.describe_compute_fleet()
+    last_start_time = compute_fleet["lastStatusUpdatedTime"]
+    logging.info("Checking last status update time is updated")
+    assert_that(last_stop_time < last_start_time)
     wait_for_num_instances_in_cluster(cluster.cfn_name, region, desired=expected_num_nodes)
-    _test_pcluster_instances_and_status(cluster, region, compute_fleet_status="RUNNING")
+    _test_describe_instances(cluster, region)
+    check_status(cluster, "CREATE_COMPLETE", "running", "RUNNING")
 
 
 def _test_pcluster_export_cluster_logs(s3_bucket_factory, cluster, region, instance_ids):
