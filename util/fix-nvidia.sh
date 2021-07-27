892ed3250e0e029112561d9abed7cc5a88c629be
Francesco De Martino <fdm@amazon.com>
2021-07-27 22:11:28 +0200

    wip

--- util/fix-nvidia.sh
+++ util/fix-nvidia.sh
@@ -8,7 +8,7 @@ if [ "${nvswitches}" -gt "1" ]; then
     # From https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html#ubuntu-lts
     distribution=$(. /etc/os-release;echo ${ID}${VERSION_ID} | sed -e 's/\.//g')
     echo "deb http://developer.download.nvidia.com/compute/cuda/repos/${distribution}/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-    sudo apt update -y
+    sudo apt-get update --allow-releaseinfo-change
 
     driver_version=$(nvidia-smi | grep -oP "(?<=Driver Version: )[0-9.]+")
     driver_major=$(echo ${driver_version} | cut -d. -f1)
