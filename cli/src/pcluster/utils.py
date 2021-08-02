62be71245cc3d36c922c8a65168633288b0ab9d7
Charlie Gruenwald <cgruenwa@amazon.com>
2021-08-01 19:00:27 -0600

    [integ-tests] Remove double-counting of ms offset.

--- cli/src/pcluster/utils.py
+++ cli/src/pcluster/utils.py
@@ -127,12 +127,9 @@ def to_iso_time(time_in):
     """
     if time_in:
         if isinstance(time_in, int):
-            ms_offset = 0
             if time_in > 1e12:
                 time_in /= 1000
-                ms_offset = time_in % 1000
             time_ = datetime.datetime.utcfromtimestamp(time_in)
-            time_ += datetime.timedelta(milliseconds=ms_offset)
             time_ = time_.replace(tzinfo=datetime.timezone.utc)
         elif isinstance(time_in, str):
             time_ = dateutil.parser.parse(time_in)
