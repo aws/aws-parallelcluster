33320e4fdd00406b92a57dc800ccfee34014801f
Enrico Usai <usai@amazon.com>
2021-08-09 15:44:25 +0200

    Update config.guess and config.sub files to support ARM architecture
    Arm tests were failing with:
    
    ```
    checking build system type... ./config.guess: unable to guess system type
    E
    E           This script, last modified 2009-11-20, has failed to recognize
    E           the operating system you are using. It is advised that you
    E           download the most up to date version of the config scripts from
    E
    E             http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD
    E           and
    E             http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD
    E
    E           If the version you run (./config.guess) is already up to date, please
    E           send the following data and any information you think might be
    E           pertinent to <config-patches@gnu.org> in order to provide the needed
    E           information to handle your system.
    E
    E           config.guess timestamp = 2009-11-20
    E
    E           uname -m = aarch64
    E           uname -r = 4.14.238-182.422.amzn2.aarch64
    E           uname -s = Linux
    E           uname -v = #1 SMP Tue Jul 20 20:36:04 UTC 2021
    E
    E           /usr/bin/uname -p = aarch64
    E           /bin/uname -X     =
    E
    E           hostinfo               =
    E           /bin/universe          =
    E           /usr/bin/arch -k       =
    E           /bin/arch              = aarch64
    E           /usr/bin/oslevel       =
    E           /usr/convex/getsysinfo =
    E
    E           UNAME_MACHINE = aarch64
    E           UNAME_RELEASE = 4.14.238-182.422.amzn2.aarch64
    E           UNAME_SYSTEM  = Linux
    E           UNAME_VERSION = #1 SMP Tue Jul 20 20:36:04 UTC 2021
    E           configure: error: cannot guess build type; you must specify one
    ```
    
    In the micro-benchmarks-5.6.3 archive the timestamp was 2013.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/efa/test_efa/test_hit_efa/init_osu_benchmarks.sh
+++ tests/integration-tests/tests/efa/test_efa/test_hit_efa/init_osu_benchmarks.sh
@@ -11,10 +11,15 @@ mkdir -p /shared/${MPI_VERSION}
 
 #wget --no-check-certificate http://mvapich.cse.ohio-state.edu/download/mvapich/${OSU_BENCHMARKS_PACKAGE_NAME}.tgz
 cp "./${OSU_BENCHMARKS_PACKAGE_NAME}.tgz" /shared/${MPI_VERSION}
-
 cd /shared/${MPI_VERSION}
 tar zxvf "./${OSU_BENCHMARKS_PACKAGE_NAME}.tgz"
 
-cd "${OSU_BENCHMARKS_PACKAGE_NAME}/"
+# Update config.guess and config.sub files to support ARM architecture.
+cd
+cp "./config.guess" "/shared/${MPI_VERSION}/${OSU_BENCHMARKS_PACKAGE_NAME}/"
+cp "./config.sub" "/shared/${MPI_VERSION}/${OSU_BENCHMARKS_PACKAGE_NAME}/"
+
+# Compile OSU benchmarks
+cd "/shared/${MPI_VERSION}/${OSU_BENCHMARKS_PACKAGE_NAME}/"
 ./configure CC=$(which mpicc) CXX=$(which mpicxx)
 make
