a55388b6c0e178164c0ce83c8f0d71feed356896
Luca Carrogu <carrogu@amazon.com>
2021-09-24 11:56:19 +0200

    Refactor test for IAM roles
    Test is now using roles defined in cluster-roles.cfn.yaml which are deployed through a CFN stack.
    
    Test check updatabilty of the roles and perform job submission to check scaling works
    
    Test launched with command
    `python test_runner.py --key-name xxx --key-path xxx --show-output --stackname-suffix iamtest --tests-config configs/iam.yaml
    
    Configuration test used `iam.yaml`:
    ```
    {%- import 'common.jinja2' as common -%}
    ---
    test-suites:
      iam:
        test_iam.py::test_iam_roles:
          dimensions:
            - regions: ["eu-south-1"]
              schedulers: ["slurm", "awsbatch"]
              oss: ["alinux2"]
              instances: {{ common.INSTANCES_DEFAULT_X86 }}
    ```
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/tests/iam/test_iam.py
+++ tests/integration-tests/tests/iam/test_iam.py
@@ -10,7 +10,7 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 import logging
-import os
+import os as os_lib
 from shutil import copyfile
 
 import boto3
@@ -21,93 +21,266 @@ from remote_command_executor import RemoteCommandExecutor
 from s3_common_utils import check_s3_read_resource, check_s3_read_write_resource, get_policy_resources
 
 from tests.common.assertions import assert_no_errors_in_logs
+from tests.common.schedulers_common import get_scheduler_commands
+from tests.schedulers.test_awsbatch import _test_job_submission as test_job_submission_awsbatch
+from tests.schedulers.test_slurm import _wait_for_computefleet_changed as wait_for_computefleet_changed
 
 
 @pytest.mark.usefixtures("os", "instance")
 def test_iam_roles(
     region,
     scheduler,
-    common_pcluster_policies,
-    role_factory,
-    instance_profile_factory,
+    create_roles_stack,
     pcluster_config_reader,
     clusters_factory,
     test_datadir,
 ):
     is_awsbatch = scheduler == "awsbatch"
-    if is_awsbatch:
-        instance_policies = common_pcluster_policies["awsbatch_instance_policy"]
-        lambda_policies = common_pcluster_policies["awsbatch_lambda_policy"]
-    else:
-        instance_policies = common_pcluster_policies["traditional_instance_policy"]
-        lambda_policies = common_pcluster_policies["traditional_lambda_policy"]
-    _, cluster_role_arn = role_factory("ec2", [instance_policies])
-    instance_profile = instance_profile_factory([instance_policies])
-    lambda_role_name, lambda_role_arn = role_factory("lambda", [lambda_policies])
 
+    cfn_client, ec2_client, iam_client, lambda_client = _create_boto3_clients(region)
+
+    compute_instance_profile, compute_instance_role, head_instance_role, lambda_role = _create_cluster_roles(
+        create_roles_stack, "integ-tests-iam-cluster-roles", "cluster-roles.cfn.yaml", is_awsbatch
+    )
+
+    create_config, update_config = _get_config_create_and_update(test_datadir)
+
+    cluster = _test_cluster_create(
+        cfn_client,
+        clusters_factory,
+        compute_instance_profile,
+        compute_instance_role,
+        create_config,
+        ec2_client,
+        head_instance_role,
+        iam_client,
+        is_awsbatch,
+        lambda_client,
+        lambda_role,
+        pcluster_config_reader,
+    )
+
+    _test_cluster_update(
+        cfn_client,
+        cluster,
+        compute_instance_profile,
+        compute_instance_role,
+        create_roles_stack,
+        ec2_client,
+        head_instance_role,
+        iam_client,
+        is_awsbatch,
+        lambda_client,
+        lambda_role,
+        pcluster_config_reader,
+        update_config,
+    )
+
+    _test_cluster_scaling(cluster, is_awsbatch, region, scheduler)
+
+
+def _get_config_create_and_update(test_datadir):
     # Copy the config file template for reuse in update.
     config_file_name = "pcluster.config.yaml"
-    config_file_path = os.path.join(str(test_datadir), config_file_name)
+    config_file_path = os_lib.path.join(str(test_datadir), config_file_name)
     updated_config_file_name = "pcluster.config.update.yaml"
-    updated_config_file_path = os.path.join(str(test_datadir), updated_config_file_name)
+    updated_config_file_path = os_lib.path.join(str(test_datadir), updated_config_file_name)
     copyfile(config_file_path, updated_config_file_path)
+    return config_file_name, updated_config_file_name
 
+
+def _create_boto3_clients(region):
+    # Create boto3 client
+    cfn_client = boto3.client("cloudformation", region_name=region)
+    ec2_client = boto3.client("ec2", region_name=region)
+    iam_client = boto3.client("iam", region_name=region)
+    lambda_client = boto3.client("lambda", region_name=region)
+    return cfn_client, ec2_client, iam_client, lambda_client
+
+
+def _test_cluster_create(
+    cfn_client,
+    clusters_factory,
+    compute_instance_profile,
+    compute_instance_role,
+    config_file_name,
+    ec2_client,
+    head_instance_role,
+    iam_client,
+    is_awsbatch,
+    lambda_client,
+    lambda_role,
+    pcluster_config_reader,
+):
     cluster_config = pcluster_config_reader(
         config_file=config_file_name,
-        ec2_iam_role=cluster_role_arn,
-        iam_lambda_role=lambda_role_arn,
-        instance_profile=instance_profile,
+        head_instance_role=head_instance_role,
+        compute_instance_role=compute_instance_role,
+        compute_instance_profile=compute_instance_profile,
+        iam_lambda_role=lambda_role,
+        min_count=1,
     )
     cluster = clusters_factory(cluster_config)
-
-    cfn_client = boto3.client("cloudformation", region_name=region)
-    lambda_client = boto3.client("lambda", region_name=region)
-
-    # Check all CloudFormation stacks after creation
+    # Check roles are attached to the resources
     # If scheduler is awsbatch, there will still be IAM roles created.
-    _check_lambda_role(cfn_client, lambda_client, cluster.name, lambda_role_name, not is_awsbatch)
+    _check_roles(
+        cfn_client,
+        ec2_client,
+        iam_client,
+        lambda_client,
+        cluster.name,
+        head_instance_role,
+        compute_instance_role,
+        lambda_role,
+        not is_awsbatch,
+    )
+    return cluster
 
-    # Test updating the iam_lambda_role, instance_role and instance profile
-    updated_lambda_role_name, updated_lambda_role_arn = role_factory("lambda", [lambda_policies])
-    _, update_cluster_role_arn = role_factory("ec2", [instance_policies])
-    updated_instance_profile = instance_profile_factory([instance_policies])
 
-    assert_that(updated_lambda_role_arn == lambda_role_arn).is_false()
-    assert_that(update_cluster_role_arn == cluster_role_arn).is_false()
-    assert_that(updated_instance_profile == instance_profile).is_false()
+def _test_cluster_scaling(cluster, is_awsbatch, region, scheduler):
+    remote_command_executor = RemoteCommandExecutor(cluster)
+    if is_awsbatch:
+        timeout = (
+            120 if region.startswith("cn-") else 60
+        )  # Longer timeout in china regions due to less reliable networking
+        test_job_submission_awsbatch(
+            remote_command_executor, f"awsbsub --vcpus 2 --memory 256 --timeout {timeout} sleep 1"
+        )
+    else:
+        scheduler_commands = get_scheduler_commands(scheduler, remote_command_executor)
+        job_id = scheduler_commands.submit_command_and_assert_job_accepted(
+            submit_command_args={"command": "sleep 1", "nodes": 1}
+        )
+        scheduler_commands.wait_job_completed(job_id)
 
+
+def _test_cluster_update(
+    cfn_client,
+    cluster,
+    compute_instance_profile,
+    compute_instance_role,
+    create_roles_stack,
+    ec2_client,
+    head_instance_role,
+    iam_client,
+    is_awsbatch,
+    lambda_client,
+    lambda_role,
+    pcluster_config_reader,
+    updated_config_file_name,
+):
+    (
+        another_compute_instance_profile,
+        another_compute_instance_role,
+        another_head_instance_role,
+        another_lambda_role,
+    ) = _create_cluster_roles(
+        create_roles_stack, "integ-tests-iam-cluster-roles", "cluster-roles.cfn.yaml", is_awsbatch
+    )
+
+    assert_that(another_lambda_role == lambda_role).is_false()
+    assert_that(another_head_instance_role == head_instance_role).is_false()
+    if not is_awsbatch:
+        assert_that(another_compute_instance_profile == compute_instance_profile).is_false()
+        assert_that(another_compute_instance_role == compute_instance_role).is_false()
+
+    # Update cluster with new roles
+    cluster.stop()
+    wait_for_computefleet_changed(cluster, "DISABLED" if is_awsbatch else "STOPPED")
     cluster.update(
         str(
             pcluster_config_reader(
                 config_file=updated_config_file_name,
-                ec2_iam_role=update_cluster_role_arn,
-                iam_lambda_role=updated_lambda_role_arn,
-                instance_profile=updated_instance_profile,
+                head_instance_role=another_head_instance_role,
+                compute_instance_role=another_compute_instance_role,
+                compute_instance_profile=another_compute_instance_profile,
+                iam_lambda_role=another_lambda_role,
+                min_count=0,
             )
         )
     )
+    cluster.start()
+    wait_for_computefleet_changed(cluster, "ENABLED" if is_awsbatch else "RUNNING")
+    # Check new roles are attached to the resources
+    _check_roles(
+        cfn_client,
+        ec2_client,
+        iam_client,
+        lambda_client,
+        cluster.name,
+        another_head_instance_role,
+        another_compute_instance_role,
+        another_lambda_role,
+        not is_awsbatch,
+    )
 
-    # Check all CloudFormation stacks after update
-    _check_lambda_role(cfn_client, lambda_client, cluster.name, updated_lambda_role_name, not is_awsbatch)
+
+def _create_cluster_roles(create_roles_stack, stack_prefix, roles_file, is_awsbatch):
+    cluster_roles_stack = create_roles_stack(stack_prefix=stack_prefix, roles_file=roles_file)
+    if is_awsbatch:
+        head_instance_role = cluster_roles_stack.cfn_outputs["HeadNodeRoleBatch"]
+        lambda_role = cluster_roles_stack.cfn_outputs["CustomLambdaResourcesRoleBatch"]
+        compute_instance_role = ""
+        compute_instance_profile = ""
+    else:
+        head_instance_role = cluster_roles_stack.cfn_outputs["HeadNodeRoleSlurm"]
+        compute_instance_profile = cluster_roles_stack.cfn_outputs["ComputeNodeInstanceProfileSlurm"]
+        compute_instance_role = cluster_roles_stack.cfn_outputs["ComputeNodeRoleSlurm"]
+        lambda_role = cluster_roles_stack.cfn_outputs["CustomLambdaResourcesRoleSlurm"]
+    return compute_instance_profile, compute_instance_role, head_instance_role, lambda_role
 
 
-def _check_lambda_role(cfn_client, lambda_client, stack_name, lambda_role_name, check_no_role_is_created):
-    """Test lambda role is attached to all Lambda functions in the stack and its substack."""
+def _check_roles(
+    cfn_client,
+    ec2_client,
+    iam_client,
+    lambda_client,
+    stack_name,
+    head_instance_role,
+    compute_instance_role,
+    lambda_role,
+    check_no_role_is_created,
+):
+    """Test roles are attached to EC2 instances and Lambda functions."""
     resources = cfn_client.describe_stack_resources(StackName=stack_name)["StackResources"]
     for resource in resources:
         resource_type = resource["ResourceType"]
         if check_no_role_is_created:
-            # If check_no_role_is_created, check that there is no role created in the stack and its substack.
+            # If check_no_role_is_created, check that there is no role created in the stack.
             assert_that(resource_type).is_not_equal_to("AWS::IAM::Role")
-        if resource_type == "AWS::CloudFormation::Stack":
-            # Recursively check substacks
-            _check_lambda_role(
-                cfn_client, lambda_client, resource["PhysicalResourceId"], lambda_role_name, check_no_role_is_created
-            )
         if resource_type == "AWS::Lambda::Function":
             # Check the role is attached to the Lambda function
             lambda_function = lambda_client.get_function(FunctionName=resource["PhysicalResourceId"])["Configuration"]
-            assert_that(lambda_role_name in lambda_function["Role"]).is_true()
+            assert_that(lambda_function["Role"]).is_equal_to(lambda_role)
+        if resource_type == "AWS::EC2::Instance":
+            # Check the role is attached to the EC2 instance
+            instance_profile_arn = (
+                ec2_client.describe_instances(InstanceIds=[resource["PhysicalResourceId"]])
+                .get("Reservations")[0]
+                .get("Instances")[0]
+                .get("IamInstanceProfile")
+                .get("Arn")
+            )
+
+            instance_roles_list = [
+                role.get("Arn")
+                for role in (
+                    iam_client.get_instance_profile(
+                        InstanceProfileName=_get_resource_name_from_resource_arn(instance_profile_arn)
+                    )
+                    .get("InstanceProfile")
+                    .get("Roles")
+                )
+            ]
+
+            if resource["LogicalResourceId"] == "HeadNode":
+                assert_that(instance_roles_list).contains(head_instance_role)
+            elif resource["LogicalResourceId"] == "ComputeNode":
+                assert_that(instance_roles_list).contains(compute_instance_role)
+
+
+def _get_resource_name_from_resource_arn(resource_arn):
+    return resource_arn.rsplit("/", 1)[-1] if resource_arn else ""
 
 
 @pytest.mark.regions(["ap-northeast-2"])
