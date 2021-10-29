bba557276874112738ed243de7918d1b619cd143
Evan Bollig <bollig@gmail.com>
2021-10-29 13:14:34 -0500

    Update build-model.sh
    Re-uses the gradle installed by gradlew rather than looking for a pre-installed version, and provides advice to install yq

--- api/spec/build-model.sh
+++ api/spec/build-model.sh
@@ -4,7 +4,8 @@ set -ex
 if ! command -v yq &> /dev/null
 then
     echo "Please install yq: https://mikefarah.gitbook.io/yq/"
+    echo "   GO111MODULE=on go get github.com/mikefarah/yq/v4; export PATH=\$PATH:~/go/bin"
     exit 1
 fi
-pushd smithy && gradle build && popd
+pushd smithy && ../../gradlew build && popd
 yq eval -P smithy/build/smithyprojections/smithy/source/openapi/ParallelCluster.openapi.json > openapi/ParallelCluster.openapi.yaml
