2e9fb8ce1818db1d8a3c80fc511d1890cdaeb837
Enrico Usai <usai@amazon.com>
2021-12-23 15:05:49 +0100

    Use values obtained with libfabric 1.13 for OSU alltoall benchmark
    IntelMPI requires DC (Delivery Complete) and this wasn't correctly implemented in libfabric until 1.11.
    
    DC behaviour has been fixed on libfabric 1.12 but this change has an impact in performances
    for osu_alltoall benchmark, on some packet sizes (32, 4096, 8192, 16384, 32768)
    causing constant failures in our tests when using IntelMPI 2021.
    
    IntelMPI 2019 uses libfabric 1.10 -> bugged version -> good performances but because of the bug.
    IntelMPI 2021 uses libfabric 1.13 -> fixed version -> performances out of the previous tolerated range.
    
    With this patch we're using libfabric 1.13 values as expected results for the affected packet sizes.
    
    ## Other:
    * Exported `I_MPI_DEBUG` variable to easily discover the used libfabric version.
    * In this patch we're also updating Size 1 and 2 to be a more real value and not a spike.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/common/data/osu/osu_collective_submit_intelmpi.sh
+++ tests/integration-tests/tests/common/data/osu/osu_collective_submit_intelmpi.sh
@@ -6,6 +6,8 @@ OSU_BENCHMARK_VERSION={{ osu_benchmark_version }}
 NUM_OF_PROCESSES={{ num_of_processes }}
 
 module load intelmpi
+export I_MPI_DEBUG=1
+
 # Run collective benchmark. The collective operations are close to what a real application looks like.
 # NOTE: The test is sized for 4 compute nodes.
 # -np total number of processes to run (all CPUs * 4 nodes)
