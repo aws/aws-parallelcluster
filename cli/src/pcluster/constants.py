c8b24cfd8c577c9136fd3664524b7f14e6c54a6f
Luca Carrogu <carrogu@amazon.com>
2022-02-11 14:33:12 +0100

    Bump version to 3.2.0
    Bump version from 3.2.0b1 to 3.2.0
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- cli/src/pcluster/constants.py
+++ cli/src/pcluster/constants.py
@@ -65,8 +65,8 @@ MAX_NUMBER_OF_COMPUTE_RESOURCES = 5
 MAX_STORAGE_COUNT = {"ebs": 5, "efs": 1, "fsx": 1, "raid": 1}
 
 COOKBOOK_PACKAGES_VERSIONS = {
-    "parallelcluster": "3.2.0b1",
-    "cookbook": "aws-parallelcluster-cookbook-3.2.0b1",
+    "parallelcluster": "3.2.0",
+    "cookbook": "aws-parallelcluster-cookbook-3.2.0",
     "chef": "17.2.29",
     "berkshelf": "7.2.0",
     "ami": "dev",
