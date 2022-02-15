875a8e1b8bb76115359b410c0859986cd26a4985
Enrico Usai <usai@amazon.com>
2022-02-15 13:16:59 +0100

    Execute OSU benchmarks on 32 nodes
    Fix scripts to submit OSU benchmarks:
    * Verbose debugging
    * Print env
    * Force slurm as bootstrap mechanism for mpirun
    * Remove round robin scheduling from mpirun submission
    
    Relevant notes:
    * Intel MPI is shipped with an internal Libfabric (1.10.1-impi for Intel MPI 2019, and 1.13.0-impi for Intel MPI 2021).
    * Libfabric >1.12 fixed an issue with FI_DELIVERY_COMPLETE: ofiwg/libfabric#6092 and this fix causes worst performances, for this reason we decided to use 3.1.1 results as baseline.
    
    Disabled multithreading for x86 tests only
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/efa/test_efa/test_hit_efa/osu_pt2pt_submit_intelmpi.sh
+++ tests/integration-tests/tests/efa/test_efa/test_hit_efa/osu_pt2pt_submit_intelmpi.sh
@@ -5,6 +5,7 @@ BENCHMARK_NAME={{ benchmark_name }}
 OSU_BENCHMARK_VERSION={{ osu_benchmark_version }}
 
 module load intelmpi
-export I_MPI_DEBUG=1
+export I_MPI_DEBUG=10
+env
 
-mpirun -np 2 -rr --map-by ppr:1:node /shared/intelmpi/osu-micro-benchmarks-${OSU_BENCHMARK_VERSION}/mpi/pt2pt/${BENCHMARK_NAME} > /shared/${BENCHMARK_NAME}.out
+mpirun -np 2 -bootstrap=slurm --map-by ppr:1:node /shared/intelmpi/osu-micro-benchmarks-${OSU_BENCHMARK_VERSION}/mpi/pt2pt/${BENCHMARK_NAME} > /shared/${BENCHMARK_NAME}.out
