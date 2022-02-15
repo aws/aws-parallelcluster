8f98c5694d3c43cb36158569654f7d9d5922a61a
Giacomo Marciani <mgiacomo@amazon.com>
2022-02-15 12:39:35 +0100

    Execute OSU benchmarks on clusters with 32 compute nodes and multi-treading disabled. Fix scripts to submit OSU benchmarks. Determined new baseline: 3.1.1
    Intel MPI is shipped with an internal Libfabric (1.10.1-impi for Intel MPI 2019, and 1.13.0-impi for Intel MPI 2021).
    Libfabric >1.12 fixed an issue with FI_DELIVERY_COMPLETE: https://github.com/ofiwg/libfabric/pull/6092
    and this fix causes worst performances, for this reason we need to use 3.1.1 results as baseline.
    
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- tests/integration-tests/tests/common/data/osu/osu_pt2pt_submit_openmpi.sh
+++ tests/integration-tests/tests/common/data/osu/osu_pt2pt_submit_openmpi.sh
@@ -5,4 +5,7 @@ BENCHMARK_NAME={{ benchmark_name }}
 OSU_BENCHMARK_VERSION={{ osu_benchmark_version }}
 
 module load openmpi
+
+env
+
 mpirun -np 2 --map-by ppr:1:node /shared/openmpi/osu-micro-benchmarks-${OSU_BENCHMARK_VERSION}/mpi/pt2pt/${BENCHMARK_NAME} > /shared/${BENCHMARK_NAME}.out
