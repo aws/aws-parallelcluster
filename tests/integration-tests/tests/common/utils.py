f3af66ed7b2a7ec988ed41847230078c2fd48c91
Giacomo Marciani <mgiacomo@amazon.com>
2022-02-15 12:39:35 +0100

    Execute OSU benchmarks on clusters with 32 compute nodes and multi-treading disabled. Fix scripts to submit OSU benchmarks. Determined new baseline: 3.1.1
    Intel MPI is shipped with an internal Libfabric (1.10.1-impi for Intel MPI 2019, and 1.13.0-impi for Intel MPI 2021).
    Libfabric >1.12 fixed an issue with FI_DELIVERY_COMPLETE: https://github.com/ofiwg/libfabric/pull/6092
    and this fix causes worst performances, for this reason we need to use 3.1.1 results as baseline.
    
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- tests/integration-tests/tests/common/utils.py
+++ tests/integration-tests/tests/common/utils.py
@@ -138,8 +138,9 @@ def retrieve_pcluster_ami_without_standard_naming(region, os, version, architect
 
 
 @retry(stop_max_attempt_number=3, wait_fixed=5000)
-def fetch_instance_slots(region, instance_type):
-    return get_instance_info(instance_type, region).get("VCpuInfo").get("DefaultVCpus")
+def fetch_instance_slots(region, instance_type, multithreading_disabled=False):
+    vcpus = get_instance_info(instance_type, region).get("VCpuInfo").get("DefaultVCpus")
+    return int(vcpus / 2) if multithreading_disabled else vcpus
 
 
 @retry(stop_max_attempt_number=10, wait_fixed=seconds(50))
