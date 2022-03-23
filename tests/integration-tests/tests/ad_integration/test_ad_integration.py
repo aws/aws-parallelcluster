cbe28e7da9f519cfc0545e97438e2a324dceb27d
Giacomo Marciani <mgiacomo@amazon.com>
2022-03-23 16:42:51 +0100

    Remove FSx as shared storage from clusters used by AD integration tests. Using EFS as a replacement.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- tests/integration-tests/tests/ad_integration/test_ad_integration.py
+++ tests/integration-tests/tests/ad_integration/test_ad_integration.py
@@ -485,7 +485,8 @@ def _check_files_permissions(users):
             f"/home/{user.alias}/my_file",
             f"/shared/{user.alias}_file",
             f"/ebs/{user.alias}_file",
-            f"/efs/{user.alias}_file",
+            #TODO EFS mounted on /shared as replacement for FSx which is currently casuing issues.
+            #f"/efs/{user.alias}_file",
         ]:
             user.run_remote_command(f"touch {path}")
             # Specify that only owner of file should have read/write access.
