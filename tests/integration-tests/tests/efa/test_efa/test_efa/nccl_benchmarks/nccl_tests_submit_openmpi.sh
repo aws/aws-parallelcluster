fd5e064b879cb0bf30c73348860f37479f19d17c
Hanwen <hanwenli@amazon.com>
2022-07-06 11:55:07 -0700

    Integ-tests: only use simple protocol when running NCCL test
    This change is done to fix test_efa test on p4d. It is done according to the doc: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/efa-start-nccl-base.html#nccl-start-base-test
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- tests/integration-tests/tests/efa/test_efa/test_efa/nccl_benchmarks/nccl_tests_submit_openmpi.sh
+++ tests/integration-tests/tests/efa/test_efa/test_efa/nccl_benchmarks/nccl_tests_submit_openmpi.sh
@@ -13,5 +13,6 @@ mpirun \
 -x RDMAV_FORK_SAFE=1 \
 -x NCCL_ALGO=ring \
 -x NCCL_DEBUG=WARNING \
+-x NCCL_PROTO=simple \
 --mca pml ^cm --mca btl tcp,self --mca btl_tcp_if_exclude lo,docker0 --bind-to none \
 /shared/openmpi/nccl-tests-2.10.0/build/all_reduce_perf -b 8 -e 1G -f 2 -g 1 -c 1 -n 100 > /shared/nccl_tests.out
