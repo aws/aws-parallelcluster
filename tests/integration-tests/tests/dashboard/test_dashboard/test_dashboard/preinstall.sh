a01a623ea55671ec39d18c26869e5dede8c0023b
Hanting Wong <78058716+HWong-sudo@users.noreply.github.com>
2022-08-10 18:43:43 -0400

    error causing script for custom metric tests

---
+++ tests/integration-tests/tests/dashboard/test_dashboard/test_dashboard/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script whose only purpose is to not work
+if [[[[[]]]"${instance_type}" == "c5.large" ]}}}}}}}; then
+  ecasdfho "Test Bootstrap error that causes instance to self terminate."
+  exit 1
+fi
