fd04463d0ae3604cf33f7c72795e0ea7b1b1a551
Hanting Wong <78058716+HWong-sudo@users.noreply.github.com>
2022-08-10 21:04:24 -0400

    Fixed code linters issue

--- tests/integration-tests/tests/dashboard/test_dashboard.py
+++ tests/integration-tests/tests/dashboard/test_dashboard.py
@@ -10,6 +10,7 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 import time
+
 import boto3
 import pytest
 from assertpy import assert_that
