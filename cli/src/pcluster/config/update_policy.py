bda0447ca1f24c6d84cdac2ea9a5bf3e31120366
Eddy Mwiti <eddmwiti@amazon.com>
2022-11-28 12:17:25 +0000

    [SubnetUpdate] Block subnet updates when using a managed Fsx for Lustre FileSystem
    Signed-off-by: Eddy Mwiti <eddmwiti@amazon.com>

--- cli/src/pcluster/config/update_policy.py
+++ cli/src/pcluster/config/update_policy.py
@@ -188,6 +188,23 @@ def is_managed_placement_group_deletion(change, patch):
     ) and not is_placement_group_managed_for_compute_resource(target_q_networking, target_cr_networking)
 
 
+def get_managed_fsx_from_config(config):
+    """Extract managed Fsx for Lustre shared storage entries in a cluster configuration."""
+    managed_fsx_storage = [
+        storage
+        for storage in config.get("SharedStorage", [])
+        if storage.get("StorageType") == "FsxLustre" and "FileSystemId" not in storage.get("FsxLustreSettings", {})
+    ]
+    return managed_fsx_storage
+
+
+def unchanged_managed_fsx_lustre_names(_, patch):
+    """Get list of managed Fsx for Lustre Shared Storage that hasn't changed between cluster configuration updates."""
+    managed_fsx_names_before_update = {fsx.get("Name") for fsx in get_managed_fsx_from_config(patch.base_config)}
+    managed_fsx_names_after_update = {fsx.get("Name") for fsx in get_managed_fsx_from_config(patch.target_config)}
+    return managed_fsx_names_before_update.intersection(managed_fsx_names_after_update)
+
+
 def is_slurm_scheduler(patch):
     return patch.cluster.stack.scheduler == SLURM
 
@@ -238,6 +255,18 @@ def fail_reason_managed_placement_group(change, patch):
     return reason
 
 
+def fail_reason_managed_fsx(change, patch):
+    managed_fsx_lustre_names = unchanged_managed_fsx_lustre_names(change, patch)
+    if managed_fsx_lustre_names:
+        reason = (
+            f"Updating the {change.key} parameter will cause these FSx for Lustre file system(s) to be replaced: "
+            f"{managed_fsx_lustre_names}"
+        )
+    else:
+        reason = fail_reason_queue_update_strategy(change, patch)
+    return reason
+
+
 def condition_checker_queue_update_strategy(change, patch):
     result = not patch.cluster.has_running_capacity()
     # QueueUpdateStrategy can override UpdatePolicy of parameters under SlurmQueues
@@ -261,6 +290,14 @@ def condition_checker_managed_placement_group(change, patch):
     return result
 
 
+def condition_checker_managed_fsx(change, patch):
+    if unchanged_managed_fsx_lustre_names(change, patch):
+        result = False
+    else:
+        result = condition_checker_queue_update_strategy(change, patch)
+    return result
+
+
 def actions_needed_shared_storage_update(change, patch):
     if is_awsbatch_scheduler(change, patch):
         return (
@@ -274,6 +311,17 @@ def actions_needed_shared_storage_update(change, patch):
         actions += ", or set QueueUpdateStrategy in the configuration used for the 'update-cluster' operation"
 
     return actions
+
+
+def actions_needed_managed_fsx(change, patch):
+    fsx_lustre_names = unchanged_managed_fsx_lustre_names(change, patch)
+    if fsx_lustre_names:
+        return (
+            "If you intend to proceed with the update, please make sure to back-up your data and explicitly replace the"
+            f" file system(s) ({fsx_lustre_names}) with a new one(s) in the cluster configuration."
+        )
+    else:
+        return actions_needed_queue_update_strategy(change, patch)
 
 
 def condition_checker_shared_storage_update_policy(change, patch):
@@ -320,6 +368,7 @@ UpdatePolicy.ACTIONS_NEEDED = {
     "pcluster_stop_conditional": actions_needed_queue_update_strategy,
     "managed_placement_group": actions_needed_managed_placement_group,
     "shared_storage_update_conditional": actions_needed_shared_storage_update,
+    "managed_fsx": actions_needed_managed_fsx,
 }
 
 # Base policies
@@ -454,4 +503,13 @@ UpdatePolicy.UNSUPPORTED = UpdatePolicy(
         else "{0} the parameter '{1}'".format("Restore" if change.is_list else "Remove", change.key)
     )
     + ". If you need this change, please consider creating a new cluster instead of updating the existing one.",
+)
+
+# Block update if cluster has a managed Fsx for Lustre FileSystem, otherwise fallback to QueueUpdateStrategy
+UpdatePolicy.MANAGED_FSX = UpdatePolicy(
+    name="MANAGED_FSX",
+    level=6,
+    fail_reason=fail_reason_managed_fsx,
+    action_needed=UpdatePolicy.ACTIONS_NEEDED["managed_fsx"],
+    condition_checker=condition_checker_managed_fsx,
 )
