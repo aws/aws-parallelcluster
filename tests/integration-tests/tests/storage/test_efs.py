639b33e6b2456575d57c560594f2d1e1b307face
Eddy Mwiti <eddmwiti@amazon.com>
2022-12-13 10:40:42 +0100

    [MultiAZ] Shared Storage-EFS Integration Tests: Reboot compute nodes in all partitions/queues
    Signed-off-by: Eddy Mwiti <eddmwiti@amazon.com>
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- tests/integration-tests/tests/storage/test_efs.py
+++ tests/integration-tests/tests/storage/test_efs.py
@@ -188,13 +188,16 @@ def _check_efs_after_nodes_reboot(
     # Re-establish connection after head node reboot
     remote_command_executor = RemoteCommandExecutor(cluster)
     scheduler_commands = scheduler_commands_factory(remote_command_executor)
-    compute_nodes = scheduler_commands.get_compute_nodes("queue-0")
-    for compute_node in compute_nodes:
-        scheduler_commands.reboot_compute_node(compute_node, asap=False)
-    scheduler_commands.wait_nodes_status("idle", compute_nodes)
-    _check_efs_correctly_mounted_and_shared(
-        all_mount_dirs, remote_command_executor, scheduler_commands, iam_authorizations, encryption_in_transits
-    )
+    for partition in scheduler_commands.get_partitions():
+        compute_nodes = scheduler_commands.get_compute_nodes(filter_by_partition=partition)
+        logging.info(f"Rebooting compute nodes: {compute_nodes} in partition {partition}")
+        for compute_node in compute_nodes:
+            scheduler_commands.reboot_compute_node(compute_node, asap=False)
+        scheduler_commands.wait_nodes_status("idle", compute_nodes)
+        logging.info(f"Compute nodes in partition {partition} now IDLE: {compute_nodes}")
+        _check_efs_correctly_mounted_and_shared(
+            all_mount_dirs, remote_command_executor, scheduler_commands, iam_authorizations, encryption_in_transits
+        )
     return remote_command_executor, scheduler_commands
 
 
