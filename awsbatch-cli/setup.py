44005ffbc1c0e5ea4c5ebefe40d15eb8eb658db1
Hanwen <hanwenli@amazon.com>
2022-12-23 12:04:35 -0800

    [AWSBatch-CLI] Add Python 3.10 support
    This commit also fixes the changelog of AWSBatch-CLI 1.1.0. Although the code of AWSBatch-CLI 1.1.0 supports Python 3.10, we didn't add the support in the setup.py.
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- awsbatch-cli/setup.py
+++ awsbatch-cli/setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
     ],
