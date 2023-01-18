1b7f763acc6e641902691f2fcc5dc83a4d38541d
Himani Deshpande <himanidp@amazon.com>
2023-01-18 10:44:10 -0500

    Add latest Python version for virtual enviornment
    Adding latest Python version and aws-cdk version to the THIRD-PARTY-LICENSES.txt
    
    PR: https://github.com/aws/aws-parallelcluster/pull/4802

--- util/create-attribution-doc.sh
+++ util/create-attribution-doc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 set -ex
+# Install the python version if it doesnt exist
+if test ! -d ${PYENV_ROOT}/versions/3.9.10;
+then 
+  env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.9.10
+fi
 
-pyenv virtualenv attribution-doc-env
+pyenv virtualenv 3.9.10 attribution-doc-env
 # switch to a specific virtual env
 source ${PYENV_ROOT}/versions/attribution-doc-env/bin/activate
-
-
 
 # Update Pip
 pip3 install --upgrade pip
@@ -22,10 +25,10 @@ pip3 install -e cli
 #pip3 install -r requirements.txt
 
 final_license_file=THIRD-PARTY-LICENSES.txt
-python_license_file=python_license.txt
-aws_cdk_license_file=aws-cdk-license.txt
+
 # Create a list of aws cdk Sub-packages we want to ignore 
 aws_cdk_ignore_subpackages=$(pip list | grep cdk | awk '{print $1}') 
+aws_cdk_version=$(pip list | grep cdk | awk '{print $2}'| head -n 1)
 # Create a pip License document
 pip-licenses -i $aws_cdk_ignore_subpackages aws-parallelcluster pip-licenses --format=plain-vertical --with-license-file --with-urls --no-license-path --with-authors --output-file=$final_license_file
 
@@ -34,13 +37,15 @@ pip-licenses -i $aws_cdk_ignore_subpackages aws-parallelcluster pip-licenses --f
 cpy_version=$(python -V |  grep -Eo '([0-9]+)(\.?[0-9]+)' | head -1) 
 
 
-# Add other License in the final file
-# Get Aws-cdk License file
-echo "aws-cdk; https://raw.githubusercontent.com/aws/aws-cdk/main/LICENSE" >> $final_license_file
+# Appending aws-cdk and Python License to the THIRD-PARTY-LICENSES file
+# Adding a header for aws-cdk version and License URL to THIRD-PARTY-LICENSES file
+echo "aws-cdk $aws_cdk_version; https://raw.githubusercontent.com/aws/aws-cdk/main/LICENSE" >> $final_license_file
+# Appending aws-cdk License to THIRD-PARTY-LICENSES file
 curl https://raw.githubusercontent.com/aws/aws-cdk/main/LICENSE >> $final_license_file
-# Download Python License file
-echo -e "\nPython $cpy_version " >> $final_license_file
-curl $python_license_file https://raw.githubusercontent.com/python/cpython/$cpy_version/LICENSE >> $final_license_file
+# Adding a header for Python version and License URL to THIRD-PARTY-LICENSES file
+echo -e "\nPython $cpy_version; https://raw.githubusercontent.com/python/cpython/$cpy_version/LICENSE " >> $final_license_file
+# Appending Python License to THIRD-PARTY-LICENSES file
+curl https://raw.githubusercontent.com/python/cpython/$cpy_version/LICENSE >> $final_license_file
 
 
 deactivate
