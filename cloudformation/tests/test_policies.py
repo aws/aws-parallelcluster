ec2a273c3943b18d96abeb7bbeea9f16e976475d
Charlie Gruenwald <cgruenwa@amazon.com>
2023-02-01 20:03:10 -0700

    `[policies]` Add a lambda role and return it.

--- cloudformation/tests/test_policies.py
+++ cloudformation/tests/test_policies.py
@@ -89,15 +89,19 @@ def test_match_api():
     for key in policies["Resources"].keys():
         drop_keys = {"Condition"}
 
-        source_key = {"ParallelClusterFSxS3AccessPolicy": "FSxS3AccessPolicy"}.get(key, key)
+        source_key = {
+            "ParallelClusterFSxS3AccessPolicy": "FSxS3AccessPolicy",
+            "ParallelClusterLambdaRole": "ParallelClusterUserRole",
+        }.get(key, key)
         source_dict = {k: v for k, v in source["Resources"][source_key].items() if k not in drop_keys}
         dest_dict = {k: v for k, v in policies["Resources"][key].items() if k not in drop_keys}
 
-        if key == "ParallelClusterUserRole":
-            source_dict["Properties"]["ManagedPolicyArns"] = source_dict["Properties"]["ManagedPolicyArns"][2:]
+        if key in ["ParallelClusterUserRole", "ParallelClusterLambdaRole"]:
+            if key == "ParallelClusterUserRole":
+                source_dict["Properties"]["ManagedPolicyArns"] = source_dict["Properties"]["ManagedPolicyArns"][2:]
 
             def remove_batch_if(arn):
-                return arn if "Ref" in arn else arn["Fn::If"][1]
+                return arn if ("Ref" in arn or "Fn::Sub" in arn) else arn["Fn::If"][1]
 
             dest_dict["Properties"]["ManagedPolicyArns"] = list(
                 map(remove_batch_if, dest_dict["Properties"]["ManagedPolicyArns"])
@@ -106,5 +110,8 @@ def test_match_api():
         if key == "ParallelClusterFSxS3AccessPolicy":
             del source_dict["Properties"]["PolicyName"]
             del dest_dict["Properties"]["PolicyName"]
+
+        if key == "DefaultParallelClusterIamAdminPolicy":
+            source_dict["Properties"]["Roles"].append(dest_dict["Properties"]["Roles"][1])
 
         assert_that(dest_dict).is_equal_to(source_dict)
