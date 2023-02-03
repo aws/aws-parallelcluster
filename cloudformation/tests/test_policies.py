233b1033e6ca9290e73832bb214f20b629ee1dbd
Charlie Gruenwald <cgruenwa@amazon.com>
2023-02-03 12:37:15 -0700

    `[policies/test]` Remove test that validates api and hosted policy once api policy leverages hosted.

--- cloudformation/tests/test_policies.py
+++ cloudformation/tests/test_policies.py
@@ -3,7 +3,6 @@
 import botocore
 import pytest
 from assertpy import assert_that
-from cfn_flip import load_yaml
 
 TEMPLATE = "../policies/parallelcluster-policies.yaml"
 
@@ -49,54 +48,3 @@ def test_policies(cfn, random_stack_name, parameters):
     cfn.get_waiter("stack_delete_complete").wait(StackName=stack_name)
     status = cfn.describe_stacks(StackName=stack_id)["Stacks"][0]["StackStatus"]
     assert_that(status).is_equal_to("DELETE_COMPLETE")
-
-
-def test_match_api():
-    """Validate minimal changes with the API's yaml."""
-    source_path = "../../api/infrastructure/parallelcluster-api.yaml"
-    policies_path = "../policies/parallelcluster-policies.yaml"
-
-    with open(source_path, encoding="utf-8") as source_file:
-        source = load_yaml(source_file.read())
-
-    with open(policies_path, encoding="utf-8") as policies_file:
-        policies = load_yaml(policies_file.read())
-
-    # These params match except for the description
-    for key in ["Region", "EnableFSxS3Access", "FsxS3Buckets", "PermissionsBoundaryPolicy"]:
-        drop_keys = {"Description"}
-        dest_dict = {k: v for k, v in policies["Parameters"][key].items() if k not in drop_keys}
-        source_dict = {k: v for k, v in source["Parameters"][key].items() if k not in drop_keys}
-        assert_that(dest_dict).is_equal_to(source_dict)
-
-    for key in policies["Resources"].keys():
-        drop_keys = {"Condition"}
-
-        source_key = {
-            "ParallelClusterFSxS3AccessPolicy": "FSxS3AccessPolicy",
-            "ParallelClusterLambdaRole": "ParallelClusterUserRole",
-        }.get(key, key)
-
-        source_dict = {k: v for k, v in source["Resources"][source_key].items() if k not in drop_keys}
-        dest_dict = {k: v for k, v in policies["Resources"][key].items() if k not in drop_keys}
-
-        if key == "ParallelClusterLambdaRole":
-
-            def remove_batch_if(arn):
-                return arn if ("Ref" in arn or "Fn::Sub" in arn) else arn["Fn::If"][1]
-
-            dest_dict["Properties"]["ManagedPolicyArns"] = list(
-                map(remove_batch_if, dest_dict["Properties"]["ManagedPolicyArns"])
-            )
-
-        # Rename UserRole to LambdaRole, ignore policy name mismatch
-        if key == "ParallelClusterFSxS3AccessPolicy":
-            source_dict["Properties"]["Roles"][0]["Ref"] = "ParallelClusterLambdaRole"
-            del source_dict["Properties"]["PolicyName"]
-            del dest_dict["Properties"]["PolicyName"]
-
-        # Rename UserRole to LambdaRole
-        if key == "DefaultParallelClusterIamAdminPolicy":
-            source_dict["Properties"]["Roles"][0]["Ref"] = "ParallelClusterLambdaRole"
-
-        assert_that(dest_dict).is_equal_to(source_dict)
