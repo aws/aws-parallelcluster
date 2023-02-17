94422f179b416c16382db5905202b2ee4cef42cc
nse Obot <nseobot@amazon.com>
2023-02-17 00:08:23 -0500

    check /etc/hosts against IP, HOSTNAME, and DOMAIN

--- tests/integration-tests/tests/trainium/test_trainium/test_trainium/test-primary-ip.sh
+++ tests/integration-tests/tests/trainium/test_trainium/test_trainium/test-primary-ip.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 # Test the alignment of the Route 53 IP and the the host IP
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-echo TOKEN: $TOKEN | tee output-primary-ip.txt
-macs=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs`
-echo macs: $macs | tee -a output-primary-ip.txt
-for mac in $macs
-do
-  device_number=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/device-number"`
-  network_card=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/network-card"`
-  echo mac: $mac device_number: $device_number network_card: $network_card | tee -a output-primary-ip.txt
-  if [[ $device_number == '0' && $network_card == '0' ]]
-  then
-    IP_HOSTS="$(grep "$HOSTNAME" /etc/hosts | awk '{print $1}')"
-    echo IP_HOSTS: $IP_HOSTS | tee -a output-primary-ip.txt
-    mac_ip=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/local-ipv4s"`
-    echo mac_ip: $mac_ip | tee -a output-primary-ip.txt
-    for word in $IP_HOSTS
-    do
-      if [[ $word == $mac_ip ]]
-      then
-        echo PASSED | tee -a output-primary-ip.txt
-        exit 0
-      fi
-    done
-  fi
-done
+grep Ubuntu /etc/issue &>/dev/null && DNS_SERVER=$(resolvectl dns | awk '{print $4}' | sort -r | head -1)
+IP="$(host $HOSTNAME $DNS_SERVER | tail -1 | awk '{print $4}')"
+DOMAIN=$(jq .cluster.dns_domain /etc/chef/dna.json | tr -d \")
+expected="$IP $HOSTNAME.${DOMAIN::-1} $HOSTNAME"
+echo expected: $expected | tee output-primary-ip.txt
+
+actual="$(grep "$HOSTNAME" /etc/hosts)"
+echo actual: $actual | tee -a output-primary-ip.txt
+
+if [[ $expected == $actual ]]
+then
+  echo PASSED | tee -a output-primary-ip.txt
+fi
 echo Error: Route53 IP does not match host IP | tee -a output-primary-ip.txt
