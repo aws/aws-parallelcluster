142c459ec381a2bcd50d0994ee24c23dc49c5f65
Giacomo Marciani <mgiacomo@amazon.com>
2023-03-02 12:06:41 +0100

    [Multi User] Add support for AD password as SSM Parameter.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cli/tests/pcluster/aws/dummy_aws_api.py
+++ cli/tests/pcluster/aws/dummy_aws_api.py
@@ -9,6 +9,7 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+from datetime import datetime
 
 from pcluster.aws.aws_api import AWSApi
 from pcluster.aws.aws_resources import FsxFileSystemInfo, InstanceTypeInfo
@@ -26,6 +27,7 @@ from pcluster.aws.route53 import Route53Client
 from pcluster.aws.s3 import S3Client
 from pcluster.aws.s3_resource import S3Resource
 from pcluster.aws.secretsmanager import SecretsManagerClient
+from pcluster.aws.ssm import SsmClient
 from pcluster.aws.sts import StsClient
 
 
@@ -105,6 +107,7 @@ class _DummyAWSApi(AWSApi):
         self._route53 = _DummyRoute53Client()
         self._resource_groups = _DummyResourceGroupsClient()
         self._secretsmanager = _DummySecretsManagerClient()
+        self._ssm = _DummySsmClient()
 
 
 class _DummyCfnClient(CfnClient):
@@ -340,6 +343,25 @@ class _DummySecretsManagerClient(SecretsManagerClient):
             "Tags": [],
             "VersionIdsToStages": {"12345678-1234-abcd-1234-567890abcdef": ["AWSCURRENT"]},
             "CreatedDate": "2022-08-01T10:00:00+00:00",
+        }
+
+
+class _DummySsmClient(SsmClient):
+    def __init__(self):
+        """Override parent constructor. No real boto3 client is created."""
+        self._client = None
+
+    def get_parameter(self, parameter_name):
+        return {
+            "Parameter": {
+                "Name": parameter_name,
+                "Type": "SecureString",
+                "Value": "EncryptedValue",
+                "Version": 1,
+                "LastModifiedDate": datetime(2023, 3, 3),
+                "ARN": f"arn:aws:ssm:us-east-1:111111111111:parameter/{parameter_name}",
+                "DataType": "text",
+            }
         }
 
 
