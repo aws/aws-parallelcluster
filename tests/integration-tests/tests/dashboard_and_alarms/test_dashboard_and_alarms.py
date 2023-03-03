4d4a018d97b6e9dc40a40c5f82f2dcb18bdb9143
Delong Meng <delmeng@amazon.com>
2023-03-02 20:14:02 -0800

    change order of imported packages to fix code check failure

--- tests/integration-tests/tests/dashboard_and_alarms/test_dashboard_and_alarms.py
+++ tests/integration-tests/tests/dashboard_and_alarms/test_dashboard_and_alarms.py
@@ -9,13 +9,13 @@
 # or in the "LICENSE.txt" file accompanying this file.
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+import datetime
+import math
+import time
 import boto3
 import pytest
 from assertpy import assert_that
 from botocore.exceptions import ClientError
-import time
-import datetime
-import math
 
 
 @pytest.mark.usefixtures("instance", "os", "scheduler")
