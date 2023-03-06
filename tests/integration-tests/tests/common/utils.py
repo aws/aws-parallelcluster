2e7cad92d524229ee9292eae3fee03c614616dc6
Enrico Usai <usai@amazon.com>
2023-03-06 12:42:58 +0100

    Improve error message when getting AMI info for a given os
    Previous error message was:
    AttributeError: 'NoneType' object has no attribute 'get'
    
    Now it returns a readable error message.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- tests/integration-tests/tests/common/utils.py
+++ tests/integration-tests/tests/common/utils.py
@@ -83,7 +83,7 @@ def retrieve_latest_ami(region, os, ami_type="official", architecture="x86_64", 
         if ami_type == "pcluster":
             ami_name = "aws-parallelcluster-{version}-{ami_name}".format(
                 version=get_installed_parallelcluster_version(),
-                ami_name=AMI_TYPE_DICT.get(ami_type).get(os).get("name"),
+                ami_name=_get_ami_for_os(ami_type, os).get("name"),
             )
             if (
                 request
@@ -94,13 +94,13 @@ def retrieve_latest_ami(region, os, ami_type="official", architecture="x86_64", 
                 # Then retrieve public pcluster AMIs
                 additional_filters.append({"Name": "is-public", "Values": ["true"]})
         else:
-            ami_name = AMI_TYPE_DICT.get(ami_type).get(os).get("name")
+            ami_name = _get_ami_for_os(ami_type, os).get("name")
         logging.info("Parent image name %s" % ami_name)
         response = boto3.client("ec2", region_name=region).describe_images(
             Filters=[{"Name": "name", "Values": [ami_name]}, {"Name": "architecture", "Values": [architecture]}]
             + additional_filters,
-            Owners=AMI_TYPE_DICT.get(ami_type).get(os).get("owners"),
-            IncludeDeprecated=AMI_TYPE_DICT.get(ami_type).get(os).get("includeDeprecated", False),
+            Owners=_get_ami_for_os(ami_type, os).get("owners"),
+            IncludeDeprecated=_get_ami_for_os(ami_type, os).get("includeDeprecated", False),
         )
         # Sort on Creation date Desc
         amis = sorted(response.get("Images", []), key=lambda x: x["CreationDate"], reverse=True)
@@ -114,6 +114,16 @@ def retrieve_latest_ami(region, os, ami_type="official", architecture="x86_64", 
     except IndexError as e:
         LOGGER.critical("Error no ami retrieved: {0}".format(e))
         raise
+
+
+def _get_ami_for_os(ami_type, os):
+    ami_dict = AMI_TYPE_DICT.get(ami_type)
+    if not ami_dict:
+        raise Exception(f"'{ami_type}' not found in the dict 'AMI_TYPE_DICT'")
+    os_ami = ami_dict.get(os)
+    if not os_ami:
+        raise Exception(f"'{os}' not found in the '{ami_type}' mapping referenced in the 'AMI_TYPE_DICT'")
+    return os_ami
 
 
 def retrieve_pcluster_ami_without_standard_naming(region, os, version, architecture):
