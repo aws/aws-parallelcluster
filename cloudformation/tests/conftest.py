10a35c818b6b1bde46b4a0f7f5e0e18751bf9f94
Charlie <cgruenwa@amazon.com>
2023-03-17 06:58:49 -0600

    `[cloudformation/custom_resource]` Add CFN custom resource for cluster. (#5044)
    * `[cloudformation/custom_resource]` Add CFN custom resource for cluster.
    
    * `[cloudformation/custom_resource]` Improve logging, return all keys in response.
    
    * Remove redhat8 from ARM PL test
    
    Signed-off-by: Enrico Usai <usai@amazon.com>
    
    * `[cloudformation/custom_resource]` match memory of API.
    
    * `[cloudformation/custom_resource]` Add CFN custom resource for cluster.
    
    * `[cloudformation/custom_resource]` Improve logging, return all keys in response.
    
    * `[cloudformation/custom_resource]` match memory of API.
    
    * ``[CFN]` Update CHANGELOG.md, DRY on _random_str.
    
    ---------
    
    Signed-off-by: Enrico Usai <usai@amazon.com>
    Co-authored-by: Enrico Usai <usai@amazon.com>

--- cloudformation/tests/conftest.py
+++ cloudformation/tests/conftest.py
@@ -5,6 +5,44 @@ import string
 import boto3
 import pytest
 
+PUBPRIV_TEMPLATE = "../networking/public-private.cfn.json"
+
+
+def random_str():
+    """Generate a random string."""
+    alnum = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    start = random.choice(string.ascii_uppercase + string.ascii_lowercase)
+    return start + "".join(random.choice(alnum) for _ in range(8))
+
+
+def cfn_stack_generator(path, name, parameters=None, capabilities=None):
+    """Create a stack, wait for completion and yield it."""
+    cfn = boto3.client("cloudformation")
+    with open(path, encoding="utf-8") as templ:
+        template = templ.read()
+
+    parameters = parameters or {}
+
+    # Create networking using CloudFormation, block on completion
+    cfn.create_stack(
+        StackName=name,
+        TemplateBody=template,
+        Capabilities=capabilities or ["CAPABILITY_IAM"],
+        Parameters=[{"ParameterKey": k, "ParameterValue": v} for k, v in parameters.items()],
+    )
+    cfn.get_waiter("stack_create_complete").wait(StackName=name)
+
+    try:
+        outputs = cfn.describe_stacks(StackName=name)["Stacks"][0]["Outputs"]
+        yield {o["OutputKey"]: o["OutputValue"] for o in outputs}
+
+        # Delete the stack through CFN and wait for delete to complete
+        cfn.delete_stack(StackName=name)
+        cfn.get_waiter("stack_delete_complete").wait(StackName=name)
+    except Exception as exc:
+        cfn.delete_stack(StackName=name)
+        raise exc
+
 
 def pytest_collection_modifyitems(items, config):
     """Augment the tests to add unmarked marker to tests that aren't marked."""
@@ -16,8 +54,7 @@ def pytest_collection_modifyitems(items, config):
 @pytest.fixture(name="random_stack_name")
 def random_stack_name_fixture():
     """Provide a short random id that can be used in a aack name."""
-    alnum = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    return "".join(random.choice(alnum) for _ in range(8))
+    return random_str()
 
 
 @pytest.fixture(scope="session", name="cfn")
@@ -25,3 +62,14 @@ def cfn_fixture():
     """Create a CloudFormation Boto3 client."""
     client = boto3.client("cloudformation")
     return client
+
+
+@pytest.fixture(scope="module", name="default_vpc")
+def default_vpc_fixture(cfn):
+    """Create our default VPC networking and return the stack name."""
+    ec2 = boto3.client("ec2")
+    azs = ec2.describe_availability_zones()["AvailabilityZones"]
+    stack_name = random_str()
+    parameters = {"AvailabilityZone": azs[0]["ZoneName"]}
+
+    yield from cfn_stack_generator(PUBPRIV_TEMPLATE, stack_name, parameters)
