716d3f65b7b5b0ea2a4a6394369987696937d333
Charlie <cgruenwa@amazon.com>
2023-04-07 09:01:45 -0600

    `[API]` Update to use hosted policies, lambda layer, new integ-tests (#4845)
    * `[integ-tests/custom_resource]` Move custom_resource tests into integ-tests
    
    * `[cloudformation/policies]` Update the API to leverage the published policies.
    
    * `[api]` Use the lambda role from policies.
    
    * `[policies]` Return a Lambda role to be specific on usage.
    
    * `[policies/test]` Remove test that validates api and hosted policy once api policy leverages hosted.
    
    * `[API]` Leverage the Lambda Layer to back the API.
    
    * `[integ-tests/api]` Update API integ-tests to match policies and lambda artifacts.
    
    * `[integ-tests/custom_resource]` Move testing to framework (#5110)
    
    * `[integ-tests/custom_resource]` Move custom_resource tests into integ-tests
    
    * `[integ-tests/custom_resource]` Add parameters for prebuilt assets.
    
    * `[cloudformation/custom_resource]` Use URLSuffix when construcing url
    
    * `[integ-tests/resource_bucket]` Add the resource bucket fixture.
    
    * `[integ-tests/custom_resource]` Typos, yield instead of returning from fixture, ensure stack events are returned.
    
    * `[integ-tests/api]` Update API integ-tests to match policies and lambda artifacts.
    
    * `[docker]` Restore dockerfile and CI test.
    
    * `[test_runner]` Add --policie-uri parameter to README.md
    
    * `[api/lambda_layer]` Use entrypoint from code, install lambda powertools, check for lambda.
    
    * `[API/README.md]` Remove invalid information about local testing now that we've moved to the lambda layer.
    
    * `[api/template]` Allow default policies uri. Remove extraneous line in version bump.

--- api/infrastructure/deploy-api.sh
+++ api/infrastructure/deploy-api.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -ex
+#!/bin/bash -ex
 # Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
@@ -8,18 +7,14 @@ set -ex
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-usage="$(basename "$0") [-h] --s3-bucket bucket-name --ecr-repo repo-name --region aws-region [--stack-name name] [--enable-iam-admin true|false] [--create-api-user true|false] [--skip-image-import])"
+usage="$(basename "$0") [-h] --s3-bucket bucket-name --region aws-region [--stack-name name] [--enable-iam-admin true|false] [--create-api-user true|false])"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 S3_BUCKET=
-ECR_REPO=
-IMAGE_BUILDER_VPC_ID=
-IMAGE_BUILDER_SUBNET_ID=
 STACK_NAME="ParallelClusterApi"
 ENABLE_IAM_ADMIN="true"
 CREATE_API_USER="false"
-SKIP_IMAGE_IMPORT="false" # if "true", uses the image uploaded to ECR directly, without creating a private copy
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -31,11 +26,6 @@ case $key in
     ;;
     --s3-bucket)
     S3_BUCKET=$2
-    shift # past argument
-    shift # past value
-    ;;
-    --ecr-repo)
-    ECR_REPO=$2
     shift # past argument
     shift # past value
     ;;
@@ -59,20 +49,6 @@ case $key in
     shift # past argument
     shift # past value
     ;;
-    --skip-image-import)
-    export SKIP_IMAGE_IMPORT="true"
-    shift # past argument
-    ;;
-    --image-builder-vpc-id)
-    export IMAGE_BUILDER_VPC_ID=$2
-    shift # past argument
-    shift # past value
-    ;;
-    --image-builder-subnet-id)
-    export IMAGE_BUILDER_SUBNET_ID=$2
-    shift # past argument
-    shift # past value
-    ;;
     --enable-fsx-s3-access)
     export ENABLE_FSX_S3_ACCESS=$2
     shift # past argument
@@ -90,35 +66,21 @@ case $key in
 esac
 done
 
-if [ -z "${S3_BUCKET}" ] || [ -z "${ECR_REPO}" ] || [ -z "${AWS_DEFAULT_REGION}" ] ; then
+if [ -z "${S3_BUCKET}" ] || [ -z "${AWS_DEFAULT_REGION}" ] ; then
     echo "$usage" >&2
     exit 1
 fi
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-ECR_ENDPOINT="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 S3_UPLOAD_URI="s3://${S3_BUCKET}/api/ParallelCluster.openapi.yaml"
-
-echo "Building docker image"
-"${SCRIPT_DIR}/../docker/awslambda/docker-build.sh"
-
-echo "Pushing docker image to ${ECR_ENDPOINT}/${ECR_REPO}"
-aws ecr get-login-password | docker login --username AWS --password-stdin "${ECR_ENDPOINT}"
-docker tag pcluster-lambda:latest "${ECR_ENDPOINT}/${ECR_REPO}:latest"
-docker push "${ECR_ENDPOINT}/${ECR_REPO}:latest"
+POLICIES_S3_URI="s3://${S3_BUCKET}/stacks/parallelcluster-policies.yaml"
+POLICIES_TEMPLATE_URI="http://${S3_BUCKET}.s3.${AWS_DEFAULT_REGION}.amazonaws.com/stacks/parallelcluster-policies.yaml"
 
 echo "Publishing OpenAPI specs to S3"
 aws s3 cp "${SCRIPT_DIR}/../spec/openapi/ParallelCluster.openapi.yaml" "${S3_UPLOAD_URI}"
 
-echo ""
-if [ "$SKIP_IMAGE_IMPORT" = "true" ]; then
-  echo "Uploaded image will be used directly"
-  OVERRIDE_IMAGE_URI_PRM="EcrImageUri"
-  ECR_IMAGE_URI="${ECR_ENDPOINT}/${ECR_REPO}:latest"
-else
-  echo "Uploaded image will simulate a public image and will be therefore imported into a new private image"
-  OVERRIDE_IMAGE_URI_PRM="PublicEcrImageUri"
-fi
+echo "Publishing policies CloudFormation stack to S3"
+aws s3 cp "${SCRIPT_DIR}/../../cloudformation/policies/parallelcluster-policies.yaml" "${POLICIES_S3_URI}"
 
 echo "Deploying API template"
 aws cloudformation deploy \
@@ -126,17 +88,7 @@ aws cloudformation deploy \
     --template-file "${SCRIPT_DIR}/parallelcluster-api.yaml" \
     --s3-bucket "${S3_BUCKET}" \
     --s3-prefix "api/" \
-    --parameter-overrides ApiDefinitionS3Uri="${S3_UPLOAD_URI}" "${OVERRIDE_IMAGE_URI_PRM}"="${ECR_ENDPOINT}/${ECR_REPO}:latest" \
+    --parameter-overrides ApiDefinitionS3Uri="${S3_UPLOAD_URI}" \
+                          PoliciesTemplateUri="${POLICIES_TEMPLATE_URI}" \
                           EnableIamAdminAccess="${ENABLE_IAM_ADMIN}" CreateApiUserRole="${CREATE_API_USER}" \
-                          ImageBuilderVpcId="${IMAGE_BUILDER_VPC_ID}" \
-                          ImageBuilderSubnetId="${IMAGE_BUILDER_SUBNET_ID}" \
     --capabilities CAPABILITY_NAMED_IAM
-
-echo "Updating API Lambda since updates are not fully automated yet"
-LAMBDA_FUNCTION_ARN=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[0].Outputs[?OutputKey=='ParallelClusterLambdaArn'].OutputValue" --output text)
-IMPORTED_IMAGE_URI=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query "Stacks[0].Outputs[?OutputKey=='UriOfCopyOfPublicEcrImage'].OutputValue" --output text)
-
-aws lambda update-function-code \
-    --function-name "${LAMBDA_FUNCTION_ARN}" \
-    --image-uri "${ECR_IMAGE_URI:-${IMPORTED_IMAGE_URI}}" \
-    --publish
