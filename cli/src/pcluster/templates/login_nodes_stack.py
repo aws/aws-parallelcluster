56e63a7b460711d18a9318cd3ab6773d3c2b5e60
Xuanqi He <xuanqihe@amazon.com>
2023-06-15 19:41:29 -0400

    Use a Pool Construct to organise LoginNodesStack for better readability

--- cli/src/pcluster/templates/login_nodes_stack.py
+++ cli/src/pcluster/templates/login_nodes_stack.py
@@ -3,10 +3,9 @@ from typing import Dict
 from aws_cdk import aws_autoscaling as autoscaling
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_elasticloadbalancingv2 as elbv2
-from aws_cdk.core import NestedStack, Stack
-from constructs import Construct
+from aws_cdk.core import NestedStack, Stack, Construct
 
-from pcluster.config.cluster_config import LoginNodesPool, SlurmClusterConfig
+from pcluster.config.cluster_config import SlurmClusterConfig
 from pcluster.templates.cdk_builder_utils import (
     CdkLaunchTemplateBuilder,
     LoginNodesIamResources,
@@ -17,9 +16,125 @@ from pcluster.templates.cdk_builder_utils import (
 from pcluster.utils import get_http_tokens_setting
 
 
+class Pool(Construct):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        pool,
+        config,
+        shared_storage_infos,
+        shared_storage_mount_dirs: Dict,
+        shared_storage_attributes: Dict,
+        login_security_group,
+        login_nodes_target_group,
+        stack_name,
+    ):
+        super().__init__(scope, id)
+        self._pool = pool
+        self._config = config
+        self._login_nodes_stack_id = id
+        self._shared_storage_infos = shared_storage_infos
+        self._shared_storage_mount_dirs = shared_storage_mount_dirs
+        self._shared_storage_attributes = shared_storage_attributes
+        self._login_security_group = login_security_group
+        self._login_nodes_target_group = login_nodes_target_group
+        self.stack_name = stack_name
+
+        self._add_resources()
+
+    def _add_resources(self):
+        self._add_login_node_iam_resources()
+        self._launch_template = self._add_login_nodes_pool_launch_template()
+        self._add_login_nodes_pool_auto_scaling_group()
+
+    def _add_login_node_iam_resources(self):
+        self._iam_resource = LoginNodesIamResources(
+            self,
+            f"LoginNodeIamResources{self._pool.name}",
+            self._config,
+            self._pool,
+            self._shared_storage_infos,
+            self._pool.name,
+        )
+        self._instance_profile = self._iam_resource.instance_profile
+        self._instance_role = self._iam_resource.instance_role
+
+    def _add_login_nodes_pool_launch_template(self):
+        login_nodes_pool_lt_security_groups = get_login_nodes_security_groups_full(
+            self._login_security_group,
+            self._pool,
+        )
+        login_nodes_pool_lt_nw_interface = [
+            ec2.CfnLaunchTemplate.NetworkInterfaceProperty(
+                device_index=0,
+                interface_type=None,
+                groups=login_nodes_pool_lt_security_groups,
+                subnet_id=self._pool.networking.subnet_id,
+            )
+        ]
+        return ec2.CfnLaunchTemplate(
+            self,
+            f"LoginNodeLaunchTemplate{self._pool.name}",
+            launch_template_name=f"{self.stack_name}-{self._pool.name}",
+            launch_template_data=ec2.CfnLaunchTemplate.LaunchTemplateDataProperty(
+                image_id=self._config.login_nodes_ami[self._pool.name],
+                instance_type=self._pool.instance_type,
+                key_name=self._pool.ssh.key_name,
+                iam_instance_profile=ec2.CfnLaunchTemplate.IamInstanceProfileProperty(
+                    name=self._instance_profile
+                ),
+                metadata_options=ec2.CfnLaunchTemplate.MetadataOptionsProperty(
+                    http_tokens=get_http_tokens_setting(self._config.imds.imds_support)
+                ),
+                network_interfaces=login_nodes_pool_lt_nw_interface,
+                # user_data=
+                # block_device_mappings=
+                tag_specifications=[
+                    ec2.CfnLaunchTemplate.TagSpecificationProperty(
+                        resource_type="instance",
+                        tags=get_default_instance_tags(
+                            self.stack_name,
+                            self._config,
+                            self._pool,
+                            "LoginNode",
+                            self._shared_storage_infos
+                        )
+                        # +
+                        # + custom tags for instance
+                    ),
+                    ec2.CfnLaunchTemplate.TagSpecificationProperty(
+                        resource_type="volume",
+                        tags=get_default_volume_tags(self.stack_name, "LoginNode")
+                        # +
+                        # + custom tags for volume
+                    ),
+                ],
+            ),
+        )
+
+    def _add_login_nodes_pool_auto_scaling_group(self):
+        launch_template_specification = autoscaling.CfnAutoScalingGroup.LaunchTemplateSpecificationProperty(
+            launch_template_id=self._launch_template.ref,
+            version=self._launch_template.attr_latest_version_number,
+        )
+
+        auto_scaling_group = autoscaling.CfnAutoScalingGroup(
+            self,
+            f"{self._login_nodes_stack_id}-AutoScalingGroup",
+            launch_template=launch_template_specification,
+            min_size=str(self._pool.count),
+            max_size=str(self._pool.count),
+            desired_capacity=str(self._pool.count),
+            target_group_arns=[self._login_nodes_target_group.node.default_child.ref],
+            vpc_zone_identifier=[self._pool.networking.subnet_id],
+        )
+
+        return auto_scaling_group
+
+
 class LoginNodesStack(NestedStack):
     """Stack encapsulating a set of LoginNodes and the associated resources."""
-
     def __init__(
         self,
         scope: Construct,
@@ -33,7 +148,6 @@ class LoginNodesStack(NestedStack):
         super().__init__(scope, id)
         self._login_nodes = cluster_config.login_nodes
         self._config = cluster_config
-        self._login_node_stack_id = id
         self._login_security_group = login_security_group
         self._launch_template_builder = CdkLaunchTemplateBuilder()
         self._shared_storage_infos = shared_storage_infos
@@ -62,88 +176,24 @@ class LoginNodesStack(NestedStack):
             vpc_id=self._config.vpc_id,
             availability_zones=self._pools_availability_zones,
         )
-        self._add_login_node_iam_resources()
-        self._add_launch_templates()
         self._login_nodes_target_group = self._add_login_nodes_target_group()
         self._login_nodes_load_balancer = self._add_login_nodes_load_balancer(self._login_nodes_target_group)
-        self._add_auto_scaling_groups(self._login_nodes_target_group)
 
-    def _add_login_node_iam_resources(self):
-        iam_resources = {}
+        self.pools = {}
         for pool in self._login_nodes.pools:
-            iam_resources[pool.name] = LoginNodesIamResources(
+            pool_construct = Pool(
                 self,
-                f"LoginNodeIamResources{pool.name}",
+                f"Pool{pool.name}",
+                pool,
                 self._config,
-                pool,
                 self._shared_storage_infos,
-                pool.name,
-            )
-        self._login_nodes_instance_profiles = {k: v.instance_profile for k, v in iam_resources.items()}
-        self.managed_login_nodes_instance_roles = {k: v.instance_role for k, v in iam_resources.items()}
-
-    def _add_launch_templates(self):
-        self.login_launch_templates = {}
-        for pool in self._login_nodes.pools:
-            pool_lt_security_groups = get_login_nodes_security_groups_full(
+                self._shared_storage_mount_dirs,
+                self._shared_storage_attributes,
                 self._login_security_group,
-                pool,
+                self._login_nodes_target_group,
+                self.stack_name
             )
-            self.login_launch_templates[pool.name] = self._add_login_nodes_pool_launch_template(
-                pool,
-                pool_lt_security_groups,
-                self._login_nodes_instance_profiles,
-            )
-
-    def _add_login_nodes_pool_launch_template(
-        self,
-        login_nodes_pool: LoginNodesPool,
-        login_nodes_pool_lt_security_groups,
-        login_nodes_instance_profiles,
-    ):
-        login_node_lt_nw_interface = [
-            ec2.CfnLaunchTemplate.NetworkInterfaceProperty(
-                device_index=0,
-                interface_type=None,
-                groups=login_nodes_pool_lt_security_groups,
-                subnet_id=login_nodes_pool.networking.subnet_id,
-            )
-        ]
-        return ec2.CfnLaunchTemplate(
-            self,
-            f"LoginNodeLaunchTemplate{login_nodes_pool.name}",
-            launch_template_name=f"{self.stack_name}-{login_nodes_pool.name}",
-            launch_template_data=ec2.CfnLaunchTemplate.LaunchTemplateDataProperty(
-                image_id=self._config.login_nodes_ami[login_nodes_pool.name],
-                instance_type=login_nodes_pool.instance_type,
-                key_name=login_nodes_pool.ssh.key_name,
-                iam_instance_profile=ec2.CfnLaunchTemplate.IamInstanceProfileProperty(
-                    name=login_nodes_instance_profiles[login_nodes_pool.name]
-                ),
-                metadata_options=ec2.CfnLaunchTemplate.MetadataOptionsProperty(
-                    http_tokens=get_http_tokens_setting(self._config.imds.imds_support)
-                ),
-                network_interfaces=login_node_lt_nw_interface,
-                # user_data=
-                # block_device_mappings=
-                tag_specifications=[
-                    ec2.CfnLaunchTemplate.TagSpecificationProperty(
-                        resource_type="instance",
-                        tags=get_default_instance_tags(
-                            self.stack_name, self._config, login_nodes_pool, "LoginNode", self._shared_storage_infos
-                        )
-                        # +
-                        # + custom tags for instance
-                    ),
-                    ec2.CfnLaunchTemplate.TagSpecificationProperty(
-                        resource_type="volume",
-                        tags=get_default_volume_tags(self.stack_name, "LoginNode")
-                        # +
-                        # + custom tags for volume
-                    ),
-                ],
-            ),
-        )
+            self.pools[pool.name] = pool_construct
 
     def _add_login_nodes_target_group(self):
         return elbv2.NetworkTargetGroup(
@@ -180,34 +230,3 @@ class LoginNodesStack(NestedStack):
         listener = login_nodes_load_balancer.add_listener("LoginNodeListener", port=22)
         listener.add_target_groups("LoginNodeListenerTargets", target_group)
         return login_nodes_load_balancer
-
-    def _add_auto_scaling_groups(self, login_nodes_target_group):
-        self.login_nodes_auto_scaling_groups = {}
-        for pool in self._login_nodes.pools:
-            self.login_nodes_auto_scaling_groups[pool.name] = self._add_login_nodes_pool_auto_scaling_group(
-                pool,
-                login_nodes_target_group,
-            )
-
-    def _add_login_nodes_pool_auto_scaling_group(
-        self,
-        login_nodes_pool: LoginNodesPool,
-        login_nodes_target_group,
-    ):
-        launch_template_specification = autoscaling.CfnAutoScalingGroup.LaunchTemplateSpecificationProperty(
-            launch_template_id=self.login_launch_templates[login_nodes_pool.name].ref,
-            version=self.login_launch_templates[login_nodes_pool.name].attr_latest_version_number,
-        )
-
-        auto_scaling_group = autoscaling.CfnAutoScalingGroup(
-            self,
-            f"{self._login_node_stack_id}-AutoScalingGroup",
-            launch_template=launch_template_specification,
-            min_size=str(login_nodes_pool.count),
-            max_size=str(login_nodes_pool.count),
-            desired_capacity=str(login_nodes_pool.count),
-            target_group_arns=[login_nodes_target_group.node.default_child.ref],
-            vpc_zone_identifier=[login_nodes_pool.networking.subnet_id],
-        )
-
-        return auto_scaling_group
