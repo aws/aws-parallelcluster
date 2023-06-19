72c737593168801acf6d9fb79ba914c3e7983cf0
Xuanqi He <xuanqihe@amazon.com>
2023-06-18 21:45:51 -0400

    move add target group and add load balancer function into Pool Construct, re-define vpc, subnet id and AZ

--- cli/src/pcluster/templates/login_nodes_stack.py
+++ cli/src/pcluster/templates/login_nodes_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_elasticloadbalancingv2 as elbv2
 from aws_cdk.core import Construct, NestedStack, Stack
 
-from pcluster.config.cluster_config import SlurmClusterConfig
+from pcluster.config.cluster_config import LoginNodesPool, SlurmClusterConfig
 from pcluster.templates.cdk_builder_utils import (
     CdkLaunchTemplateBuilder,
     LoginNodesIamResources,
@@ -23,13 +23,12 @@ class Pool(Construct):
         self,
         scope: Construct,
         id: str,
-        pool,
-        config,
+        pool: LoginNodesPool,
+        config: SlurmClusterConfig,
         shared_storage_infos,
         shared_storage_mount_dirs: Dict,
         shared_storage_attributes: Dict,
         login_security_group,
-        login_nodes_target_group,
         stack_name,
     ):
         super().__init__(scope, id)
@@ -40,12 +39,22 @@ class Pool(Construct):
         self._shared_storage_mount_dirs = shared_storage_mount_dirs
         self._shared_storage_attributes = shared_storage_attributes
         self._login_security_group = login_security_group
-        self._login_nodes_target_group = login_nodes_target_group
         self.stack_name = stack_name
 
         self._add_resources()
 
     def _add_resources(self):
+        self._vpc = ec2.Vpc.from_vpc_attributes(
+            self,
+            f"VPC{self._pool.name}",
+            vpc_id=self._config.vpc_id,
+            availability_zones=[self._pool.networking.availability_zone],
+        )
+        self._login_nodes_pool_target_group = self._add_login_nodes_pool_target_group()
+        self._login_nodes_pool_load_balancer = self._add_login_nodes_pool_load_balancer(
+            self._login_nodes_pool_target_group
+        )
+
         self._add_login_node_iam_resources()
         self._launch_template = self._add_login_nodes_pool_launch_template()
         self._add_login_nodes_pool_auto_scaling_group()
@@ -122,11 +131,49 @@ class Pool(Construct):
             min_size=str(self._pool.count),
             max_size=str(self._pool.count),
             desired_capacity=str(self._pool.count),
-            target_group_arns=[self._login_nodes_target_group.node.default_child.ref],
+            target_group_arns=[self._login_nodes_pool_target_group.node.default_child.ref],
             vpc_zone_identifier=[self._pool.networking.subnet_id],
         )
 
         return auto_scaling_group
+
+    def _add_login_nodes_pool_target_group(self):
+        return elbv2.NetworkTargetGroup(
+            self,
+            f"{self._pool.name}TargetGroup",
+            health_check=elbv2.HealthCheck(
+                port="22",
+                protocol=elbv2.Protocol.TCP,
+            ),
+            port=22,
+            protocol=elbv2.Protocol.TCP,
+            target_type=elbv2.TargetType.INSTANCE,
+            vpc=self._vpc,
+        )
+
+    def _add_login_nodes_pool_load_balancer(
+        self,
+        target_group,
+    ):
+        login_nodes_load_balancer = elbv2.NetworkLoadBalancer(
+            self,
+            f"{self._pool.name}LoadBalancer",
+            vpc=self._vpc,
+            internet_facing=True,
+            vpc_subnets=ec2.SubnetSelection(
+                subnets=[
+                    ec2.Subnet.from_subnet_id(
+                        self,
+                        f"LoginNodesSubnet{self._pool.name}",
+                        self._pool.networking.subnet_id,
+                    )
+                ]
+            ),
+        )
+
+        listener = login_nodes_load_balancer.add_listener(f"LoginNodesListener{self._pool.name}", port=22)
+        listener.add_target_groups(f"LoginNodesListenerTargets{self._pool.name}", target_group)
+        return login_nodes_load_balancer
 
 
 class LoginNodesStack(NestedStack):
@@ -159,23 +206,6 @@ class LoginNodesStack(NestedStack):
         return Stack.of(self.nested_stack_parent).stack_name
 
     def _add_resources(self):
-        # get unique login nodes pools subnet id
-        subnet_ids = set(pool.networking.subnet_id for pool in self._login_nodes.pools)
-        self._pools_subnet_ids = list(subnet_ids)
-
-        # get unique login nodes pools availability_zone
-        availability_zones = set(pool.networking.availability_zone for pool in self._login_nodes.pools)
-        self._pools_availability_zones = list(availability_zones)
-
-        self._vpc = ec2.Vpc.from_vpc_attributes(
-            self,
-            "VPC",
-            vpc_id=self._config.vpc_id,
-            availability_zones=self._pools_availability_zones,
-        )
-        self._login_nodes_target_group = self._add_login_nodes_target_group()
-        self._login_nodes_load_balancer = self._add_login_nodes_load_balancer(self._login_nodes_target_group)
-
         self.pools = {}
         for pool in self._login_nodes.pools:
             pool_construct = Pool(
@@ -187,43 +217,6 @@ class LoginNodesStack(NestedStack):
                 self._shared_storage_mount_dirs,
                 self._shared_storage_attributes,
                 self._login_security_group,
-                self._login_nodes_target_group,
                 self.stack_name,
             )
             self.pools[pool.name] = pool_construct
-
-    def _add_login_nodes_target_group(self):
-        return elbv2.NetworkTargetGroup(
-            self,
-            f"{self.stack_name}-TargetGroup",
-            health_check=elbv2.HealthCheck(
-                port="22",
-                protocol=elbv2.Protocol.TCP,
-            ),
-            port=22,
-            protocol=elbv2.Protocol.TCP,
-            target_group_name=f"{self.stack_name}-TargetGroup",
-            target_type=elbv2.TargetType.INSTANCE,
-            vpc=self._vpc,
-        )
-
-    def _add_login_nodes_load_balancer(
-        self,
-        target_group,
-    ):
-        login_nodes_load_balancer = elbv2.NetworkLoadBalancer(
-            self,
-            f"{self.stack_name}-LoadBalancer",
-            vpc=self._vpc,
-            internet_facing=True,
-            vpc_subnets=ec2.SubnetSelection(
-                subnets=[
-                    ec2.Subnet.from_subnet_id(self, f"LoginNodesSubnet{i}", subnet_id)
-                    for i, subnet_id in enumerate(self._pools_subnet_ids)
-                ]
-            ),
-        )
-
-        listener = login_nodes_load_balancer.add_listener("LoginNodeListener", port=22)
-        listener.add_target_groups("LoginNodeListenerTargets", target_group)
-        return login_nodes_load_balancer
