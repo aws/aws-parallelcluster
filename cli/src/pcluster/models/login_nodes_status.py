2fe2d20d36e8d50d607926dba2a7a73d4106eda4
Maurizio Pili <maurizp@amazon.com>
2023-07-15 21:39:33 +0200

    Retrieve Login Nodes pool based on its name in the describe cluster

--- cli/src/pcluster/models/login_nodes_status.py
+++ cli/src/pcluster/models/login_nodes_status.py
@@ -33,6 +33,7 @@ class LoginNodesStatus:
 
     def __init__(self, stack_name):
         self._stack_name = stack_name
+        self._login_nodes_pool_name = None
         self._login_nodes_pool_available = False
         self._load_balancer_arn = None
         self._target_group_arn = None
@@ -72,8 +73,9 @@ class LoginNodesStatus:
         """Return the number of unhealthy nodes of a login nodes fleet."""
         return self._unhealthy_nodes
 
-    def retrieve_data(self):
+    def retrieve_data(self, login_nodes_pool_name):
         """Initialize the class with the information related to the login nodes pool."""
+        self._login_nodes_pool_name = login_nodes_pool_name
         self._retrieve_assigned_load_balancer()
         if self._load_balancer_arn:
             self._login_nodes_pool_available = True
@@ -82,14 +84,8 @@ class LoginNodesStatus:
 
     def _retrieve_assigned_load_balancer(self):
         load_balancers = AWSApi.instance().elb.list_load_balancers()
-        tags = self._retrieve_all_tags([o.get("LoadBalancerArn") for o in load_balancers])
-        for tag in tags:
-            if any(
-                "parallelcluster:cluster-name" == kv.get("Key") and kv.get("Value") == self._stack_name
-                for kv in tag.get("Tags")
-            ):
-                self._load_balancer_arn = tag.get("ResourceArn")
-                break
+        tags_list = self._retrieve_all_tags([o.get("LoadBalancerArn") for o in load_balancers])
+        self._load_balancer_arn_from_tags(tags_list)
         if self._load_balancer_arn:
             for load_balancer in load_balancers:
                 if load_balancer.get("LoadBalancerArn") == self._load_balancer_arn:
@@ -97,6 +93,19 @@ class LoginNodesStatus:
                     self._dns_name = load_balancer.get("DNSName")
                     self._scheme = load_balancer.get("Scheme")
                     break
+
+    def _load_balancer_arn_from_tags(self, tags_list):
+        for tags in tags_list:
+            if self._key_value_tag_found(
+                tags, "parallelcluster:cluster-name", self._stack_name
+            ) and self._key_value_tag_found(tags, "parallelcluster:login-nodes-pool", self._login_nodes_pool_name):
+                self._load_balancer_arn = tags.get("ResourceArn")
+                break
+
+    def _key_value_tag_found(self, tags, key, value):
+        if any(key == kv.get("Key") and kv.get("Value") == value for kv in tags.get("Tags")):
+            return True
+        return False
 
     def _retrieve_all_tags(self, load_balancers):
         tags = []
