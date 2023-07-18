b06a9048b54fc814bd5cf919dea89c408dbc7b23
kkolur5 <99568777+kkolur5@users.noreply.github.com>
2023-07-18 11:46:23 -0400

    [Test]Replacing S3 readonly access to restricted S3 access. (#5501)
    * [Test]Adding Integration test to automate performance testing using OpenFOAM.
    
    * [Test]Replacing S3 readonly access to restricted S3 access.

--- tests/integration-tests/tests/performance_tests/test_starccm/test_starccm/starccm.install.sh
+++ tests/integration-tests/tests/performance_tests/test_starccm/test_starccm/starccm.install.sh
@@ -21,7 +21,7 @@ tar -xf $(basename ${STARCCM_PACKAGE_ZIP})
 STARCCM_INSTALLER_ZIP="STAR-CCM+16.02.008_01_linux-x86_64-2.17_gnu9.2.zip"
 unzip ${STARCCM_INSTALLER_ZIP}
 cd STAR-CCM+16.02.008_01_linux-x86_64-2.17_gnu9.2
-# (exeucted by the below printf) During the installation, you need to type: <ENTER>, Y, N, /shared/${TARGET_USER}/STAR-CCM+, Y, <ENTER>, <ENTER>
+# (executed by the below printf) During the installation, you need to type: <ENTER>, Y, N, /shared/${TARGET_USER}/STAR-CCM+, Y, <ENTER>, <ENTER>
 printf "\nY\nN\n${TARGET_USER_DIR}/STAR-CCM+\nY\n\n\n" | ./STAR-CCM+16.02.008_01_linux-x86_64-2.17_gnu9.2.sh
 
 rm -rf ${STARCCM_PACKAGE_ZIP}
