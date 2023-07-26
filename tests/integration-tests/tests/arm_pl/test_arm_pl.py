dfa4e93f83aff32f6e6c69f2c9bc2c040021ea02
Ryan Anderson <ndry@amazon.com>
2023-07-26 09:38:41 -0400

    Use the base dir based on os for armpl tests (#5532)
    Co-authored-by: Himani Anil Deshpande <79726937+himani2411@users.noreply.github.com>

--- tests/integration-tests/tests/arm_pl/test_arm_pl.py
+++ tests/integration-tests/tests/arm_pl/test_arm_pl.py
@@ -76,14 +76,16 @@ def _test_armpl_examples(
     # On centos7 we need to use binutils v2.30 for proper architecture detection
     scl_centos7 = "scl enable devtoolset-8" if os == "centos7" else ""
 
-    # Assert pass the example tests
-    remote_command_executor.run_remote_command(
-        f"sudo chmod 777 /opt/arm/armpl/{armpl_version}/armpl_{armpl_major_minor_version}_gcc-{gcc_version}/examples"
+    armpl_base_dir = (
+        f"/opt/arm/armpl/{armpl_version}/armpl_{armpl_version}_gcc-{gcc_version}"
+        if os == "ubuntu2204"
+        else f"/opt/arm/armpl/{armpl_version}/armpl_{armpl_major_minor_version}_gcc-{gcc_version}"
     )
+    # Assert pass the example tests
+    remote_command_executor.run_remote_command(f"sudo chmod 777 {armpl_base_dir}/examples")
     test_result = remote_command_executor.run_remote_command(
         f"module load {armpl_module_general_name} && "
-        f"cd /opt/arm/armpl/{armpl_version}/"
-        f"armpl_{armpl_major_minor_version}_gcc-{gcc_version}/examples && make clean && {scl_centos7} make"
+        f"cd {armpl_base_dir}/examples && make clean && {scl_centos7} make"
     ).stdout.lower()
     assert_that(test_result).contains("testing: no example difference files were generated")
     assert_that(test_result).contains("test passed ok")
