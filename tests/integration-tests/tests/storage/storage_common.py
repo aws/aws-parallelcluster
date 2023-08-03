6f44296268e869f11356ea2e329030097980a1a4
Xuanqi He <xuanqihe@amazon.com>
2023-08-03 11:45:42 -0400

    Add integration test for LoginNodes on File system(ebs, efs)

--- tests/integration-tests/tests/storage/storage_common.py
+++ tests/integration-tests/tests/storage/storage_common.py
@@ -52,6 +52,55 @@ def get_cluster_subnet_ids_groups(cluster: Cluster, scheduler: str, include_head
     return compute_subnet_ids
 
 
+def test_directory_correctly_shared_between_ln_and_hn(
+    remote_command_executor_head_node, remote_command_executor_login_node, mount_dir
+):
+    """
+    This test verifies if a shared directory is correctly shared between the HeadNode and the LoginNode.
+
+    Preconditions:
+    - An EBS volume is expected to be mounted at <HN_folder> on the HeadNode and exported through NFS.
+    - The EBS volume is expected to be mounted at <LN_folder> on the LoginNode through NFS.
+
+    Test Steps:
+    1. Writes a file from the HeadNode to the shared volume.
+    2. Writes a file from the LoginNode to the shared volume.
+    3. Reads both files from both the HeadNode and the LoginNode to validate that shared volume is working correctly.
+
+    Expected Result:
+    - Both files can be successfully read from both the HeadNode and the LoginNode,
+      proving that the shared volume is correctly shared between the two nodes.
+    """
+    logging.info("Testing FS correctly mounted on login nodes")
+    head_node_file = random_alphanumeric()
+    logging.info(f"Writing HeadNode File: {head_node_file}")
+    remote_command_executor_head_node.run_remote_command(
+        "touch {mount_dir}/{head_node_file} && cat {mount_dir}/{head_node_file}".format(
+            mount_dir=mount_dir, head_node_file=head_node_file
+        )
+    )
+
+    login_node_file = random_alphanumeric()
+    logging.info(f"Writing LoginNode File: {login_node_file}")
+    remote_command_executor_login_node.run_remote_command(
+        "touch {mount_dir}/{login_node_file} && cat {mount_dir}/{login_node_file}".format(
+            mount_dir=mount_dir, login_node_file=login_node_file
+        )
+    )
+
+    files_to_read = [head_node_file, login_node_file]
+    read_all_files_command = "cat {files_to_read}".format(
+        files_to_read=" ".join([f"{mount_dir}/{target_file}" for target_file in files_to_read]),
+    )
+    # Attempt reading files from HeadNode
+    logging.info(f"Reading Files: {files_to_read} from HeadNode")
+    remote_command_executor_head_node.run_remote_command(read_all_files_command)
+
+    # Attempt reading files from LoginNode
+    logging.info(f"Reading Files: {files_to_read} from LoginNode")
+    remote_command_executor_login_node.run_remote_command(read_all_files_command)
+
+
 def verify_directory_correctly_shared(remote_command_executor, mount_dir, scheduler_commands, partitions=None):
     """
     Confirm nodes can read and write to the FileSystem
@@ -64,16 +113,16 @@ def verify_directory_correctly_shared(remote_command_executor, mount_dir, schedu
         "A" reads files: ["A-<random_alphanumeric_characters>", "B-<random_alphanumeric_characters>"]
         "B" reads files: ["A-<random_alphanumeric_characters>", "B-<random_alphanumeric_characters>"]
     """
-    head_node_file = random_alphanumeric()
-    logging.info(f"Writing HeadNode File: {head_node_file}")
+    executor_node_file = random_alphanumeric()
+    logging.info(f"{remote_command_executor.get_target_host_type()}: Writing File: {executor_node_file}")
     remote_command_executor.run_remote_command(
-        "touch {mount_dir}/{head_node_file} && cat {mount_dir}/{head_node_file}".format(
-            mount_dir=mount_dir, head_node_file=head_node_file
+        "touch {mount_dir}/{executor_node_file} && cat {mount_dir}/{executor_node_file}".format(
+            mount_dir=mount_dir, executor_node_file=executor_node_file
         )
     )
 
     # Submit a "Write" job to each partition
-    files_to_read = [head_node_file]
+    files_to_read = [executor_node_file]
     partitions = (
         partitions or scheduler_commands.get_partitions() if isinstance(scheduler_commands, SlurmCommands) else [None]
     )
@@ -92,8 +141,8 @@ def verify_directory_correctly_shared(remote_command_executor, mount_dir, schedu
     read_all_files_command = "cat {files_to_read}".format(
         files_to_read=" ".join([f"{mount_dir}/{target_file}" for target_file in files_to_read]),
     )
-    # Attempt reading files from HeadNode
-    logging.info(f"Reading Files: {files_to_read} from HeadNode")
+    # Attempt reading files from executor node
+    logging.info(f"{remote_command_executor.get_target_host_type()}: Reading Files: {files_to_read}")
     remote_command_executor.run_remote_command(read_all_files_command)
 
     # Submit a "Read" job to each partition
