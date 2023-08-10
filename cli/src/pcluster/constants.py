4082d274d70a89d8f1a90d1a3efdd48486729652
Enrico Usai <usai@amazon.com>
2023-08-10 12:42:42 +0200

    Upgrade Cinc to version 18.2.7
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/src/pcluster/constants.py
+++ cli/src/pcluster/constants.py
@@ -119,7 +119,7 @@ MAX_EXISTING_STORAGE_COUNT = {"efs": 20, "fsx": 20, "raid": 0}
 COOKBOOK_PACKAGES_VERSIONS = {
     "parallelcluster": "3.8.0",
     "cookbook": "aws-parallelcluster-cookbook-3.8.0",
-    "chef": "17.2.29",
+    "chef": "18.2.7",
     "berkshelf": "7.2.0",
     "ami": "dev",
 }
