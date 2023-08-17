c612195248cad925ac1b8a37a64f913e3f0e1dc3
Jacopo De Amicis <jdamicis@amazon.it>
2023-08-17 15:54:11 +0200

    Extend Slurm submission commands for `--test-only`
    Add new parameter for Slurm submission methods to support the `--test-only`
    option of `sbatch`, which allows to test the submission of jobs without
    actually submitting them.
    
    Adapt existing integration tests that were using the `--test-only` inside
    the `other_options` parameter.
    
    Adapt abstract method signature for assert_job_submitted
    
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- tests/integration-tests/tests/schedulers/test_slurm.py
+++ tests/integration-tests/tests/schedulers/test_slurm.py
@@ -2294,7 +2294,8 @@ def _test_memory_based_scheduling_enabled_false(
         nodes=1,
         command="sleep 1",
         constraint="ondemand1-i1",
-        other_options="--mem=4000 --test-only",
+        test_only=True,
+        other_options="--mem=4000",
         raise_on_error=False,
     )
     assert_that(result.stdout).is_equal_to("allocation failure: Requested node configuration is not available")
@@ -2303,7 +2304,8 @@ def _test_memory_based_scheduling_enabled_false(
     result = slurm_commands.submit_command(
         nodes=1,
         command="sleep 1",
-        other_options="--mem=4000 --test-only",
+        test_only=True,
+        other_options="--mem=4000",
         raise_on_error=False,
     )
     assert_that(result.stdout).matches(r"^.*Job \d* to start.*$")
@@ -2393,7 +2395,8 @@ def _test_memory_based_scheduling_enabled_true(
         slots=2,
         command="sleep 1",
         constraint="ondemand1-i1",
-        other_options="-c 1 --mem-per-cpu=2000 --test-only",
+        test_only=True,
+        other_options="-c 1 --mem-per-cpu=2000",
         raise_on_error=False,
     )
     assert_that(result.stdout).is_equal_to("allocation failure: Requested node configuration is not available")
