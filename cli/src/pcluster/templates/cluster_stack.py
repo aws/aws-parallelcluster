1c5dad801f891361d5216cb9ba816d641c82138b
Eva Zhong <xiaoyaoe@amazon.com>
2023-09-18 17:48:19 -0400

    add size restrictions

--- cli/src/pcluster/templates/cluster_stack.py
+++ cli/src/pcluster/templates/cluster_stack.py
@@ -1422,12 +1422,12 @@ class ClusterCdkStack:
                             f" cfn-signal --exit-code=0 --reason='Update complete'"
                             f" --region {self.stack.region} --url {cloudformation_url}"
                             f" '{self.wait_condition_handle.ref}' ||"
-                            f' cfn-signal --exit-code=1 --reason="$(cat /var/log/parallelcluster/bootstrap_error_msg '
-                            f"2>/dev/null || echo 'Update failed')\""
+                            f" cfn-signal --exit-code=1 --reason=\"$(head -c 4096 /var/log/parallelcluster/bootstrap_error_msg 2>/dev/null || echo 'Update failed')\""
                             f" --region {self.stack.region} --url {cloudformation_url}"
-                            f" '{self.wait_condition_handle.ref}'"
+                            f" '{self.wait_condition_handle.ref}';"
                             " rm -f /var/log/parallelcluster/bootstrap_error_msg"
                         ),
+
                         "cwd": "/etc/chef",
                     }
                 }
