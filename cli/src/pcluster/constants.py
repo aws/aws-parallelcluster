c28ecb2b2ef633c1c25955584e4b81a7340a945f
Jacopo De Amicis <jdamicis@amazon.it>
2023-10-18 15:37:48 +0200

    Bump version to 3.9.0
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cli/src/pcluster/constants.py
+++ cli/src/pcluster/constants.py
@@ -121,8 +121,8 @@ MAX_NEW_STORAGE_COUNT = {"efs": 1, "fsx": 1, "raid": 1}
 MAX_EXISTING_STORAGE_COUNT = {"efs": 20, "fsx": 20, "raid": 0}
 
 COOKBOOK_PACKAGES_VERSIONS = {
-    "parallelcluster": "3.8.0",
-    "cookbook": "aws-parallelcluster-cookbook-3.8.0",
+    "parallelcluster": "3.9.0",
+    "cookbook": "aws-parallelcluster-cookbook-3.9.0",
     "chef": "18.2.7",
     "berkshelf": "8.0.7",
     "ami": "dev",
