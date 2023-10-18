399e149d77e07636276a65d9e40fbdc2cff2bcf5
chenwany <chenwany@amazon.com>
2023-10-18 14:55:49 -0700

    Fix test_intel_hpc integration test by disable checking expired gpg key when running integ test
    The gpg key is a valid keyt but expired on 2023-9-30 and intel-clck-2019 is an unsupported version now
    
    curl -fsSL https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB | gpg2 --import
    [ec2-user@ip-172-31-47-208 ~]$ curl -fsSL -O https://yum.repos.intel.com/clck/2019/repodata/repomd.xml.asc
    [root@ip-172-31-45-34 ~]# gpg --verify repomd.xml.asc
    gpg: Signature made Fri 23 Oct 2020 02:46:46 PM UTC using RSA key ID 7E6C5DBE
    gpg: Good signature from "Intel(R) Software Development Products"
    gpg: Note: This key has expired!
    Primary key fingerprint: 52AB D6E8 7E42 1793 9718  73FF ACFA 9FC5 7E6C 5DBE
    
    Disable the key verification when running integration test to download intel-clck-2019
    
    Signed-off-by: chenwany <chenwany@amazon.com>

--- tests/integration-tests/tests/intel_hpc/test_intel_hpc/test_intel_hpc/install_clck.sh
+++ tests/integration-tests/tests/intel_hpc/test_intel_hpc/test_intel_hpc/install_clck.sh
@@ -8,4 +8,9 @@ set -e
 sudo rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 sudo yum-config-manager --add-repo https://yum.repos.intel.com/clck/2019/setup/intel-clck-2019.repo
 sudo yum-config-manager --add-repo https://yum.repos.intel.com/clck-ext/2019/setup/intel-clck-ext-2019.repo
+# Disable the verification of the gpg key since it is expired on 2023-09-30 and intel-clck-2019 is an unsupported version
+sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/intel-clck-ext-2019.repo
+sudo sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/intel-clck-ext-2019.repo
+sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/intel-clck-2019.repo
+sudo sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/intel-clck-2019.repo
 sudo yum -y install intel-clck-2019.9-056
