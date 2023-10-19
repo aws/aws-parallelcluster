dff42fe17a59adfd4331ebacf6667e0552286784
Xuanqi He <xuanqihe@amazon.com>
2023-10-18 22:20:23 -0400

    fix integration tests

--- tests/integration-tests/tests/schedulers/test_custom_munge_key.py
+++ tests/integration-tests/tests/schedulers/test_custom_munge_key.py
@@ -9,10 +9,13 @@
 # or in the "LICENSE.txt" file accompanying this file.
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+from time import sleep
+
 import pytest
 from assertpy import assert_that
 from constants import ENCODE_CUSTOM_MUNGE_KEY
 from remote_command_executor import RemoteCommandExecutor
+from utils import wait_for_computefleet_changed
 
 
 @pytest.mark.usefixtures("instance", "os")
@@ -21,8 +24,6 @@ def test_custom_munge_key(
     region,
     pcluster_config_reader,
     clusters_factory,
-    test_datadir,
-    architecture,
     scheduler_commands_factory,
     store_secret_in_secret_manager,
     use_login_node,
@@ -56,9 +57,11 @@ def test_custom_munge_key(
     if use_login_node:
         remote_command_executor_login = RemoteCommandExecutor(cluster, use_login_node=True)
         _test_custom_munge_key_fetch_and_decode(remote_command_executor_login)
-        _test_munge_key_shared(remote_command_executor_login)
+        remote_command_executor_login.close_connection()
+
     # Stop compute fleets
     cluster.stop()
+    wait_for_computefleet_changed(cluster, "STOPPED")
 
     if use_login_node:
         # Test error message when login nodes are not stopped.
@@ -69,7 +72,19 @@ def test_custom_munge_key(
             config_file="pcluster.stop_login.config.yaml",
             custom_munge_key_arn=custom_munge_key_arn,
         )
-        cluster.update(update_cluster_stop_login_config)
+        cluster.update(str(update_cluster_stop_login_config))
+
+    # wait for LoginNodes gracetime_period
+    for _i in range(5):
+        result = remote_command_executor.run_remote_command(
+            "sudo /opt/parallelcluster/scripts/slurm/check_login_nodes_stopped.sh",
+            raise_on_error=False,
+        )
+        exit_code = result.return_code
+        if exit_code == 0:
+            break
+        else:
+            sleep(180)
 
     # Test rotation script run successfully
     result = remote_command_executor.run_remote_command("sudo /opt/parallelcluster/scripts/slurm/update_munge_key.sh")
@@ -80,7 +95,7 @@ def test_custom_munge_key(
         config_file="pcluster.remove_custom_munge_key.config.yaml",
         use_login_node=use_login_node,
     )
-    cluster.update(update_cluster_remove_custom_munge_key_config)
+    cluster.update(str(update_cluster_remove_custom_munge_key_config))
     # Test Munge Key has been changed
     _test_custom_munge_key_fetch_and_decode(remote_command_executor, use_custom_munge_key=False)
 
@@ -88,7 +103,7 @@ def test_custom_munge_key(
 def _test_custom_munge_key_fetch_and_decode(remote_command_executor, use_custom_munge_key=True):
     """Test encoded munge key in secret manager has been successfully fetched by cluster and decode."""
     result = remote_command_executor.run_remote_command("sudo cat /etc/munge/munge.key | base64")
-    encode_munge_key = result.stdout.strip()
+    encode_munge_key = result.stdout.strip().replace("\n", "")
     if use_custom_munge_key:
         assert_that(encode_munge_key).is_equal_to(ENCODE_CUSTOM_MUNGE_KEY)
     else:
@@ -98,7 +113,7 @@ def _test_custom_munge_key_fetch_and_decode(remote_command_executor, use_custom_
 def _test_munge_key_shared(remote_command_executor):
     """Test munge key has been successfully shared to shared directory."""
     compute_node_munge_key_path = "/opt/parallelcluster/shared/.munge/.munge.key"
-    head_node_munge_key_path = "/opt/parallelcluster/shared_login_node/.munge/.munge.key"
+    head_node_munge_key_path = "/opt/parallelcluster/shared_login_nodes/.munge/.munge.key"
 
     assert_that(
         remote_command_executor.run_remote_command(f"sudo test -f {compute_node_munge_key_path}").return_code,
@@ -120,7 +135,7 @@ def _test_update_munge_key_without_stop_login_or_compute(remote_command_executor
     exit_code = result.return_code
     assert_that(exit_code).is_equal_to(1)
     if compute_stopped:
-        expected_message = "Compute fleet is not stopped."
-    else:
         expected_message = "Login nodes are running."
+    else:
+        expected_message = "Compute fleet is not stopped."
     assert_that(command_output).contains(expected_message)
