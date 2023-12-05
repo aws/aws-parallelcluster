a5f80f0b470a19024ff461502284a91f96d8cc7e
Jacopo De Amicis <jdamicis@amazon.it>
2023-12-05 18:38:49 +0100

    Add check for private IP assignment
    Add bash function to check that the secondary private IP was
    correctly assigned to the instance via EC2.
    
    Add DescribeNetworkInterfaces policy to allow the implemented
    check.
    
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cloudformation/external-slurmdbd/resources/user_data.sh
+++ cloudformation/external-slurmdbd/resources/user_data.sh
@@ -14,11 +14,28 @@ function vendor_cookbook
   export HOME="${!HOME_BAK}"
 }
 
+function wait_for_private_ip_assignment
+{
+  rc=1
+  retries=10
+  retry=1
+  sleeptime=1
+  while [ \( $rc -eq 1 \) -a \( $retry -le $retries \) ]; do
+    aws ec2 describe-network-interfaces --network-interface-ids "${!ENI_ID}" --region "${AWS::Region}" | jq .NetworkInterfaces[0].PrivateIpAddresses | grep -q '"PrivateIpAddress": "${PrivateIp}"'
+    rc=$?
+    retry=$((retry+1))
+    sleep $sleeptime
+  done
+  return $rc
+}
+
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
 ENI_ID=$(aws ec2 describe-instances --instance-ids ${!INSTANCE_ID} --region "${AWS::Region}" | jq .Reservations[0].Instances[0].NetworkInterfaces[0].NetworkInterfaceId | tr -d '"')
 
 aws ec2 assign-private-ip-addresses --region "${AWS::Region}" --network-interface-id "${!ENI_ID}" --private-ip-addresses ${PrivateIp} --allow-reassignment
+
+wait_for_private_ip_assignment || echo "Assignment of private IP ${PrivateIp} was not successful."
 
 ip addr add ${PrivateIp}/${SubnetPrefix} dev eth0
 
