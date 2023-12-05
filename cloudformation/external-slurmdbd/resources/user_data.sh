6d0cd4f23a803acde31b477a44cd3b92f8c31d7f
Jacopo De Amicis <jdamicis@amazon.it>
2023-12-05 02:00:02 +0100

    Set up private IP for external slurmdbd instance
    Add components to allow the assignment of static private secondary
    IP address to the slurmdbd instance.
    
    Add possibility to set up the external slurmdbd instance as a
    standalone instance in the CDK app rather than using an ASG.
    
    Minor improvements to the provisional user_data.sh file.
    
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cloudformation/external-slurmdbd/resources/user_data.sh
+++ cloudformation/external-slurmdbd/resources/user_data.sh
@@ -7,12 +7,20 @@ function vendor_cookbook
   tar -xzf /etc/chef/aws-parallelcluster-cookbook.tgz
   HOME_BAK="${!HOME}"
   export HOME="/tmp"
-  for d in `ls /tmp/cookbooks`; do
-    cd /tmp/cookbooks/$d
+  for d in /tmp/cookbooks/*; do
+    cd "$d" || continue
     LANG=en_US.UTF-8 /opt/cinc/embedded/bin/berks vendor /etc/chef/cookbooks --delete
   done;
   export HOME="${!HOME_BAK}"
 }
+
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
+ENI_ID=$(aws ec2 describe-instances --instance-ids ${!INSTANCE_ID} --region "${AWS::Region}" | jq .Reservations[0].Instances[0].NetworkInterfaces[0].NetworkInterfaceId | tr -d '"')
+
+aws ec2 assign-private-ip-addresses --region "${AWS::Region}" --network-interface-id "${!ENI_ID}" --private-ip-addresses ${PrivateIp} --allow-reassignment
+
+ip addr add ${PrivateIp}/${SubnetPrefix} dev eth0
 
 if [ "${CustomCookbookUrl}" != "NONE" ]; then
   curl --retry 3 -v -L -o /etc/chef/aws-parallelcluster-cookbook.tgz ${CustomCookbookUrl}
