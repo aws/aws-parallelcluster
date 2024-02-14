4362d7b4f4eb8d2105f2d80e46b47457901471b2
Giacomo Marciani <mgiacomo@amazon.com>
2024-02-14 21:42:31 +0100

    [Test] Use argument typing compatible with Python 3.7.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- tests/integration-tests/utils.py
+++ tests/integration-tests/utils.py
@@ -21,6 +21,7 @@ import subprocess
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from hashlib import sha1
+from typing import List
 
 import boto3
 import requests
@@ -60,7 +61,7 @@ def generate_metric_data_entry(metric_name: str, dimensions_as_dict: dict, value
     }
 
 
-def publish_metrics_to_cloudwatch(namespace: str, cw_client, cw_metrics: list[CWMetric]):
+def publish_metrics_to_cloudwatch(namespace: str, cw_client, cw_metrics: List[CWMetric]):
     cw_client.put_metric_data(
         Namespace=namespace,
         MetricData=[
