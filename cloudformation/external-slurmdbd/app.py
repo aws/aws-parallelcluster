50fbae9c68f73f1d253c5be98bc48284863f6734
Hanwen <hanwenli@amazon.com>
2024-03-28 12:33:43 -0700

    Add Json template generated from Python AWS CDK code
    This template will be provided in ParallelCluster public documentation and used in integration tests. This commit also adds a Github check to ensure the external-slurmdbd.json is updated everytime external_slurmdbd_stack.py is updated. This approach is consistent with what we are doing for "API Model Validation"
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cloudformation/external-slurmdbd/app.py
+++ cloudformation/external-slurmdbd/app.py
@@ -4,6 +4,8 @@ import aws_cdk as cdk
 from external_slurmdbd.external_slurmdbd_stack import ExternalSlurmdbdStack
 
 app = cdk.App()
-ExternalSlurmdbdStack(app, "ExternalSlurmdbdStack")
+ExternalSlurmdbdStack(
+    app, "ExternalSlurmdbdStack", synthesizer=cdk.DefaultStackSynthesizer(generate_bootstrap_version_rule=False)
+)
 
 app.synth()
