8c0278363a357eb537f825e8b395734d97c2c832
Chris Makin <cmakin@amazon.com>
2024-07-11 21:20:38 -0400

    Update test_dcv integration test to include login nodes

--- tests/integration-tests/tests/dcv/test_dcv.py
+++ tests/integration-tests/tests/dcv/test_dcv.py
@@ -20,7 +20,7 @@ from framework.credential_providers import run_pcluster_command
 from remote_command_executor import RemoteCommandExecutionError, RemoteCommandExecutor
 from utils import (
     add_keys_to_known_hosts,
-    check_head_node_security_group,
+    check_node_security_group,
     get_username_for_os,
     remove_keys_from_known_hosts,
 )
@@ -52,75 +52,36 @@ def _test_dcv_configuration(
     dcv_authenticator_port = dcv_port + 1
     cluster_config = pcluster_config_reader(dcv_port=str(dcv_port), access_from=access_from)
     cluster = clusters_factory(cluster_config)
-    remote_command_executor = RemoteCommandExecutor(cluster)
 
-    # check configuration parameters
-    check_head_node_security_group(region, cluster, dcv_port, expected_cidr=access_from)
+    # command executors for the head and login nodes
+    head_node_remote_command_executor = RemoteCommandExecutor(cluster)
+    login_node_remote_command_executor = RemoteCommandExecutor(cluster, use_login_node=True)
 
-    # dcv connect show url
-    env = operating_system.environ.copy()
-    env["AWS_DEFAULT_REGION"] = region
+    # check configuration parameters of the head and login nodes
+    check_node_security_group(region, cluster, dcv_port, expected_cidr=access_from, node_type="HeadNode")
+    check_node_security_group(region, cluster, dcv_port, expected_cidr=access_from, node_type="LoginNode")
 
-    # add ssh key to jenkins user known hosts file to avoid ssh keychecking prompt
-    host_keys_file = operating_system.path.expanduser("~/.ssh/known_hosts")
-    add_keys_to_known_hosts(cluster.head_node_ip, host_keys_file)
-
-    try:
-        result = run_pcluster_command(
-            ["pcluster", "dcv-connect", "--cluster-name", cluster.name, "--show-url"], env=env
-        )
-    finally:
-        # remove ssh key from jenkins user known hosts file
-        remove_keys_from_known_hosts(cluster.head_node_ip, host_keys_file, env=env)
-
-    assert_that(result.stdout).matches(
-        r"Please use the following one-time URL in your browser within 30 seconds:\n"
-        r"https:\/\/(\b(?:\d{1,3}\.){3}\d{1,3}\b):" + str(dcv_port) + r"\?authToken=(.*)"
-    )
-    if access_from == "0.0.0.0/0":
-        url = re.search(r"https:\/\/.*", result.stdout).group(0)
-        response = requests.get(url, verify=False)
-        assert_that(response.status_code).is_equal_to(200)
+    # test dcv connect show url for head and login node
+    _test_show_url(cluster, region, dcv_port, access_from)
+    _test_show_url(cluster, region, dcv_port, access_from,  use_login_node=True)
 
     # check error cases
-    _check_auth_ko(
-        remote_command_executor,
-        dcv_authenticator_port,
-        "-d action=requestToken -d authUser=centos -d sessionID=invalidSessionId",
-        "The given session does not exists",
-    )
-    _check_auth_ko(
-        remote_command_executor, dcv_authenticator_port, "-d action=test", "The action specified 'test' is not valid"
-    )
-    _check_auth_ko(
-        remote_command_executor, dcv_authenticator_port, "-d action=requestToken -d authUser=centos", "Wrong parameters"
-    )
+    _check_error_cases(head_node_remote_command_executor, dcv_authenticator_port)
+    _check_error_cases(login_node_remote_command_executor, dcv_authenticator_port)
 
     shared_dir = f"/home/{get_username_for_os(os)}"
 
     # launch a session and verify the authenticator works
-    command_execution = remote_command_executor.run_remote_command(f"{DCV_CONNECT_SCRIPT} {shared_dir}")
-    dcv_parameters = re.search(
-        r"PclusterDcvServerPort=([\d]+) PclusterDcvSessionId=([\w]+) PclusterDcvSessionToken=([\w-]+)",
-        command_execution.stdout,
-    )
-    if dcv_parameters:
-        dcv_session_id = dcv_parameters.group(2)
-        dcv_session_token = dcv_parameters.group(3)
-        _check_auth_ok(remote_command_executor, dcv_authenticator_port, dcv_session_id, dcv_session_token, os)
-    else:
-        print(
-            "Command '{0} {1}' fails, output: {2}, error: {3}".format(
-                DCV_CONNECT_SCRIPT, shared_dir, command_execution.stdout, command_execution.stderr
-            )
-        )
-        raise AssertionError
+    _test_authenticator(head_node_remote_command_executor, dcv_authenticator_port, shared_dir, os)
+    _test_authenticator(login_node_remote_command_executor, dcv_authenticator_port, shared_dir, os)
 
     # check shared dir configuration
-    _check_shared_dir(remote_command_executor, shared_dir)
+    _check_shared_dir(head_node_remote_command_executor, shared_dir)
+    _check_shared_dir(login_node_remote_command_executor, shared_dir)
 
     # Ensure no system programs crashed
-    _check_no_crashes(remote_command_executor, test_datadir)
+    _check_no_crashes(head_node_remote_command_executor, test_datadir)
+    _check_no_crashes(login_node_remote_command_executor, test_datadir)
 
     # Check that logs are stored in CloudWatch as expected
     FeatureSpecificCloudWatchLoggingTestRunner.run_tests_for_feature(
@@ -159,3 +120,71 @@ def _check_auth_ok(remote_command_executor, external_authenticator_port, session
 def _check_no_crashes(remote_command_executor, test_datadir):
     """Verify no core files in /var/crash, which on ubuntu18 causes a popup when logging into the 1st session."""
     remote_command_executor.run_remote_script(str(test_datadir / "verify_no_core_files.sh"))
+
+
+def _check_error_cases(remote_command_executor, dcv_authenticator_port):
+    """Check DCV errors for both head and login nodes."""
+    _check_auth_ko(
+        remote_command_executor,
+        dcv_authenticator_port,
+        "-d action=requestToken -d authUser=centos -d sessionID=invalidSessionId",
+        "The given session does not exists",
+    )
+    _check_auth_ko(
+        remote_command_executor, dcv_authenticator_port, "-d action=test", "The action specified 'test' is not valid"
+    )
+    _check_auth_ko(
+        remote_command_executor, dcv_authenticator_port, "-d action=requestToken -d authUser=centos", "Wrong parameters"
+    )
+
+
+def _test_show_url(cluster, region, dcv_port, access_from, use_login_node=False):
+    """Test dcv-connect with --show-url."""
+    env = operating_system.environ.copy()
+    env["AWS_DEFAULT_REGION"] = region
+
+    node_ip = cluster.get_login_node_public_ip() if use_login_node else cluster.head_node_ip
+
+    # add ssh key to jenkins user known hosts file to avoid ssh keychecking prompt
+    host_keys_file = operating_system.path.expanduser("~/.ssh/known_hosts")
+    add_keys_to_known_hosts(node_ip, host_keys_file)
+
+    dcv_connect_args = ["pcluster", "dcv-connect", "--cluster-name", cluster.name, "--show-url"]
+
+    if use_login_node:
+        dcv_connect_args.append(["--login-node-ip", node_ip])
+
+    try:
+        result = run_pcluster_command(dcv_connect_args, env=env)
+    finally:
+        # remove ssh key from jenkins user known hosts file
+        remove_keys_from_known_hosts(node_ip, host_keys_file, env=env)
+
+    assert_that(result.stdout).matches(
+        r"Please use the following one-time URL in your browser within 30 seconds:\n"
+        r"https:\/\/(\b(?:\d{1,3}\.){3}\d{1,3}\b):" + str(dcv_port) + r"\?authToken=(.*)"
+    )
+    if access_from == "0.0.0.0/0":
+        url = re.search(r"https:\/\/.*", result.stdout).group(0)
+        response = requests.get(url, verify=False)
+        assert_that(response.status_code).is_equal_to(200)
+
+
+def _test_authenticator(remote_command_executor, dcv_authenticator_port, shared_dir, os):
+    """Launch a DCV session and verify authenticator."""
+    command_execution = remote_command_executor.run_remote_command(f"{DCV_CONNECT_SCRIPT} {shared_dir}")
+    dcv_parameters = re.search(
+        r"PclusterDcvServerPort=([\d]+) PclusterDcvSessionId=([\w]+) PclusterDcvSessionToken=([\w-]+)",
+        command_execution.stdout,
+    )
+    if dcv_parameters:
+        dcv_session_id = dcv_parameters.group(2)
+        dcv_session_token = dcv_parameters.group(3)
+        _check_auth_ok(remote_command_executor, dcv_authenticator_port, dcv_session_id, dcv_session_token, os)
+    else:
+        print(
+            "Command '{0} {1}' fails, output: {2}, error: {3}".format(
+                DCV_CONNECT_SCRIPT, shared_dir, command_execution.stdout, command_execution.stderr
+            )
+        )
+        raise AssertionError
