15d3e04ae4349226ce40ab7de24f5d7c36529614
Xuanqi He <xuanqihe@amazon.com>
2025-03-18 17:04:48 -0400

    Add a 45 seconds delay to accommodate node replacement process

--- tests/integration-tests/tests/schedulers/test_slurm.py
+++ tests/integration-tests/tests/schedulers/test_slurm.py
@@ -1437,6 +1437,8 @@ def _wait_for_node_reset(
             wait_fixed_secs=wait_fixed_secs,
             stop_max_delay_secs=stop_max_delay_secs,
         )
+        # Add delay to accommodate node replacement process (~45s between node down status and replacement)
+        time.sleep(45)
         logging.info("Assert static nodes are replaced")
         wait_for_compute_nodes_states(
             scheduler_commands,
