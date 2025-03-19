25f4f3830dbfd5b47cab5894b8366e2b16ba8c04
Xuanqi He <93849823+hehe7318@users.noreply.github.com>
2025-03-19 10:41:06 -0400

    [Integ-Test] Fix test_slurm_scaling by increasing job running time and setting stop_max_delay_secs based on the OS (#6737)
    * Use c5.xlarge instead of hpc5a instance type. The slurm job is just sleep.
    * We noticed bootstrap time in Rocky and Rhel is explicitly longer than other OSs. `c5.xlarge` bootstrap time in Rocky and Rhel is around 6 minutes. Set stop_max_delay_secs based on the OS. Increase `wait_for_num_nodes_in_scheduler` timeout to 7 mins.
    * Ensure job running time bigger than `_wait_for_node_reset` timeout plus `_assert_nodes_not_terminated` waiting_time.
    * Modify parameter name to improve readability
    * Add a 45 seconds delay to accommodate node replacement process (~45s between node down status and replacement)

--- tests/integration-tests/tests/common/hit_common.py
+++ tests/integration-tests/tests/common/hit_common.py
@@ -85,7 +85,9 @@ def assert_compute_node_reasons(scheduler_commands, compute_nodes, expected_reas
         assert_that(node_info).contains(f"Reason={expected_reason}")
 
 
-@retry(wait_fixed=seconds(20), stop_max_delay=minutes(6))
+# TOFIX We observe in 3.13.0 an increase in the bootstrap time for Rocky and RHEL.
+# We must address it and restore the default wait time to 5 minutes.
+@retry(wait_fixed=seconds(20), stop_max_delay=minutes(7))
 def wait_for_num_nodes_in_scheduler(scheduler_commands, desired, filter_by_partition=None):
     assert_num_nodes_in_scheduler(scheduler_commands, desired, filter_by_partition)
 
