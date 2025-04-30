c4167b3525c562d628c434aefe4d9bbd372d0574
hanwenli <hanwenli@amazon.com>
2025-04-30 10:17:26 -0700

    [integ-tests-framework] Only force serial test run if the tests are running in the same region
    This commit uses the first event of slurmd log.

--- tests/integration-tests/conftest.py
+++ tests/integration-tests/conftest.py
@@ -1141,12 +1141,12 @@ def s3_bucket_key_prefix():
 
 
 @pytest.fixture(scope="class")
-def serial_execution_by_instance(request, instance):
+def serial_execution_by_instance(request, instance, region):
     """Enforce serial execution of tests, according to the adopted instance."""
     if instance in ["c5n.18xlarge", "p4d.24xlarge"]:
         logging.info("Enforcing serial execution for instance %s", instance)
         outdir = request.config.getoption("output_dir")
-        lock_file = f"{outdir}/{instance}.lock"
+        lock_file = f"{outdir}/{instance}{region}.lock"
         lock = FileLock(lock_file=lock_file)
         logging.info("Acquiring lock file %s", lock.lock_file)
         with lock.acquire(poll_interval=15, timeout=12000):
