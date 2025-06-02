b59fb4eabb810fec1db50a9bfd24afc284f24407
hanwenli <hanwenli@amazon.com>
2025-06-02 05:36:37 -0700

    [integ-test-framework] Make OS rotation deterministic
    This issue was discovered because there were some tests running with the same OS for a few days

--- tests/integration-tests/framework/tests_configuration/config_renderer.py
+++ tests/integration-tests/framework/tests_configuration/config_renderer.py
@@ -71,8 +71,8 @@ def _propagate_os_jinja_variables(prefix, result, today_number, supported_x86_os
         supported_arm_oses = supported_x86_oses
     # The OS list is the intersection of supported OSes and available AMIs.
     # If the intersection is empty, fallback to supported OS to prevent the framework from failing of div by 0
-    available_amis_oss_x86 = list(set(supported_x86_oses) & set(available_amis_oss_x86)) or supported_x86_oses
-    available_amis_oss_arm = list(set(supported_arm_oses) & set(available_amis_oss_arm)) or supported_arm_oses
+    available_amis_oss_x86 = sorted(list(set(supported_x86_oses) & set(available_amis_oss_x86))) or supported_x86_oses
+    available_amis_oss_arm = sorted(list(set(supported_arm_oses) & set(available_amis_oss_arm))) or supported_arm_oses
     for index in range(len(supported_x86_oses)):
         result[f"{prefix}OS_X86_{index}"] = available_amis_oss_x86[(today_number + index) % len(available_amis_oss_x86)]
         result[f"{prefix}OS_ARM_{index}"] = available_amis_oss_arm[(today_number + index) % len(available_amis_oss_arm)]
