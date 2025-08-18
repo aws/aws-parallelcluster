e120610c34abda9771b5fa939bf232ba8ffdda8f
Xuanqi He <xuanqihe@amazon.com>
2025-08-18 15:56:44 -0400

    To update the ExtraChefAttributes, stop compute fleet then update with the --force-update flag. Then start compute fleet.

--- tests/integration-tests/tests/ultraserver/test_gb200.py
+++ tests/integration-tests/tests/ultraserver/test_gb200.py
@@ -22,6 +22,7 @@ from remote_command_executor import RemoteCommandExecutor
 from tests.common.assertions import assert_regex_in_file, wait_for_instances_in_compute_resource
 from tests.common.schedulers_common import SlurmCommands
 from tests.common.utils import is_existing_remote_file, read_remote_file, terminate_nodes_manually
+from utils import wait_for_computefleet_changed
 
 # This is the capacity block reservation for p6e-gb200.36xlarge.
 # Given the limited availability of this capacity we test this instance type on demand,
@@ -385,7 +386,11 @@ def test_gb200(
         compute_resource_without_imex=compute_resource_without_imex,
     )
 
-    cluster.update(str(updated_cluster_config))
+    cluster.stop()
+    wait_for_computefleet_changed(cluster, "STOPPED")
+    cluster.update(str(updated_cluster_config), force_update=True)
+    cluster.start()
+    wait_for_computefleet_changed(cluster, "RUNNING")
 
     # Verify topology plugin configuration after update
     assert_topology_plugin_configured(
