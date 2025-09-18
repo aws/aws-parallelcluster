74ed690b12691681d405e0b039068e6d83447a18
hanwenli <hanwenli@amazon.com>
2025-09-18 11:21:36 -0700

    [integ-tests] Fix NCCL test on Ubuntu 24 on p6-200
    Add NCCL_SOCKET_FAMILY=AF_INET to force NCCL to use IPv4. On Ubuntu 24 with p6-b200, without this parameter, NCCL hangs on IPv6, which is not supported by ParallelCluster
    
    Reference: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/env.html#nccl-socket-family

--- tests/integration-tests/tests/common/data/nccl/nccl_tests_submit_openmpi.sh
+++ tests/integration-tests/tests/common/data/nccl/nccl_tests_submit_openmpi.sh
@@ -15,10 +15,12 @@ fi
 
 #  -x NCCL_ALGO=ring is not needed after NCCL 2.12. NCCL autodetects which ALGO (ring or tree) to use
 # -x FI_EFA_USE_DEVICE_RDMA=1 is not needed from aws nccl pfi plugin version v1.6.0
+# -x NCCL_SOCKET_FAMILY=AF_INET forces NCCL to use IPv4. On Ubuntu 24 with p6-b200, without this parameter, NCCL hangs on IPv6, which is not supported by ParallelCluster
 mpirun \
 -x LD_LIBRARY_PATH=/shared/openmpi/nccl-${NCCL_VERSION}/build/lib/:${OFI_PATH}:$LD_LIBRARY_PATH \
 -x NCCL_DEBUG=WARNING \
 -x NCCL_TESTS_SPLIT_MASK=0x0 \
+-x NCCL_SOCKET_FAMILY=AF_INET \
 -x RDMAV_FORK_SAFE=1 \
 --bind-to none \
 /shared/openmpi/nccl-tests-${NCCL_BENCHMARKS_VERSION}/build/all_reduce_perf -b 1024 -e 8G -f 2 -g 1 -c 1 > /shared/nccl_tests.out
