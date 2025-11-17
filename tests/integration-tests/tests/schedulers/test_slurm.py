f8168d28ce09b22a2cead9a57aa53289d42b4c8b
Xuanqi He <93849823+hehe7318@users.noreply.github.com>
2025-11-17 09:23:04 -0500

    [IntegTest][develop] For RHEL8, in test_slurm_memory_based_scheduling allocate more memory to avoid OOM kills in isolated regions
    * For RHEL8, in isolated regions, allocate more memory to avoid OOM kills
    * Add a FIXME comment specifying it's a short term solution.

--- tests/integration-tests/tests/schedulers/test_slurm.py
+++ tests/integration-tests/tests/schedulers/test_slurm.py
@@ -724,13 +724,15 @@ def test_slurm_custom_config_parameters(
     assert "4100" == slurm_commands.get_node_attribute("q1-dy-cr2-1", "Memory")
 
 
-@pytest.mark.usefixtures("region", "os", "instance", "scheduler")
+@pytest.mark.usefixtures("instance", "scheduler")
 @pytest.mark.slurm_memory_based_scheduling
 def test_slurm_memory_based_scheduling(
     pcluster_config_reader,
     clusters_factory,
     test_datadir,
     scheduler_commands_factory,
+    os,
+    region,
 ):
     cluster_config = pcluster_config_reader()
     cluster = clusters_factory(cluster_config)
@@ -757,6 +759,8 @@ def test_slurm_memory_based_scheduling(
         remote_command_executor,
         slurm_commands,
         test_datadir,
+        os,
+        region,
     )
 
     _test_memory_based_scheduling_with_multiple_instance_types(slurm_commands)
@@ -2448,6 +2452,8 @@ def _test_memory_based_scheduling_enabled_true(
     remote_command_executor,
     slurm_commands,
     test_datadir,
+    os,
+    region,
 ):
     """Test Slurm with memory-based scheduling feature enabled"""
 
@@ -2517,12 +2523,16 @@ def _test_memory_based_scheduling_enabled_true(
             "raise_on_error": False,
         }
     )
+
+    # FIXME: This is a short term way to unblock the test in isolated regions under conditions we do not fully get.
+    # For RHEL8, allocate more memory to avoid OOM kills in isolated regions
+    mem_allocation = "3000" if os == "rhel8" and "us-iso" in region else "2500"
     job_id_2 = slurm_commands.submit_command_and_assert_job_accepted(
         submit_command_args={
             "nodes": 1,
             "slots": 1,
             "command": "srun ./a.out 2000000000",
-            "other_options": "--mem=2500 -w queue1-st-ondemand1-i1-1",
+            "other_options": f"--mem={mem_allocation} -w queue1-st-ondemand1-i1-1",
             "raise_on_error": False,
         }
     )
