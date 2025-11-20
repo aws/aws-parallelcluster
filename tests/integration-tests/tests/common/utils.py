d5cab0c20ea6f8e11bdffdfe44481c64b88ebc32
Himani Anil Deshpande <himanidp@amazon.com>
2025-11-20 15:17:53 -0500

    [Test] Using new naming convention for filtering Official AL2 DLAMI
    https://docs.aws.amazon.com/dlami/latest/devguide/aws-deep-learning-multiframework-ami-amazon-linux-2.html

--- tests/integration-tests/tests/common/utils.py
+++ tests/integration-tests/tests/common/utils.py
@@ -58,7 +58,7 @@ OS_TO_OFFICIAL_AMI_NAME_OWNER_MAP = {
 
 # Remarkable AMIs are latest deep learning base AMI and FPGA developer AMI without pcluster infrastructure
 OS_TO_REMARKABLE_AMI_NAME_OWNER_MAP = {
-    "alinux2": {"name": "Deep Learning Base AMI (Amazon Linux 2)*", "owners": ["amazon"]},
+    "alinux2": {"name": "Deep Learning OSS Nvidia Driver AMI (Amazon Linux 2)*", "owners": ["amazon"]},
     "ubuntu2404": {
         "name": "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-*-server-*",
         "owners": ["099720109477"],
