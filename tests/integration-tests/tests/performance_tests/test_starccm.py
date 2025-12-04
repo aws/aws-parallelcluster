67d6989345f28ad1ddfe9549674511144fa84bea
Himani Anil Deshpande <himanidp@amazon.com>
2025-12-03 23:41:14 -0500

    [StarCCM] Update test_starccm performance test to use in_place_update_on_fleet_enabled

--- tests/integration-tests/tests/performance_tests/test_starccm.py
+++ tests/integration-tests/tests/performance_tests/test_starccm.py
@@ -58,12 +58,13 @@ def calculate_observed_value(result, remote_command_executor, scheduler_commands
 
 
 @pytest.mark.usefixtures("serial_execution_by_instance")
-@pytest.mark.flaky(reruns=0)
+@pytest.mark.parametrize("in_place_update_on_fleet_enabled", ["true", "false"])
 def test_starccm(
     vpc_stack,
     instance,
     os,
     region,
+    in_place_update_on_fleet_enabled,
     scheduler,
     pcluster_config_reader,
     clusters_factory,
@@ -77,10 +78,15 @@ def test_starccm(
     s3 = boto3.client("s3")
     s3.upload_file(str(test_datadir / "dependencies.install.sh"), bucket_name, "scripts/dependencies.install.sh")
 
+    extra_chef_attributes = '{{"cluster": {{"in_place_update_on_fleet_enabled": "{}"}}}}'.format(
+        in_place_update_on_fleet_enabled
+    )
+
     cluster_config = pcluster_config_reader(
         bucket_name=bucket_name,
         install_extra_deps=os in OSS_REQUIRING_EXTRA_DEPS,
         number_of_nodes=max(number_of_nodes),
+        extra_chef_attributes=extra_chef_attributes,
     )
     cluster = clusters_factory(cluster_config)
     logging.info("Cluster Created")
@@ -109,9 +115,8 @@ def test_starccm(
         instance_slots = fetch_instance_slots(region, instance, multithreading_disabled=True)
         run_command = f'sbatch --ntasks={num_of_nodes * instance_slots} starccm.slurm.sh "{podkey}" "{licpath}"'
         multiple_runs = []
-        # Run at least four times up to whatever parallelism allows to maximize usage of available nodes
-        # Running the test multiple times reduces and get the average value improves stability of the result.
-        number_of_runs = max(parallelism, 4)
+        # Run at least twice up to whatever parallelism allows to maximize usage of available nodes
+        number_of_runs = max(parallelism, 2)
         for _ in range(number_of_runs):
             multiple_runs.append(remote_command_executor.run_remote_command(run_command))
         for run in multiple_runs:
