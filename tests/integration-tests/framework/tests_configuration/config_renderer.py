0d14503a597e11f6077c879b8c5ba02ed56ffc5c
hanwenli <hanwenli@amazon.com>
2026-03-02 06:46:09 -0800

    [integ-tests-framework] Expand regions of dynamic capacity reservation
    This further reduces insufficient capacity issues

--- tests/integration-tests/framework/tests_configuration/config_renderer.py
+++ tests/integration-tests/framework/tests_configuration/config_renderer.py
@@ -299,7 +299,16 @@ def _check_or_create_capacity_reservations(config_file, os_parameters, instance_
                 )
                 end_date = datetime.now(timezone.utc) + timedelta(hours=hours)
                 specs.append((instance_type, os_platform, count, end_date, enable_placement_group))
-            candidate_regions = ["us-east-1", "us-east-2", "us-west-2", "eu-west-1"]
+            candidate_regions = [
+                "us-east-1",
+                "us-east-2",
+                "us-west-2",
+                "eu-west-1",
+                "ap-northeast-2",
+                "ap-southeast-2",
+                "ap-northeast-1",
+                "eu-west-2",
+            ]
             if len(specs) == 1:
                 # Single instance type reservation: check for existing reservation to be frugal.
                 # It is hard to implement such logic for multiple instance types reservation.
