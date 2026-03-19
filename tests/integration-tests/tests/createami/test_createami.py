f6ca265facc3823d05ff4923ba2f86443121d0a2
Helena Greebe <hgreebe@amazon.com>
2026-03-19 11:06:16 -0400

    Add test for build image in isolated env
    Switch proxy to al2023
    
    Fix allowlist

--- tests/integration-tests/tests/createami/test_createami.py
+++ tests/integration-tests/tests/createami/test_createami.py
@@ -12,6 +12,7 @@
 import datetime
 import json
 import logging
+import os
 import re
 import tarfile
 import tempfile
@@ -49,6 +50,52 @@ class ImageNotFound(Exception):
     """Exception when image is not found."""
 
     pass
+
+
+@pytest.fixture(scope="class")
+def no_internet_proxy_stack(region, request, cfn_stacks_factory):
+    """Deploy a VPC with a proxy that only allows OS repo access and VPC endpoints for AWS services."""
+    proxy_template_path = os.path.join(os.path.dirname(__file__), "test_createami", "test_build_image_no_internet", "proxy_stack.yaml")
+    with open(proxy_template_path) as f:
+        template = f.read()
+
+    stack = CfnStack(
+        name=generate_stack_name("integ-tests-build-image-no-internet", request.config.getoption("stackname_suffix")),
+        region=region,
+        template=template,
+        parameters=[{"ParameterKey": "Keypair", "ParameterValue": request.config.getoption("key_name")}],
+        capabilities=["CAPABILITY_IAM"],
+    )
+    cfn_stacks_factory.create_stack(stack)
+    yield stack
+    if not request.config.getoption("no_delete"):
+        cfn_stacks_factory.delete_stack(stack.name, region)
+
+
+@pytest.mark.usefixtures("instance")
+def test_build_image_no_internet(
+    region,
+    os,
+    instance,
+    pcluster_config_reader,
+    architecture,
+    no_internet_proxy_stack,
+    images_factory,
+    request,
+):
+    """Test build image in a private subnet with no internet access, only VPC endpoints and a proxy for OS repos."""
+    base_ami = retrieve_latest_ami(region, os, architecture=architecture)
+
+    image_id = generate_stack_name("integ-tests-build-image-no-internet", request.config.getoption("stackname_suffix"))
+    image_config = pcluster_config_reader(
+        config_file="image.config.yaml",
+        parent_image=base_ami,
+        subnet_id=no_internet_proxy_stack.cfn_outputs["PrivateSubnetId"],
+        security_group_id=no_internet_proxy_stack.cfn_outputs["DefaultSecurityGroupId"],
+    )
+
+    image = images_factory(image_id, image_config, region)
+    _test_build_image_success(image)
 
 
 @pytest.mark.usefixtures("instance")
