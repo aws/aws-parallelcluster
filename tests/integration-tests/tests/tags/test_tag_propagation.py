34ae15311c3932ee51d8a35610a6e0012043cffa
hanwenli <hanwenli@amazon.com>
2026-04-14 11:28:01 -0700

    [integ-tests] Skip root volume tags check in us-iso regions
    The tags are not propagated to root volumes in us-iso regions

--- tests/integration-tests/tests/tags/test_tag_propagation.py
+++ tests/integration-tests/tests/tags/test_tag_propagation.py
@@ -128,6 +128,7 @@ def _check_tag_propagation(
                 {"Name": "HeadNode", "parallelcluster:node-type": "HeadNode"},
             ),
             "tag_getter_kwargs": {"cluster": cluster, "os": os},
+            "skip": "us-iso" in cluster.region,
         },
         {
             "resource": "Compute Node",
