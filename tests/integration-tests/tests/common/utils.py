a63e9f72b2923aa716012cd85253575385a8ce63
Helena Greebe <hgreebe@amazon.com>
2026-05-27 12:36:48 -0400

    Pin Rhel 9.7 version to fix efa installation
    (cherry picked from commit bff786f888425537e89d989538cb6afae734d98a)

--- tests/integration-tests/tests/common/utils.py
+++ tests/integration-tests/tests/common/utils.py
@@ -53,7 +53,7 @@ OS_TO_OFFICIAL_AMI_NAME_OWNER_MAP = {
     "rocky8": {"name": "Rocky-8-EC2-Base-8.10*", "owners": ["792107900819"]},  # TODO add china and govcloud accounts
     "rhel8.9": {"name": "RHEL-8.9*_HVM-*", "owners": RHEL_OWNERS},
     "rocky8.9": {"name": "Rocky-8-EC2-Base-8.9*", "owners": ["792107900819"]},  # TODO add china and govcloud accounts
-    "rhel9": {"name": "RHEL-9.*_HVM*", "owners": RHEL_OWNERS},
+    "rhel9": {"name": "RHEL-9.7*_HVM*", "owners": RHEL_OWNERS},
     "rocky9": {"name": "Rocky-9-EC2-Base-9.*", "owners": ["792107900819"]},  # TODO add china and govcloud accounts
 }
 
