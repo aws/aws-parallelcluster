7d0a111ec295ec0d67cd4502f7127423c4bec34d
hgreebe <141743196+hgreebe@users.noreply.github.com>
2026-08-26 12:50:24 +0000

    Bump version to 3.16.1

--- cli/src/pcluster/constants.py
+++ cli/src/pcluster/constants.py
@@ -156,8 +156,8 @@ MAX_NEW_STORAGE_COUNT = {"efs": 1, "fsx": 1, "raid": 1}
 MAX_EXISTING_STORAGE_COUNT = {"efs": 20, "fsx": 20, "raid": 0}
 
 COOKBOOK_PACKAGES_VERSIONS = {
-    "parallelcluster": "3.16.0",
-    "cookbook": "aws-parallelcluster-cookbook-3.16.0",
+    "parallelcluster": "3.16.1",
+    "cookbook": "aws-parallelcluster-cookbook-3.16.1",
     "chef": "19.3.14",
     "ami": "dev",
 }
