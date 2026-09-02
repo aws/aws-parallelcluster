e8e2bb7f8e84195396eda6e62da94ef26416a8dd
Giacomo Marciani <mgiacomo@amazon.com>
2026-09-02 10:54:32 -0400

    Revert "DROPME Inject failure in cluster creation to validate RCA improvements."
    This reverts commit afc80f30a41a5c29486a21a9f64f7e60ff86e751.

--- tests/integration-tests/tests/basic/test_essential_features/test_essential_features/pre_install.sh
+++ tests/integration-tests/tests/basic/test_essential_features/test_essential_features/pre_install.sh
@@ -8,9 +8,6 @@ done
 
 case $# in
     3)
-        sudo mkdir -p /etc/systemd/system/nfs-mountd.service.d
-        printf '[Service]\nExecStartPre=/bin/false\n' | sudo tee /etc/systemd/system/nfs-mountd.service.d/inject-fail.conf
-        sudo systemctl daemon-reload
         exit 0
     ;;
     *)
